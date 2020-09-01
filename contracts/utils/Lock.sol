57b37dbee48ae09bf0f4c735429c7d24a28a12bf
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-09-01 14:35:17 -0300

    Create PoolRegistry, remove BPT, exit fee, and join/exit swap

---
+++ contracts/utils/Lock.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.5.12;
+
+contract Lock {
+    modifier _lock_() {
+        require(!_mutex, "ERR_REENTRY");
+        _mutex = true;
+        _;
+        _mutex = false;
+    }
+
+    modifier _viewlock_() {
+        require(!_mutex, "ERR_REENTRY");
+        _;
+    }
+
+    bool private _mutex;
+}
---
+++ contracts/utils/Logs.sol
@@ -0,0 +1,47 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity 0.5.12;
+
+contract Logs {
+    event LOG_SWAP(
+        address indexed caller,
+        address indexed tokenIn,
+        address indexed tokenOut,
+        uint256         tokenAmountIn,
+        uint256         tokenAmountOut
+    );
+
+    event LOG_JOIN(
+        address indexed caller,
+        address indexed tokenIn,
+        uint256         tokenAmountIn
+    );
+
+    event LOG_EXIT(
+        address indexed caller,
+        address indexed tokenOut,
+        uint256         tokenAmountOut
+    );
+
+    event LOG_CALL(
+        bytes4  indexed sig,
+        address indexed caller,
+        bytes           data
+    ) anonymous;
+
+    modifier _logs_() {
+        emit LOG_CALL(msg.sig, msg.sender, msg.data);
+        _;
+    }
+}
