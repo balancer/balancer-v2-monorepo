db3b05cf9bdc861bbfd4ecd995f6f871b2d68bc2
Greg Taschuk <gregorytaschuk@gmail.com>
2020-09-06 16:03:02 -0700

    pool initialization, joining, and exit

--- contracts/IPoolGovernance.sol
+++ contracts/IPoolGovernance.sol
@@ -7,7 +7,12 @@ interface IPoolGovernance {
 
   function setPublicSwap(bytes32 poolID) external;
 
-  //function bind(bytes32 poolID, address token, uint balance, uint denorm);
-  //function rebind(bytes32 poolID, address token, uint balance, uint denorm);
-  //function unbind(bytes32 poolID, address token);
+  function addInitialLiquidity(bytes32 poolID, address[] calldata initialTokens, uint[] calldata amountsIn) external;
+  function addLiquidity(bytes32 poolID, uint[] calldata amountsIn) external;
+  function removeLiquidity(bytes32 poolID, address recipient, uint[] calldata amountsOut) external;
+
+  function getTokenAmountsIn(bytes32 poolID, uint ratio, uint[] calldata maxAmountsIn) external returns (uint[] memory);
+  function getTokenAmountsOut(bytes32 poolID, uint ratio, uint[] calldata minAmountsOut) external returns (uint[] memory);
+  function getPoolTokenBalance(bytes32 poolID, address token) external view returns (uint);
+  function getPoolTokens(bytes32 poolID) external view returns (address[] memory);
 }
