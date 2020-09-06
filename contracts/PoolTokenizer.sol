99432b772f7dec4f12a6115d0018663e9703f453
Greg Taschuk <gregorytaschuk@gmail.com>
2020-09-06 16:03:02 -0700

    pool initialization, joining, and exit

--- contracts/PoolTokenizer.sol
+++ contracts/PoolTokenizer.sol
@@ -3,29 +3,137 @@
 pragma solidity 0.5.12;
 
 import "./IPoolGovernance.sol";
+import "./BToken.sol";
 
-contract PoolTokenizer{
+contract PoolTokenizer is BToken{
   IPoolGovernance vault;
   bytes32 public poolID;
+  address owner;
 
   constructor(address _vault, bytes32 _poolID) public {
     vault = IPoolGovernance(_vault);
     poolID = _poolID;
+    owner = msg.sender;
   }
 
-  function setSwapFee(uint swapFee) public {
+  modifier _lock_() {
+    require(!_mutex, "ERR_REENTRY");
+    _mutex = true;
+    _;
+    _mutex = false;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Must be the contract owner");
+    _;
+  }
+
+  bool private _mutex;
+  
+
+  // Governance functions
+  function setOwner(address newOwner) public onlyOwner {
+    owner = newOwner;
+  }
+
+  function setSwapFee(uint swapFee) public onlyOwner {
     vault.setSwapFee(poolID, swapFee);
   }
 
-  function setController(address manager) public {
+  function setController(address manager) public onlyOwner {
     vault.setController(poolID, manager);
   }
 
-  function setPublicSwap() public {
+  function setPublicSwap() public onlyOwner {
     vault.setPublicSwap(poolID);
   }
 
-  //function bind(address token, uint balance, uint denorm);
-  //function rebind(address token, uint balance, uint denorm)
-  //function unbind(address token)
+
+  // Add the initial liquidity to a pool
+  function initPool(uint initialBPT, address[] calldata initialTokens, uint[] calldata initialBalances) external onlyOwner {
+    require(totalSupply() == 0, "POOL ALREADY INITIALIZED");
+    require(msg.sender == owner, "MUST BE OWNER");
+
+    for (uint i = 0; i < initialTokens.length; i++) {
+      address t = initialTokens[i];
+      uint amountIn = initialBalances[i];
+      IERC20(t).transferFrom(msg.sender, address(vault), amountIn);
+    }
+
+    vault.addInitialLiquidity(poolID, initialTokens, initialBalances);
+    _mintPoolShare(initialBPT);
+    _pushPoolShare(msg.sender, initialBPT);
+  }
+
+
+  // Joining a pool
+  // poolAmountOut - how much bpt the user expects to get
+  // maxAmountsIn - the max amounts of each token the user is willing to add to the vault
+  function joinPool(uint poolAmountOut, uint[] calldata maxAmountsIn)
+  external
+  _lock_
+  {
+    uint poolTotal = totalSupply();
+    uint ratio = bdiv(poolAmountOut, poolTotal);
+    require(ratio != 0, "ERR_MATH_APPROX");
+
+
+    address[] memory tokens = vault.getPoolTokens(poolID);
+    uint[] memory amountsIn = vault.getTokenAmountsIn(poolID, ratio, maxAmountsIn);
+
+    for (uint i = 0; i < tokens.length; i++) {
+      address t = tokens[i];
+      uint amountIn = amountsIn[i];
+      bool xfer = IERC20(t).transferFrom(msg.sender, address(vault), amountIn);
+      require(xfer, "transfer must succeed");
+    }
+
+    vault.addLiquidity(poolID, amountsIn);
+
+    _mintPoolShare(poolAmountOut);
+    _pushPoolShare(msg.sender, poolAmountOut);
+  }
+
+  function exitPool(uint poolAmountIn, uint[] calldata minAmountsOut)
+  external
+  _lock_
+  {
+    uint poolTotal = totalSupply();
+    uint ratio = bdiv(poolAmountIn, poolTotal);
+    require(ratio != 0, "ERR_MATH_APPROX");
+
+    _pullPoolShare(msg.sender, poolAmountIn);
+    _burnPoolShare(poolAmountIn);
+
+    uint[] memory amountsOut = vault.getTokenAmountsOut(poolID, ratio, minAmountsOut);
+
+    vault.removeLiquidity(poolID, msg.sender, amountsOut);
+  }
+
+  // 'Underlying' token-manipulation functions make external calls but are NOT locked
+  // You must `_lock_` or otherwise ensure reentry-safety
+
+  function _pullPoolShare(address from, uint amount)
+  internal
+  {
+    _pull(from, amount);
+  }
+
+  function _pushPoolShare(address to, uint amount)
+  internal
+  {
+    _push(to, amount);
+  }
+
+  function _mintPoolShare(uint amount)
+  internal
+  {
+    _mint(amount);
+  }
+
+  function _burnPoolShare(uint amount)
+  internal
+  {
+    _burn(amount);
+  }
 }
--- test/PoolTokenizer.test.ts
+++ test/PoolTokenizer.test.ts
@@ -2,16 +2,31 @@ import { ethers } from "@nomiclabs/buidler";
 const { expect } = require("chai");
 import { ContractFactory, Contract, Signer } from "ethers";
 
+const { BigNumber } = ethers;
+const TTOKEN_DECIMALS = 3
+
+const fromTokenUnits = (num: string) => {
+  const power = BigNumber.from(10).pow(TTOKEN_DECIMALS)
+  const scaled = parseFloat(num)
+  return BigNumber.from(scaled).mul(BigNumber.from(power))
+}
+
 describe("PoolTokenizer", function() {
   let deployer: Signer
+  let admin: Signer
+  let user1: Signer
+  let user2: Signer
+  let adminAddress: string, user1Address: string, user2Address: string;
   let poolID: string
   let vault: Contract
   let tokenizer: Contract;
 
   beforeEach(async function() {
-    let [user1] = await ethers.getSigners();
+    [deployer, admin, user1, user2] = await ethers.getSigners();
 
-    deployer = user1;
+    adminAddress = await admin.getAddress()
+    user1Address = await user1.getAddress()
+    user2Address = await user2.getAddress()
 
     const Vault: ContractFactory = await ethers.getContractFactory("MockVault");
     const Tokenizer: ContractFactory = await ethers.getContractFactory("PoolTokenizer");
@@ -24,14 +39,16 @@ describe("PoolTokenizer", function() {
 
     tokenizer = await Tokenizer.deploy(vault.address, poolID);
     await tokenizer.deployed();
+    await tokenizer.setOwner(adminAddress)
+    tokenizer = tokenizer.connect(admin)
+    vault = vault.connect(admin)
 
     await vault.createPool(poolID)
   })
 
   it("Should give your Tokenizer sole proprietorship", async function() {
-
     let [returnedController, returnedSwapFee, returnedSwapPublic] = await vault.pools(poolID);
-    expect(returnedController).to.equal(await deployer.getAddress());
+    expect(returnedController).to.equal(await admin.getAddress());
 
     await vault.setController(poolID, tokenizer.address)
 
@@ -44,5 +61,85 @@ describe("PoolTokenizer", function() {
     expect(returnedSwapFee3).to.equal(123);
   });
 
+  describe("with tokens and a tokenizer", () => {
+    let weth: Contract, dai: Contract;
+    let WETH: string, DAI: string;
+    beforeEach(async () => {
+      const TToken: ContractFactory = await ethers.getContractFactory("TToken");
+      weth = await TToken.deploy('Wrapped Ether', 'WETH', TTOKEN_DECIMALS);
+      dai = await TToken.deploy('Dai Stablecoin', 'DAI', TTOKEN_DECIMALS);
 
+      await weth.deployed()
+      await dai.deployed()
+
+      WETH = weth.address;
+      DAI = dai.address;
+
+      //Admin balances
+      await weth.mint(adminAddress, fromTokenUnits('100'));
+      await dai.mint(adminAddress, fromTokenUnits('100'));
+
+      // User1 balances
+      await weth.mint(user1Address, fromTokenUnits('25'));
+      await dai.mint(user1Address, fromTokenUnits('40000'));
+
+      // User2 balances
+      await weth.mint(user2Address, fromTokenUnits('12'));
+      await dai.mint(user2Address, fromTokenUnits('0'));
+
+      await vault.setController(poolID, tokenizer.address)
+
+      weth = weth.connect(admin)
+      await weth.approve(tokenizer.address, fromTokenUnits('1000'));
+      dai = dai.connect(admin)
+      await dai.approve(tokenizer.address, fromTokenUnits('1000'));
+    })
+
+    it("Should let you initialize a pool", async () => {
+      weth = weth.connect(admin)
+      await weth.approve(tokenizer.address, fromTokenUnits('1000'));
+      dai = dai.connect(admin)
+      await dai.approve(tokenizer.address, fromTokenUnits('1000'));
+
+      // Admin inits pool
+      tokenizer = tokenizer.connect(admin)
+      await tokenizer.initPool(100, [WETH, DAI], [fromTokenUnits('20'), fromTokenUnits('30')])
+      let bpt = await tokenizer.balanceOf(adminAddress)
+      expect(bpt.toNumber()).to.equal(100)
+    })
+    describe("with an initialized pool", () => {
+      beforeEach(async () => {
+        // Admin inits pool
+        tokenizer = tokenizer.connect(admin)
+        await tokenizer.initPool(100, [WETH, DAI], [fromTokenUnits('20'), fromTokenUnits('30')])
+      })
+
+      it("Should allow you to join a pool", async () => {
+        weth = weth.connect(user1)
+        await weth.approve(tokenizer.address, fromTokenUnits('1000'));
+        dai = dai.connect(user1)
+        await dai.approve(tokenizer.address, fromTokenUnits('1000'));
+
+        // User 1 joins pool
+        tokenizer = tokenizer.connect(user1)
+        await tokenizer.joinPool(50, [fromTokenUnits('15'), fromTokenUnits('25')])
+        let bpt = await tokenizer.balanceOf(user1Address)
+        expect(bpt.toNumber()).to.equal(50)
+      })
+
+      it("Should not allow you to join a pool when maxAmountIn is too low", async () => {
+        tokenizer = tokenizer.connect(user1)
+        await expect(tokenizer.joinPool(50, [fromTokenUnits('5'), fromTokenUnits('5')])).to.be.revertedWith('ERR_LIMIT_IN')
+      })
+
+      describe('as a member of an initialized pool', async () => {
+        it('Should allow you to exit a pool', async () => {
+          // admin withdraws half their balance
+          await tokenizer.exitPool(50, [fromTokenUnits('10'), fromTokenUnits('15')])
+          let bpt = await tokenizer.balanceOf(adminAddress)
+          expect(bpt.toNumber()).to.equal(50)
+        })
+      })
+    })
+  })
 });
