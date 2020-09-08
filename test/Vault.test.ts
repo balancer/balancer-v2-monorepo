03d566387c37e97fe787c94222fef8494268c357
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-09-08 18:53:29 -0300

    Remove outdated failing test

--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -6,19 +6,11 @@ describe("Vault", () => {
   let VaultFactory: ContractFactory;
   let vault: Contract;
 
-  let feeCollector: Signer;
-
   before(async () => {
     VaultFactory = await ethers.getContractFactory("Vault");
-
-    [feeCollector] = await ethers.getSigners();
   });
 
   beforeEach('deploy vault from feeCollector', async () => {
-    vault = await (await VaultFactory.connect(feeCollector).deploy()).deployed();
-  });
-
-  it("has correct feeCollector", async function() {
-    expect(await vault.getFeeCollector()).to.equal(await feeCollector.getAddress());
+    vault = await (await VaultFactory.deploy()).deployed();
   });
 });
