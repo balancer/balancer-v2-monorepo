20d5701d9b592650999618e59c84bab3aa59186d
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-09-16 15:48:24 -0300

    Fix linter errors

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,17 +1,17 @@
-import { usePlugin, BuidlerConfig } from "@nomiclabs/buidler/config";
+import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config';
 
-usePlugin("@nomiclabs/buidler-waffle");
+usePlugin('@nomiclabs/buidler-waffle');
 
 const config: BuidlerConfig = {
   networks: {
-    "buidlerevm": {
+    buidlerevm: {
       // The BPool contract is too large to be deployed normally when compiled with no optimizations
       blockGasLimit: 12e6,
       allowUnlimitedContractSize: true,
     },
   },
   solc: {
-    version: "0.5.12",
+    version: '0.5.12',
   },
 };
 
--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -1,6 +1,6 @@
 import { ethers } from '@nomiclabs/buidler';
 import { expect } from 'chai';
-import { ContractFactory, Contract, Signer, BigNumber, ContractReceipt } from 'ethers';
+import { ContractFactory, Contract, Signer, ContractReceipt } from 'ethers';
 import * as expectEvent from './helpers/expectEvent';
 import { MAX_UINT256 } from './helpers/constants';
 
@@ -12,9 +12,9 @@ describe('Vault', () => {
   let vault: Contract;
 
   let TestTokenFactory: ContractFactory;
-  let tokens: { [symbol: string]: Contract } = {};
+  const tokens: { [symbol: string]: Contract } = {};
 
-  async function deployToken(symbol: string, decimals?: Number) {
+  async function deployToken(symbol: string, decimals?: number) {
     const token = await (await TestTokenFactory.deploy(symbol, symbol, decimals ?? 18)).deployed();
     tokens[symbol] = token;
   }
@@ -34,14 +34,11 @@ describe('Vault', () => {
   });
 
   describe('pool management', () => {
-    let poolId: String;
+    let poolId: string;
 
     beforeEach('add pool', async () => {
-      poolId = ethers.utils.id('Test')
-      const receipt: ContractReceipt = await (await vault.connect(controller).newPool(poolId)).wait();
-      const event = expectEvent.inReceipt(receipt, 'PoolCreated');
-
-      //poolId = event.args!.poolId;
+      poolId = ethers.utils.id('Test');
+      await vault.connect(controller).newPool(poolId);
     });
 
     it('has the correct controller', async () => {
@@ -54,82 +51,86 @@ describe('Vault', () => {
 
     beforeEach('setup pools & mint tokens', async () => {
       // Mint and approve controller liquidity
-      await Promise.all(['DAI', 'MKR'].map(async token => {
-        await tokens[token].mint(await controller.getAddress(), 100e18.toString());
-        await tokens[token].connect(controller).approve(vault.address, MAX_UINT256);
-      }));
-
+      await Promise.all(
+        ['DAI', 'MKR'].map(async (token) => {
+          await tokens[token].mint(await controller.getAddress(), (100e18).toString());
+          await tokens[token].connect(controller).approve(vault.address, MAX_UINT256);
+        })
+      );
 
       for (let poolIdIdx = 0; poolIdIdx < totalPools; ++poolIdIdx) {
-        let poolId: String = ethers.utils.id('batch' + poolIdIdx)
+        const poolId: string = ethers.utils.id('batch' + poolIdIdx);
 
         const receipt: ContractReceipt = await (await vault.connect(controller).newPool(poolId)).wait();
         expectEvent.inReceipt(receipt, 'PoolCreated', { poolId });
 
         // 50-50 DAI-MKR pool with 1e18 tokens in each
-        await vault.connect(controller).bind(poolId, tokens['DAI'].address, 1e18.toString(), 1e18.toString());
-        await vault.connect(controller).bind(poolId, tokens['MKR'].address, 1e18.toString(), 1e18.toString());
+        await vault.connect(controller).bind(poolId, tokens['DAI'].address, (1e18).toString(), (1e18).toString());
+        await vault.connect(controller).bind(poolId, tokens['MKR'].address, (1e18).toString(), (1e18).toString());
       }
 
       // Mint tokens for trader
-      await tokens['DAI'].mint(await trader.getAddress(), 1e18.toString());
-      await tokens['MKR'].mint(await trader.getAddress(), 1e18.toString());
+      await tokens['DAI'].mint(await trader.getAddress(), (1e18).toString());
+      await tokens['MKR'].mint(await trader.getAddress(), (1e18).toString());
     });
 
     it('single pair single pool swap', async () => {
       // Trade 1e18 MKR for 0.5e18 DAI
 
-      const diffs = [{
-        token: tokens['DAI'].address,
-        vaultDelta: 0
-      }, {
-        token: tokens['MKR'].address,
-        vaultDelta: 0
-      }];
+      const diffs = [
+        {
+          token: tokens['DAI'].address,
+          vaultDelta: 0,
+        },
+        {
+          token: tokens['MKR'].address,
+          vaultDelta: 0,
+        },
+      ];
 
       const swaps = [
         {
           poolId: ethers.utils.id('batch0'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.51e18.toString() }, // Math isn't 100% accurate
-          tokenB: { tokenDiffIndex: 1, balance: 2e18.toString() },
-        }
+          tokenA: { tokenDiffIndex: 0, balance: (0.51e18).toString() }, // Math isn't 100% accurate
+          tokenB: { tokenDiffIndex: 1, balance: (2e18).toString() },
+        },
       ];
 
       // Send tokens & swap - would normally happen in the same tx
-      await tokens['MKR'].connect(trader).transfer(vault.address, 1e18.toString());
-      const receipt = await (await vault.connect(trader).batchSwap(diffs, swaps)).wait();
-
-      // console.log('Gas:', receipt.gasUsed.toString());
+      await tokens['MKR'].connect(trader).transfer(vault.address, (1e18).toString());
+      await vault.connect(trader).batchSwap(diffs, swaps);
     });
 
     it('single pair multi pool (batch) swap', async () => {
       // Trade 0.68e18 MKR for 0.5e18 DAI
 
-      const diffs = [{
-        token: tokens['DAI'].address,
-        vaultDelta: 0
-      }, {
-        token: tokens['MKR'].address,
-        vaultDelta: 0
-      }];
+      const diffs = [
+        {
+          token: tokens['DAI'].address,
+          vaultDelta: 0,
+        },
+        {
+          token: tokens['MKR'].address,
+          vaultDelta: 0,
+        },
+      ];
 
       const swaps = [
         {
           poolId: ethers.utils.id('batch0'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.75e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 1.34e18.toString() },
-        },{
+          tokenA: { tokenDiffIndex: 0, balance: (0.75e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (1.34e18).toString() },
+        },
+        {
           poolId: ethers.utils.id('batch1'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.75e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 1.34e18.toString() },
-        }
+          tokenA: { tokenDiffIndex: 0, balance: (0.75e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (1.34e18).toString() },
+        },
       ];
 
       // Send tokens & swap - would normally happen in the same tx
-      await tokens['MKR'].connect(trader).transfer(vault.address, 0.68e18.toString());
-      const receipt = await (await vault.connect(trader).batchSwap(diffs, swaps)).wait();
-
-      // console.log('Gas:', receipt.gasUsed.toString());
+      await tokens['MKR'].connect(trader).transfer(vault.address, (0.68e18).toString());
+      await vault.connect(trader).batchSwap(diffs, swaps);
     });
   });
 
@@ -138,58 +139,69 @@ describe('Vault', () => {
 
     beforeEach('setup unbalanced pools & mint tokens', async () => {
       // Mint and approve controller liquidity
-      await Promise.all(['DAI', 'MKR'].map(async token => {
-        await tokens[token].mint(await controller.getAddress(), 100e18.toString());
-        await tokens[token].connect(controller).approve(vault.address, MAX_UINT256);
-      }));
+      await Promise.all(
+        ['DAI', 'MKR'].map(async (token) => {
+          await tokens[token].mint(await controller.getAddress(), (100e18).toString());
+          await tokens[token].connect(controller).approve(vault.address, MAX_UINT256);
+        })
+      );
 
       for (let poolIdIdx = 0; poolIdIdx < totalPools; ++poolIdIdx) {
-        let poolId = ethers.utils.id('unbalanced' + poolIdIdx)
+        const poolId = ethers.utils.id('unbalanced' + poolIdIdx);
         const receipt: ContractReceipt = await (await vault.connect(controller).newPool(poolId)).wait();
         expectEvent.inReceipt(receipt, 'PoolCreated', { poolId });
 
         // 50-50 DAI-MKR pool with a 1 to 4 DAI:MKR ratio
-        await vault.connect(controller).bind(poolId, tokens['DAI'].address, 0.5e18.toString(), 1e18.toString());
-        await vault.connect(controller).bind(poolId, tokens['MKR'].address, 2e18.toString(), 1e18.toString());
+        await vault.connect(controller).bind(poolId, tokens['DAI'].address, (0.5e18).toString(), (1e18).toString());
+        await vault.connect(controller).bind(poolId, tokens['MKR'].address, (2e18).toString(), (1e18).toString());
       }
 
       // Move the first pool to a difference price point (1 to 10 DAI:MKR) by withdrawing DAI
-      let firstPoolId = ethers.utils.id('unbalanced0')
-      await vault.connect(controller).rebind(firstPoolId, tokens['DAI'].address, 0.2e18.toString(), 1e18.toString());
+      const firstPoolId = ethers.utils.id('unbalanced0');
+      await vault
+        .connect(controller)
+        .rebind(firstPoolId, tokens['DAI'].address, (0.2e18).toString(), (1e18).toString());
     });
 
     it('works', async () => {
-      const diffs = [{
-        token: tokens['DAI'].address,
-        vaultDelta: 0
-      }, {
-        token: tokens['MKR'].address,
-        vaultDelta: 0
-      }];
+      const diffs = [
+        {
+          token: tokens['DAI'].address,
+          vaultDelta: 0,
+        },
+        {
+          token: tokens['MKR'].address,
+          vaultDelta: 0,
+        },
+      ];
 
       // Move the unbalanced pool to a 1:7 ratio (this is not the optimal ratio)
 
       const swaps = [
         {
           poolId: ethers.utils.id('unbalanced0'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.2391e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 1.673e18.toString() },
-        },{
+          tokenA: { tokenDiffIndex: 0, balance: (0.2391e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (1.673e18).toString() },
+        },
+        {
           poolId: ethers.utils.id('unbalanced1'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.4902e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 2.04e18.toString() },
-        },{
+          tokenA: { tokenDiffIndex: 0, balance: (0.4902e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (2.04e18).toString() },
+        },
+        {
           poolId: ethers.utils.id('unbalanced2'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.4902e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 2.04e18.toString() },
-        },{
+          tokenA: { tokenDiffIndex: 0, balance: (0.4902e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (2.04e18).toString() },
+        },
+        {
           poolId: ethers.utils.id('unbalanced3'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.4902e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 2.04e18.toString() },
-        },{
+          tokenA: { tokenDiffIndex: 0, balance: (0.4902e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (2.04e18).toString() },
+        },
+        {
           poolId: ethers.utils.id('unbalanced4'),
-          tokenA: { tokenDiffIndex: 0, balance: 0.4902e18.toString() },
-          tokenB: { tokenDiffIndex: 1, balance: 2.04e18.toString() },
+          tokenA: { tokenDiffIndex: 0, balance: (0.4902e18).toString() },
+          tokenB: { tokenDiffIndex: 1, balance: (2.04e18).toString() },
         },
       ];
 
@@ -197,7 +209,7 @@ describe('Vault', () => {
       const preMKR = await tokens['MKR'].balanceOf(await trader.getAddress());
 
       // Swap
-      const receipt = await (await vault.connect(trader).batchSwap(diffs, swaps)).wait();
+      await vault.connect(trader).batchSwap(diffs, swaps);
 
       const postDAI = await tokens['DAI'].balanceOf(await trader.getAddress());
       const postMKR = await tokens['MKR'].balanceOf(await trader.getAddress());
