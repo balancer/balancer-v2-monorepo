eb1f8938d949b8d1d21007ad774ac291f51c69f4
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-09-22 17:17:30 -0300

    Enable Solidity optimizer

--- buidler.config.ts
+++ buidler.config.ts
@@ -3,15 +3,12 @@ import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config';
 usePlugin('@nomiclabs/buidler-waffle');
 
 const config: BuidlerConfig = {
-  networks: {
-    buidlerevm: {
-      // The BPool contract is too large to be deployed normally when compiled with no optimizations
-      blockGasLimit: 12e6,
-      allowUnlimitedContractSize: true,
-    },
-  },
   solc: {
     version: '0.5.12',
+    optimizer: {
+      enabled: true,
+      runs: 9999,
+    },
   },
 };
 
