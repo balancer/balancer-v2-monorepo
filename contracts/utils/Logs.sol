38b97bd75eee7fc244734eca98400f96ff6bea99
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-09-23 13:13:33 -0300

    Port all contracts to solc 0.7

--- contracts/utils/Logs.sol
+++ contracts/utils/Logs.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +12,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity 0.5.12;
+pragma solidity ^0.7.1;
 
 contract Logs {
     event LogSwap(
