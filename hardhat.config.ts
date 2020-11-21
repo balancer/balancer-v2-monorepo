7f6694576c106ade5196606c76d8837f4b4d2bdc
Mike McDonald <mikeraymcdonald@gmail.com>
2020-11-21 17:11:52 -0600

    add rinkeby network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { HardhatUserConfig } from 'hardhat/config';
 import 'hardhat-deploy';
 import '@nomiclabs/hardhat-waffle';
@@ -14,13 +15,19 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       saveDeployments: false,
     },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [
+        `0x${process.env.RINKEBY_PRIVATE_KEY}`
+      ],
+    }
   },
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
       // We use explicit chain IDs so that export-all works correctly: https://github.com/wighawag/hardhat-deploy#options-2
       1: 0, // mainnet
-      4: '', // rinkeby
+      4: 0, // rinkeby
     },
   },
   solidity: {
