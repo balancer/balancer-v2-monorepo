2d70fbab6f7a1c0356caac036d9f220cc209c4fa
johngrantuk <johngrantuk@googlemail.com>
2020-12-11 11:09:36 +0000

    Fixed suggestions. Added default mnemonic.

--- test/helpers/tokens.ts
+++ test/helpers/tokens.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'hardhat';
 import { SignerWithAddress } from 'hardhat-deploy-ethers/dist/src/signer-with-address';
 import { BigNumber, Contract } from 'ethers';
 import { fromPairs, Dictionary } from 'lodash';
@@ -40,4 +41,34 @@ export async function mintTokens(
   amount: number | string | BigNumber
 ): Promise<void> {
   await tokens[symbol].mint(typeof recipient == 'string' ? recipient : recipient.address, amount.toString());
+}
+
+// Deploys multiple tokens and returns a symbol -> token dictionary
+export async function deployTokensNew(admin: string, symbols: Array<string>, decimals: Array<number>): Promise<TokenList> {
+  const tokenSymbols: TokenList = {};
+  // Get artifact for TestToken
+  const Token = await ethers.getContractFactory('TestToken');
+  // Get deployed Token Factory
+  const tokenFactory = await ethers.getContract('TokenFactory');
+  // Find list of tokens already deployed by factory
+  const totalTokens = await tokenFactory.getTotalTokens();
+  const deployedTokens = await tokenFactory.getTokens(0, totalTokens);
+  // For each token deploy if not already deployed
+  for (let i = 0; i < symbols.length; i++) {
+    if (symbols[i] === 'WETH') {
+      const weth = await ethers.getContract('WETH9');
+      tokenSymbols[symbols[i]] = weth;
+      continue;
+    }
+    const address = await tokenFactory.callStatic.create(admin, symbols[i], symbols[i], decimals[i]);
+    if (!deployedTokens.includes(address)) {
+      // TODO reinstate this
+      //const addr = await deployToken(admin, symbols[i], decimals[i]);
+      //if (addr !== address) console.log(`TOKEN DEPLOY ERROR`);
+    }
+    // Get token contract
+    const tokenContract = await Token.attach(address);
+    tokenSymbols[symbols[i]] = tokenContract;
+  }
+  return tokenSymbols;
 }
