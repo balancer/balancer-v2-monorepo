925738dba6d831823dcc2e403b88a6f19ff20693
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-12-18 15:08:23 -0300

    Remove unused scripts

--- scripts/helpers/pools.ts
+++ scripts/helpers/pools.ts
@@ -1,7 +1,5 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import { Contract, ContractReceipt } from 'ethers';
-import { MAX_UINT256 } from '../../test/helpers/constants';
-import { TokenList } from '../../test/helpers/tokens';
 import { ethers } from 'hardhat';
 import { deploy } from './deploy';
 
@@ -9,48 +7,6 @@ export const PairTS = 0;
 export const TupleTS = 1;
 
 export type TradingStrategyType = typeof PairTS | typeof TupleTS;
-
-export async function createPool(
-  vault: Contract,
-  strategy: Contract,
-  strategyType: number,
-  controller: SignerWithAddress
-): Promise<string> {
-  const receipt: ContractReceipt = await (
-    await vault.connect(controller).newPool(strategy.address, strategyType)
-  ).wait();
-
-  const event = receipt.events?.find((e) => e.event == 'PoolCreated');
-  if (event == undefined) {
-    throw new Error('Could not find PoolCreated event');
-  }
-
-  return event.args?.poolId;
-}
-
-export async function setupPool(
-  vault: Contract,
-  strategy: Contract,
-  strategyType: number,
-  tokens: TokenList,
-  controller: SignerWithAddress,
-  makeup: Array<[string, string]>
-): Promise<string> {
-  const poolId = createPool(vault, strategy, strategyType, controller);
-
-  for (const entry of makeup) {
-    const token = tokens[entry[0]];
-    const amount = entry[1];
-
-    await token.mint(controller.address, entry[1]);
-    await token.connect(controller).approve(vault.address, MAX_UINT256);
-
-    // Transfer tokens, don't withdraw from user balance
-    await vault.connect(controller).addLiquidity(poolId, controller.address, [token.address], [amount], [amount]);
-  }
-
-  return poolId;
-}
 
 export type PoolName = 'ConstantProductPool' | 'StablecoinPool';
 
