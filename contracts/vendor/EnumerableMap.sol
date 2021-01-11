b42651cc2d57f32aeaa5e5f1bad30625ce5d5a2f
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-01-11 10:29:57 -0300

    Use same compiler version in all files

--- contracts/vendor/EnumerableMap.sol
+++ contracts/vendor/EnumerableMap.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.7.1;
 
 // Based on the EnumerableSet library from OpenZeppelin contracts, altered to include
 // the following:
