427e033f44a676f3191cb649c345db713fedd085
Greg Taschuk <gregorytaschuk@gmail.com>
2021-01-25 10:35:34 -0800

    removed unused deploy tokens methods (artifact of merge)

--- scripts/helpers/tokens.ts
+++ scripts/helpers/tokens.ts
@@ -6,26 +6,6 @@ import { deploy } from './deploy';
 
 export type TokenList = Dictionary<Contract>;
 
-// Deploys a vanilla ERC20 token that can be minted by any account
-export async function deployTokenFromFactory(
-  ethers: any,
-  admin: string,
-  symbol: string,
-  decimals?: number
-): Promise<string> {
-  // Get deployed Token Factory
-  const tokenFactory = await ethers.getContract('TokenFactory');
-
-  const tx = await tokenFactory.create(admin, symbol, symbol, decimals ?? 18);
-  const receipt = await tx.wait();
-  const event = receipt.events?.find((e: any) => e.event == 'TokenCreated');
-  if (event == undefined) {
-    throw new Error('Could not find TokenCreated event');
-  }
-
-  return event.args.token;
-}
-
 export async function deployToken(
   ethers: any,
   symbol: string,
@@ -36,30 +16,6 @@ export async function deployToken(
   const deployer = from || defaultDeployer;
   const testToken = await deploy('TestToken', { from: deployer, args: [deployer.address, symbol, symbol, decimals] });
   return testToken.address;
-}
-
-// Deploys multiple tokens and returns a symbol -> token dictionary
-export async function deployTokensFromFactory(
-  ethers: any,
-  Token: ContractFactory,
-  tokenFactory: Contract,
-  symbols: Array<string>,
-  decimals: Array<number>,
-  from?: SignerWithAddress
-): Promise<TokenList> {
-  const tokenSymbols: TokenList = {};
-  const totalTokens = await tokenFactory.getTotalTokens();
-  for (let i = 0; i < symbols.length; i++) {
-    if (symbols[i] === 'WETH') {
-      const weth = await ethers.getContract('WETH9');
-      tokenSymbols[symbols[i]] = weth;
-      continue;
-    }
-    const addr = await deployToken(ethers, symbols[i], decimals[i], from);
-    const tokenContract = await Token.attach(addr);
-    tokenSymbols[symbols[i]] = tokenContract;
-  }
-  return tokenSymbols;
 }
 
 // Deploys multiple tokens and returns a symbol -> token dictionary
