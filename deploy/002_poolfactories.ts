9fc5edde1d50bf8b74f201f5cf0ad909043e2737
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-01-26 11:18:24 -0300

    Fix deploy scripts

--- deploy/002_poolfactories.ts
+++ deploy/002_poolfactories.ts
@@ -3,50 +3,24 @@ import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy, execute, read } = deployments;
+  const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
   const vault = await deployments.get('Vault');
 
-  const weightedPoolFactory = await deploy('WeightedPoolFactory', {
+  await deploy('WeightedPoolFactory', {
     from: deployer,
     args: [vault.address],
     log: true,
     deterministicDeployment: true,
   });
 
-  const stablePoolFactory = await deploy('StablePoolFactory', {
+  await deploy('StablePoolFactory', {
     from: deployer,
     args: [vault.address],
     log: true,
     deterministicDeployment: true,
   });
-
-  // The factories need to be granted permission to add universal agents, so that they can create pools
-
-  const addUniversalAgentRole = await read('Authorizer', {}, 'ADD_UNIVERSAL_AGENT_ROLE');
-
-  await execute(
-    'Authorizer',
-    {
-      from: deployer,
-      log: true,
-    },
-    'grantRole',
-    addUniversalAgentRole,
-    weightedPoolFactory.address
-  );
-
-  await execute(
-    'Authorizer',
-    {
-      from: deployer,
-      log: true,
-    },
-    'grantRole',
-    addUniversalAgentRole,
-    stablePoolFactory.address
-  );
 };
 export default func;
