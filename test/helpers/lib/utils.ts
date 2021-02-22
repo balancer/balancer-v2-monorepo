40763031e265fc71309bf5c1c40013df8a3c838d
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-02-22 13:38:11 -0300

    tests: fix helpers warnings

--- test/helpers/lib/utils.ts
+++ test/helpers/lib/utils.ts
@@ -1,12 +1,12 @@
 import { EthereumProvider } from 'hardhat/types';
 
-export async function takeSnapshot(provider: EthereumProvider) {
+export async function takeSnapshot(provider: EthereumProvider): Promise<string> {
   return (await provider.request({
     method: 'evm_snapshot',
   })) as string;
 }
 
-export async function revert(provider: EthereumProvider, snapshotId: string) {
+export async function revert(provider: EthereumProvider, snapshotId: string): Promise<void> {
   await provider.request({
     method: 'evm_revert',
     params: [snapshotId],
@@ -22,7 +22,7 @@ export async function getProvider(provider?: EthereumProvider): Promise<Ethereum
   return hre.network.provider;
 }
 
-export function wrapWithTitle(title: string | undefined, str: string) {
+export function wrapWithTitle(title: string | undefined, str: string): string {
   if (title === undefined) {
     return str;
   }
