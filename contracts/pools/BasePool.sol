e056b5354297c0d0928eb86fbde7547c0052a02a
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2021-03-01 14:12:12 -0300

    vault: ensure two token pools tokens are sorted (#344)

--- contracts/pools/BasePool.sol
+++ contracts/pools/BasePool.sol
@@ -97,12 +97,15 @@ abstract contract BasePool is IBasePool, BalancerPoolToken {
         require(tokens.length >= _MIN_TOKENS, "MIN_TOKENS");
         require(tokens.length <= _MAX_TOKENS, "MAX_TOKENS");
 
-        require(swapFee <= _MAX_SWAP_FEE, "MAX_SWAP_FEE");
-
-        // Because these Pools will register tokens only once, if the tokens array is sorted, then the Pool tokens will
-        // have this same order. We rely on this property to make Pools simpler to write, as it lets us assume that the
+        // The Vault only requires the token list to be ordered for the Two Token Pools specialization. However,
+        // to make the developer experience consistent, we are requiring this condition for all the native pools.
+        // Also, since these Pools will register tokens only once, we can ensure the Pool tokens will follow the same
+        // order. We rely on this property to make Pools simpler to write, as it lets us assume that the
         // order of token-specific parameters (such as token weights) will not change.
         InputHelpers.ensureArrayIsSorted(tokens);
+
+        require(swapFee <= _MAX_SWAP_FEE, "MAX_SWAP_FEE");
+
         bytes32 poolId = vault.registerPool(specialization);
 
         // Pass in zero addresses for Asset Managers
