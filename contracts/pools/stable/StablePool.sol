38e079a6a8feb49f96aadc038674012f79620869
Daniel Fernandez <fernandez.daniel@gmail.com>
2021-03-18 12:05:10 -0300

    Update contracts/pools/stable/StablePool.sol
    Co-authored-by: Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>

--- contracts/pools/stable/StablePool.sol
+++ contracts/pools/stable/StablePool.sol
@@ -412,7 +412,7 @@ contract StablePool is BaseGeneralPool, StableMath {
     }
 
     // This function returns the appreciation of one BPT relative to the
-    // underlying tokens. This starts at 1 when the pool is created and grows over time
+    // underlying tokens. This starts at 1 when the pool is initialized and grows over time
     // It's the equivalent to Curve's get_virtual_price() function
     function getRate() public view override returns (uint256) {
         //TODO: add cache
