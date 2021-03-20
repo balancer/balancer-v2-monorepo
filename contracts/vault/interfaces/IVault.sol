af21747214926ab9ac5ac5462a84445188925f4c
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-03-20 09:25:40 -0300

    Make InternalBalanceChanged delta-based (#401)

--- contracts/vault/interfaces/IVault.sol
+++ contracts/vault/interfaces/IVault.sol
@@ -163,7 +163,7 @@ interface IVault {
      * Because Internal Balance works exclusively with ERC20 tokens, ETH deposits and withdrawals will be reflected here
      * as having used WETH.
      */
-    event InternalBalanceChanged(address indexed user, IERC20 indexed token, uint256 balance);
+    event InternalBalanceChanged(address indexed user, IERC20 indexed token, int256 delta);
 
     // Pools
     //
