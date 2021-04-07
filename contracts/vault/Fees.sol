8df562ded7529704f3950520025f28ff03ae633d
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-04-07 10:15:25 -0300

    vault: small bytecode optimizations

--- contracts/vault/Fees.sol
+++ contracts/vault/Fees.sol
@@ -29,7 +29,7 @@ import "./interfaces/IVault.sol";
 abstract contract Fees is IVault {
     using SafeERC20 for IERC20;
 
-    ProtocolFeesCollector private _protocolFeesCollector;
+    ProtocolFeesCollector private immutable _protocolFeesCollector;
 
     constructor() {
         // Most of the protocol fee logic is not here but in the ProtocolFeesCollector contract. The reason for this is
@@ -38,6 +38,10 @@ abstract contract Fees is IVault {
     }
 
     function getProtocolFeesCollector() external view override returns (ProtocolFeesCollector) {
+        return _getProtocolFeesCollector();
+    }
+
+    function _getProtocolFeesCollector() internal view returns (ProtocolFeesCollector) {
         return _protocolFeesCollector;
     }
 
@@ -45,14 +49,14 @@ abstract contract Fees is IVault {
      * @dev Returns the percentage protocol swap fee.
      */
     function _getProtocolSwapFee() internal view returns (uint256) {
-        return _protocolFeesCollector.getSwapFee();
+        return _getProtocolFeesCollector().getSwapFee();
     }
 
     /**
      * @dev Returns the protocol fee to charge for a flash loan of `amount`.
      */
     function _calculateFlashLoanFee(uint256 amount) internal view returns (uint256) {
-        return _calculateFee(amount, _protocolFeesCollector.getFlashLoanFee());
+        return _calculateFee(amount, _getProtocolFeesCollector().getFlashLoanFee());
     }
 
     function _calculateFee(uint256 amount, uint256 pct) internal pure returns (uint256) {
@@ -63,7 +67,7 @@ abstract contract Fees is IVault {
 
     function _payFee(IERC20 token, uint256 amount) internal {
         if (amount > 0) {
-            token.safeTransfer(address(_protocolFeesCollector), amount);
+            token.safeTransfer(address(_getProtocolFeesCollector()), amount);
         }
     }
 }
