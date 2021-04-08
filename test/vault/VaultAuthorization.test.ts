311bee67b1e41ee1d69d79a03aa5f6a8b502d598
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-04-08 13:25:45 -0300

    improve test

--- test/vault/VaultAuthorization.test.ts
+++ test/vault/VaultAuthorization.test.ts
@@ -2,10 +2,12 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { Contract } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
-import * as expectEvent from '../helpers/expectEvent';
+
 import { deploy } from '../../lib/helpers/deploy';
 import { roleId } from '../../lib/helpers/roles';
+import { MONTH } from '../../lib/helpers/time';
 import { ZERO_ADDRESS } from '../../lib/helpers/constants';
+import * as expectEvent from '../helpers/expectEvent';
 
 describe('VaultAuthorization', function () {
   let authorizer: Contract, vault: Contract;
@@ -35,32 +37,6 @@ describe('VaultAuthorization', function () {
       const vault = await deployVault(ZERO_ADDRESS);
 
       expect(await vault.getAuthorizer()).to.equal(ZERO_ADDRESS);
-    });
-  });
-
-  describe('with relayer', () => {
-    sharedBeforeEach('deploy vault', async () => {
-      vault = await deployVault(authorizer.address);
-    });
-
-    context('change relayer allowance', () => {
-      sharedBeforeEach('grant permission', async () => {
-        const role = roleId(vault, 'manageUserBalance');
-        await authorizer.connect(admin).grantRole(role, relayer.address);
-        // User has to allow relayer
-        await vault.connect(other).changeRelayerAllowance(other.address, relayer.address, true);
-      });
-
-      it('should emit an event when changing relayer allowance', async () => {
-        const receipt = await (
-          await vault.connect(other).changeRelayerAllowance(other.address, relayer.address, false)
-        ).wait();
-        expectEvent.inReceipt(receipt, 'RelayerAllowanceChanged', {
-          relayer: relayer.address,
-          sender: other.address,
-          allowed: false,
-        });
-      });
     });
   });
 
@@ -115,6 +91,102 @@ describe('VaultAuthorization', function () {
     context('when the sender does not have the role to do it', () => {
       it('reverts', async () => {
         await expect(vault.connect(other).changeAuthorizer(other.address)).to.be.revertedWith('SENDER_NOT_ALLOWED');
+      });
+    });
+  });
+
+  describe('change relayer allowance', () => {
+    sharedBeforeEach('deploy vault', async () => {
+      vault = await deployVault(authorizer.address);
+    });
+
+    context('when the sender is the user', () => {
+      const itChangesTheRelayerAllowance = (allowed: boolean) => {
+        it('changes the allowance', async () => {
+          await vault.connect(other).changeRelayerAllowance(other.address, relayer.address, allowed);
+
+          expect(await vault.hasAllowedRelayer(other.address, relayer.address)).to.eq(allowed);
+        });
+
+        it('should emit an event when changing relayer allowance', async () => {
+          const tx = await vault.connect(other).changeRelayerAllowance(other.address, relayer.address, allowed);
+
+          expectEvent.inReceipt(await tx.wait(), 'RelayerAllowanceChanged', {
+            relayer: relayer.address,
+            sender: other.address,
+            allowed,
+          });
+        });
+      };
+
+      context('when the relayer was not allowed', () => {
+        sharedBeforeEach('disallow relayer', async () => {
+          await vault.connect(other).changeRelayerAllowance(other.address, relayer.address, false);
+        });
+
+        itChangesTheRelayerAllowance(true);
+        itChangesTheRelayerAllowance(false);
+      });
+
+      context('when the relayer was allowed', () => {
+        sharedBeforeEach('allow relayer', async () => {
+          await vault.connect(other).changeRelayerAllowance(other.address, relayer.address, true);
+        });
+
+        itChangesTheRelayerAllowance(true);
+        itChangesTheRelayerAllowance(false);
+      });
+    });
+
+    context('when the sender is not the user', () => {
+      it('reverts', async () => {
+        await expect(
+          vault.connect(relayer).changeRelayerAllowance(other.address, relayer.address, true)
+        ).to.be.revertedWith('SENDER_NOT_ALLOWED');
+      });
+    });
+  });
+
+  describe('emergency period', () => {
+    const EMERGENCY_PERIOD = MONTH * 3;
+    const EMERGENCY_PERIOD_CHECK_EXTENSION = MONTH;
+
+    sharedBeforeEach(async () => {
+      authorizer = await deploy('Authorizer', { args: [admin.address] });
+      vault = await deploy('Vault', {
+        args: [authorizer.address, ZERO_ADDRESS, EMERGENCY_PERIOD, EMERGENCY_PERIOD_CHECK_EXTENSION],
+      });
+    });
+
+    context('when the sender is has the role to do it', () => {
+      let role: string;
+
+      sharedBeforeEach('grant permission', async () => {
+        role = roleId(vault, 'setEmergencyPeriod');
+        await authorizer.connect(admin).grantRole(role, admin.address);
+      });
+
+      it('can change the emergency period status', async () => {
+        expect(await authorizer.hasRole(role, admin.address)).to.be.true;
+
+        await vault.connect(admin).setEmergencyPeriod(true);
+
+        const { active } = await vault.getEmergencyPeriod();
+        expect(active).to.be.true;
+      });
+
+      it('can not change the emergency period if the role was revoked', async () => {
+        await authorizer.connect(admin).revokeRole(role, admin.address);
+
+        expect(await authorizer.hasRole(role, admin.address)).to.be.false;
+
+        await expect(vault.connect(admin).setEmergencyPeriod(true)).to.be.revertedWith('SENDER_NOT_ALLOWED');
+      });
+    });
+
+    context('when the sender does not have the role to do it', () => {
+      it('reverts', async () => {
+        await expect(vault.connect(other).setEmergencyPeriod(true)).to.be.revertedWith('SENDER_NOT_ALLOWED');
       });
     });
   });
