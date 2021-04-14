fec937c3ec1f76415e0b5ce55e13c1cdeb38cf90
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-04-13 23:21:19 -0300

    Finish batch tests

--- test/vault/AssetManager.test.ts
+++ test/vault/AssetManager.test.ts
@@ -140,9 +140,10 @@ describe('Vault - asset manager', function () {
         const tokenInitialBalance = bn(200e18);
 
         sharedBeforeEach('register tokens and add liquidity', async () => {
-          await tokens.mint({ to: lp, amount: tokenInitialBalance });
+          await tokens.mint({ to: lp, amount: tokenInitialBalance.mul(5) });
           await tokens.approve({ to: vault, from: [lp, assetManager] });
 
+          // Only manage the first token (DAI)
           const assetManagers = tokens.addresses.map(() => otherAssetManager.address);
           assetManagers[0] = assetManager.address;
 
@@ -619,6 +620,227 @@ describe('Vault - asset manager', function () {
             it('reverts', async () => {
               const ops = [{ kind, poolId, token: tokens.DAI.address, amount: bn(0) }];
               await expect(vault.connect(sender).managePoolBalance(ops)).to.be.revertedWith('SENDER_NOT_ASSET_MANAGER');
+            });
+          });
+        });
+
+        describe('batch', () => {
+          context('with single pool', () => {
+            context('with the same managed token', () => {
+              it('succeeds', async () => {
+                const ops = [
+                  { poolId, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.DEPOSIT, amount: 3, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.UPDATE, amount: 10, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.WITHDRAW, amount: 6, token: tokens.DAI.address },
+                ];
+
+                const totalWithdraw = bn(5 - 3 + 6);
+                const totalManaged = bn(10 + 6); // The update overwrites the prior withdraw and deposit
+
+                const preBalance = await vault.getPoolTokenInfo(poolId, tokens.DAI.address);
+
+                await expectBalanceChange(() => vault.connect(assetManager).managePoolBalance(ops), tokens, [
+                  {
+                    account: vault,
+                    changes: { DAI: totalWithdraw.mul(-1) },
+                  },
+                  {
+                    account: assetManager,
+                    changes: { DAI: totalWithdraw },
+                  },
+                ]);
+
+                const postBalance = await vault.getPoolTokenInfo(poolId, tokens.DAI.address);
+                expect(preBalance.cash.sub(postBalance.cash)).to.equal(totalWithdraw);
+                expect(postBalance.managed).to.equal(totalManaged);
+              });
+            });
+
+            context('with managed and unmanaged tokens', () => {
+              it('reverts', async () => {
+                const ops = [
+                  { poolId, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.DEPOSIT, amount: 3, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.UPDATE, amount: 0, token: tokens.MKR.address }, // Unmanaged
+                  { poolId, kind: OP_KIND.WITHDRAW, amount: 6, token: tokens.DAI.address },
+                ];
+
+                await expect(vault.connect(assetManager).managePoolBalance(ops)).to.be.revertedWith(
+                  'SENDER_NOT_ASSET_MANAGER'
+                );
+              });
+            });
+
+            context('with managed and unregistered tokens', () => {
+              it('reverts', async () => {
+                const ops = [
+                  { poolId, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.DEPOSIT, amount: 3, token: tokens.DAI.address },
+                  { poolId, kind: OP_KIND.UPDATE, amount: 0, token: vault.address }, // Unregistered
+                  { poolId, kind: OP_KIND.WITHDRAW, amount: 6, token: tokens.DAI.address },
+                ];
+
+                await expect(vault.connect(assetManager).managePoolBalance(ops)).to.be.revertedWith(
+                  'TOKEN_NOT_REGISTERED'
+                );
+              });
+            });
+          });
+
+          context('with multiple pools', () => {
+            let poolIdA: string;
+            let poolIdB: string;
+
+            sharedBeforeEach('deploy other pool and add liquidity', async () => {
+              poolIdA = poolId;
+
+              const otherPool = await deploy('MockPool', { args: [vault.address, specialization] });
+              poolIdB = await otherPool.getPoolId();
+
+              // Manage all tokens in Pool B
+              const assetManagers = tokens.addresses.map(() => assetManager.address);
+
+              await otherPool.registerTokens(tokens.addresses, assetManagers);
+
+              await vault.connect(lp).joinPool(poolIdB, lp.address, other.address, {
+                assets: tokens.addresses,
+                maxAmountsIn: tokens.addresses.map(() => MAX_UINT256),
+                fromInternalBalance: false,
+                userData: encodeJoin(
+                  tokens.addresses.map(() => tokenInitialBalance),
+                  tokens.addresses.map(() => 0)
+                ),
+              });
+            });
+
+            context('with the same managed token', () => {
+              it('succeeds', async () => {
+                const ops = [
+                  { poolId: poolIdA, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.UPDATE, amount: 10, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.DAI.address },
+                  { poolId: poolIdA, kind: OP_KIND.DEPOSIT, amount: 2, token: tokens.DAI.address },
+                ];
+
+                const totalWithdrawA = bn(5 - 2);
+                const totalManagedA = bn(5 - 2);
+
+                const totalWithdrawB = bn(7);
+                const totalManagedB = bn(10 + 7);
+
+                const preBalanceA = await vault.getPoolTokenInfo(poolIdA, tokens.DAI.address);
+                const preBalanceB = await vault.getPoolTokenInfo(poolIdB, tokens.DAI.address);
+
+                await expectBalanceChange(() => vault.connect(assetManager).managePoolBalance(ops), tokens, [
+                  {
+                    account: vault,
+                    changes: { DAI: totalWithdrawA.add(totalWithdrawB).mul(-1) },
+                  },
+                  {
+                    account: assetManager,
+                    changes: { DAI: totalWithdrawA.add(totalWithdrawB) },
+                  },
+                ]);
+
+                const postBalanceA = await vault.getPoolTokenInfo(poolIdA, tokens.DAI.address);
+                const postBalanceB = await vault.getPoolTokenInfo(poolIdB, tokens.DAI.address);
+
+                expect(preBalanceA.cash.sub(postBalanceA.cash)).to.equal(totalWithdrawA);
+                expect(postBalanceA.managed).to.equal(totalManagedA);
+
+                expect(preBalanceB.cash.sub(postBalanceB.cash)).to.equal(totalWithdrawB);
+                expect(postBalanceB.managed).to.equal(totalManagedB);
+              });
+            });
+
+            context('with multiple managed tokens', () => {
+              it('succeeds', async () => {
+                const ops = [
+                  { poolId: poolIdA, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.UPDATE, amount: 10, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.MKR.address },
+                  { poolId: poolIdB, kind: OP_KIND.DEPOSIT, amount: 1, token: tokens.MKR.address },
+                  { poolId: poolIdA, kind: OP_KIND.DEPOSIT, amount: 2, token: tokens.DAI.address },
+                ];
+
+                const totalDAIWithdrawA = bn(5 - 2);
+                const totalDAIManagedA = bn(5 - 2);
+
+                const totalDAIWithdrawB = bn(7);
+                const totalDAIManagedB = bn(10 + 7);
+
+                const totalMKRWithdrawB = bn(7 - 1);
+                const totalMKRManagedB = bn(7 - 1);
+
+                const preDAIBalanceA = await vault.getPoolTokenInfo(poolIdA, tokens.DAI.address);
+                const preDAIBalanceB = await vault.getPoolTokenInfo(poolIdB, tokens.DAI.address);
+
+                const preMKRBalanceB = await vault.getPoolTokenInfo(poolIdB, tokens.MKR.address);
+
+                await expectBalanceChange(() => vault.connect(assetManager).managePoolBalance(ops), tokens, [
+                  {
+                    account: vault,
+                    changes: {
+                      DAI: totalDAIWithdrawA.add(totalDAIWithdrawB).mul(-1),
+                      MKR: totalMKRWithdrawB.mul(-1),
+                    },
+                  },
+                  {
+                    account: assetManager,
+                    changes: {
+                      DAI: totalDAIWithdrawA.add(totalDAIWithdrawB),
+                      MKR: totalMKRWithdrawB,
+                    },
+                  },
+                ]);
+
+                const postDAIBalanceA = await vault.getPoolTokenInfo(poolIdA, tokens.DAI.address);
+                const postDAIBalanceB = await vault.getPoolTokenInfo(poolIdB, tokens.DAI.address);
+
+                const postMKRBalanceB = await vault.getPoolTokenInfo(poolIdB, tokens.MKR.address);
+
+                expect(preDAIBalanceA.cash.sub(postDAIBalanceA.cash)).to.equal(totalDAIWithdrawA);
+                expect(postDAIBalanceA.managed).to.equal(totalDAIManagedA);
+
+                expect(preDAIBalanceB.cash.sub(postDAIBalanceB.cash)).to.equal(totalDAIWithdrawB);
+                expect(postDAIBalanceB.managed).to.equal(totalDAIManagedB);
+
+                expect(preMKRBalanceB.cash.sub(postMKRBalanceB.cash)).to.equal(totalMKRWithdrawB);
+                expect(postMKRBalanceB.managed).to.equal(totalMKRManagedB);
+              });
+            });
+
+            context('with managed and unmanaged tokens', () => {
+              it('reverts', async () => {
+                const ops = [
+                  { poolId: poolIdA, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId: poolIdA, kind: OP_KIND.UPDATE, amount: 10, token: tokens.MKR.address }, // Unmanaged
+                  { poolId: poolIdB, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.MKR.address },
+                  { poolId: poolIdA, kind: OP_KIND.DEPOSIT, amount: 2, token: tokens.DAI.address },
+                ];
+
+                await expect(vault.connect(assetManager).managePoolBalance(ops)).to.be.revertedWith(
+                  'SENDER_NOT_ASSET_MANAGER'
+                );
+              });
+            });
+
+            context('with registered and unregistered pools', () => {
+              it('reverts', async () => {
+                const invalidPoolId = '0x1234123412341234123412341234123412341234123412341234123412341234';
+                const ops = [
+                  { poolId: poolIdA, kind: OP_KIND.WITHDRAW, amount: 5, token: tokens.DAI.address },
+                  { poolId: poolIdB, kind: OP_KIND.UPDATE, amount: 10, token: tokens.MKR.address },
+                  { poolId: poolIdB, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.MKR.address },
+                  { poolId: invalidPoolId, kind: OP_KIND.WITHDRAW, amount: 7, token: tokens.MKR.address }, // Invalid
+                  { poolId: poolIdA, kind: OP_KIND.DEPOSIT, amount: 2, token: tokens.DAI.address },
+                ];
+
+                await expect(vault.connect(assetManager).managePoolBalance(ops)).to.be.revertedWith('INVALID_POOL_ID');
+              });
             });
           });
         });
