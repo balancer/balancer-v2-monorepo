726bcc52aa1432f4d8a8d841c93b546cf75348d5
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-04-15 20:12:21 -0300

    Fix typos

--- contracts/vault/interfaces/IFlashLoanRecipient.sol
+++ contracts/vault/interfaces/IFlashLoanRecipient.sol
@@ -20,10 +20,10 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IFlashLoanRecipient {
     /**
-     * @dev Called by the Vault on the recipient of flash loans.
+     * @dev When `flashLoan` is called on the Vault, it invokes the `receiveFlashLoan` hook on the recipient.
      *
      * At the time of the call, the Vault will have transferred `amounts` for `tokens` to the recipient. Before this
-     * call returns, the recipient must have transferred back `amounts` plus `feeAmounts` for each token back to the
+     * call returns, the recipient must have transferred `amounts` plus `feeAmounts` for each token back to the
      * Vault, or else the entire flash loan will revert.
      *
      * `userData` is the same value passed in the `IVault.flashLoan` call.
