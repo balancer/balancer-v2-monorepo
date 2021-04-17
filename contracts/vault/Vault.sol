ebb4b20f0655c0e27bd73183452f40cb2bd37d61
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-04-17 14:38:39 -0300

    Update Vault docs

--- contracts/vault/Vault.sol
+++ contracts/vault/Vault.sol
@@ -24,19 +24,24 @@ import "./Swaps.sol";
 
 /**
  * @dev The `Vault` is Balancer V2's core contract. A single instance of it exists for the entire network, and it is the
- * entity used to interact with Pools by joining, exiting, or swapping with them.
+ * entity used to interact with Pools by Liquidity Providers who join and exit them, Traders who swap, and Asset
+ * Managers who withdraw and deposit tokens.
  *
  * The `Vault`'s source code is split among a number of sub-contracts, with the goal of improving readability and making
- * understanding the system easier. All sub-contracts have been marked as `abstract` to explicitly indicate that only
+ * understanding the system easier. Most sub-contracts have been marked as `abstract` to explicitly indicate that only
  * the full `Vault` is meant to be deployed.
  *
  * Roughly speaking, these are the contents of each sub-contract:
  *
- *  - `UserBalance`: manage user balances (Internal Balance operations and external balance transfers)
+ *  - `AssetManagers`: Pool token Asset Manager registry, and Asset Manager interactions.
  *  - `Fees`: set and compute protocol fees.
  *  - `FlashLoans`: flash loan transfers and fees.
- *  - `PoolRegistry`: Pool registration, joining, exiting, and Asset Manager interactions.
+ *  - `PoolBalances`: Pool joins and exits.
+ *  - `PoolRegistry`: Pool registration, ID management, and basic queries.
+ *  - `PoolTokens`: Pool token registration and registration, and balance queries.
  *  - `Swaps`: Pool swaps.
+ *  - `UserBalance`: manage user balances (Internal Balance operations and external balance transfers)
+ *  - `VaultAuthorization`: access control, relayers and signature validation.
  *
  * Additionally, the different Pool specializations are handled by the `GeneralPoolsBalance`,
  * `MinimalSwapInfoPoolsBalance` and `TwoTokenPoolsBalance` sub-contracts, which in turn make use of the
@@ -46,11 +51,11 @@ import "./Swaps.sol";
  * multitude of design decisions, from minor things like the format used to store Pool IDs, to major features such as
  * the different Pool specialization settings.
  *
- * Finally, the large number of tasks carried out by the Vault means its bytecode is very large, which exceeds
+ * Finally, the large number of tasks carried out by the Vault means its bytecode is very large, close to exceeding
  * the contract size limit imposed by EIP 170 (https://eips.ethereum.org/EIPS/eip-170). Manual tuning of the source code
  * was required to improve code generation and bring the bytecode size below this limit. This includes extensive
- * utilization of `internal` functions (particularly inside modifiers), usage of named return arguments, and dedicated
- * storage access methods, to name a few.
+ * utilization of `internal` functions (particularly inside modifiers), usage of named return arguments, dedicated
+ * storage access methods, dynamic revert reason generation, and usage of inline assembly, to name a few.
  */
 contract Vault is VaultAuthorization, FlashLoans, Swaps {
     constructor(
