7ab3ee3b7ccd49053a42ca19d1ca5b436f3c55e7
Tom French <tom@tomfren.ch>
2021-05-24 15:33:04 +0100

    test: add helper function to calculate fees

--- pkg/asset-manager-utils/test/RewardsAssetManager.test.ts
+++ pkg/asset-manager-utils/test/RewardsAssetManager.test.ts
@@ -18,6 +18,29 @@ import * as expectEvent from '@balancer-labs/v2-helpers/src/test/expectEvent';
 const OVER_INVESTMENT_REVERT_REASON = 'investment amount exceeds target';
 const UNDER_INVESTMENT_REVERT_REASON = 'withdrawal leaves insufficient balance invested';
 
+type PoolConfig = {
+  targetPercentage: BigNumber;
+  upperCriticalPercentage: BigNumber;
+  lowerCriticalPercentage: BigNumber;
+  feePercentage: BigNumber;
+};
+
+const calcRebalanceFee = (poolCash: BigNumber, poolManaged: BigNumber, config: PoolConfig): BigNumber => {
+  const poolAssets = poolCash.add(poolManaged);
+  const percentageInvested = poolManaged.mul(fp(1)).div(poolAssets);
+
+  if (percentageInvested.gt(config.upperCriticalPercentage)) {
+    const upperCriticalBalance = poolAssets.mul(config.upperCriticalPercentage).div(fp(1));
+    return poolManaged.sub(upperCriticalBalance).mul(config.feePercentage).div(fp(1));
+  }
+
+  if (percentageInvested.lt(config.lowerCriticalPercentage)) {
+    const lowerCriticalBalance = poolAssets.mul(config.lowerCriticalPercentage).div(fp(1));
+    return lowerCriticalBalance.sub(poolManaged).mul(config.feePercentage).div(fp(1));
+  }
+  return BigNumber.from(0);
+};
+
 const tokenInitialBalance = bn(200e18);
 const amount = bn(100e18);
 
