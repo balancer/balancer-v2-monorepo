17d93c9550fd6e31d15c3bf567cd21d3356cadc3
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-06-11 17:51:24 -0300

    Remove unused tests

--- pkg/pool-utils/test/factories/BasePoolSplitFactory.test.ts
+++ pkg/pool-utils/test/factories/BasePoolSplitFactory.test.ts
@@ -1,20 +1,13 @@
 import { Contract } from 'ethers';
 
-import * as expectEvent from '@balancer-labs/v2-helpers/src/test/expectEvent';
 import { deploy, getArtifact } from '@balancer-labs/v2-helpers/src/contract';
 import { ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
 import { expect } from 'chai';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { ethers } from 'hardhat';
 
 describe('BasePoolSplitFactory', function () {
   let vault: Contract;
   let factory: Contract;
-  let other: SignerWithAddress;
-
-  before('setup signers', async () => {
-    [, other] = await ethers.getSigners();
-  });
 
   sharedBeforeEach(async () => {
     vault = await deploy('v2-vault/Vault', { args: [ZERO_ADDRESS, ZERO_ADDRESS, 0, 0] });
@@ -40,29 +33,5 @@ describe('BasePoolSplitFactory', function () {
     const poolCreationCode = await factory.getPoolCreationCode();
 
     expect(poolCreationCode).to.equal(artifact.bytecode);
-  });
-
-  it('creates a pool', async () => {
-    const receipt = await (await factory.create()).wait();
-    expectEvent.inReceipt(receipt, 'PoolCreated');
-  });
-
-  context('with a created pool', () => {
-    let pool: string;
-
-    sharedBeforeEach('deploy pool', async () => {
-      const receipt = await (await factory.create()).wait();
-      const event = expectEvent.inReceipt(receipt, 'PoolCreated');
-
-      pool = event.args.pool;
-    });
-
-    it('tracks pools created by the factory', async () => {
-      expect(await factory.isPoolFromFactory(pool)).to.be.true;
-    });
-
-    it('does not track pools that were not created by the factory', async () => {
-      expect(await factory.isPoolFromFactory(other.address)).to.be.false;
-    });
   });
 });
