b94c469739b74ca5dcd7039489a03ba390a60e09
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-06-24 13:06:04 -0300

    deployments: fix hardhat deploy command

--- pkg/deployments/hardhat.config.ts
+++ pkg/deployments/hardhat.config.ts
@@ -8,8 +8,8 @@ import Task from './src/task';
 import { Logger } from './src/logger';
 
 task('deploy', 'Run deployment task')
-  .addParam('task', 'Deployment task ID')
-  .addOptionalParam('force', 'Whether the task to deploy must be ignore previous deployments or not')
+  .addParam('id', 'Deployment task ID')
+  .addOptionalParam('force', 'Ignore previous deployments')
   .setAction(async (args: { id: string; force?: boolean; verbose?: boolean }, hre: HardhatRuntimeEnvironment) => {
     Logger.setDefaults(false, args.verbose || false);
     await new Task(args.id, hre.network.name).run(args.force);
