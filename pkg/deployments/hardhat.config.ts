e29c13a6236bc7ea9c38f73bbb23920ae391f9eb
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2021-06-24 16:26:00 -0300

    Deployments: Set up new layout (#648)
    * deployments: set up new layout
    
    * Deployments: Implement tasks model (#649)
    
    * deployments: implement tasks model
    
    * task: small fix
    
    * deployments: remove local deploy from CI
    
    * deployments: fix hardhat deploy command
    
    * deployments: fix vault input
    
    * deployments: fix linter

--- pkg/deployments/hardhat.config.ts
+++ pkg/deployments/hardhat.config.ts
@@ -1,144 +1,18 @@
-import 'dotenv/config';
-import '@tenderly/hardhat-tenderly';
-import 'hardhat-deploy';
-import 'hardhat-local-networks-config-plugin';
 import '@nomiclabs/hardhat-ethers';
-
-import { hardhatBaseConfig } from '@balancer-labs/v2-common';
-import { name } from './package.json';
+import 'hardhat-local-networks-config-plugin';
 
 import { task } from 'hardhat/config';
-import { TASK_DEPLOY } from 'hardhat-deploy';
-import { exec } from 'child_process';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
-task(TASK_DEPLOY).setAction(async (_, hre, runSuper) => {
-  // Hardhat deploy doesn't support loading artifacts from multiple locations, so we manually copy them here. However,
-  // Hardhat compilation deletes old artifacts, so we need to do this _after_ compiling local contracts. The solution is
-  // to manually compile, then copy, then deploy with the --no-compile flag set.
-  await hre.run('compile');
+import Task from './src/task';
+import { Logger } from './src/logger';
 
-  exec(
-    'mkdir -p artifacts && cp -R ../solidity-utils/artifacts/* ../vault/artifacts/* ../pool-weighted/artifacts/* ../pool-stable/artifacts/* ../standalone-utils/artifacts/* artifacts'
-  );
+task('deploy', 'Run deployment task')
+  .addParam('id', 'Deployment task ID')
+  .addOptionalParam('force', 'Ignore previous deployments')
+  .setAction(async (args: { id: string; force?: boolean; verbose?: boolean }, hre: HardhatRuntimeEnvironment) => {
+    Logger.setDefaults(false, args.verbose || false);
+    await new Task(args.id, hre.network.name).run(args.force);
+  });
 
-  return runSuper({ noCompile: true });
-});
-
-const CHAIN_IDS = {
-  hardhat: 31337,
-  kovan: 42,
-  goerli: 5,
-  mainnet: 1,
-  polygon: 137,
-  polygonMumbai: 80001,
-  rinkeby: 4,
-  ropsten: 3,
-  dockerParity: 17,
-};
-
-const INFURA_KEY = process.env.INFURA_KEY || '';
-const DEPLOYER_PRIVATE_KEY =
-  process.env.DEPLOYER_PRIVATE_KEY || '0000000000000000000000000000000000000000000000000000000000000000';
-
-const CONTROLLER_PRIVATE_KEY =
-  process.env.CONTROLLER_PRIVATE_KEY || '0000000000000000000000000000000000000000000000000000000000000000';
-
-export default {
-  networks: {
-    hardhat: {
-      chainId: CHAIN_IDS.hardhat,
-      saveDeployments: true,
-    },
-    localhost: {
-      saveDeployments: true,
-    },
-    mainnet: {
-      chainId: CHAIN_IDS.mainnet,
-      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-    ropsten: {
-      chainId: CHAIN_IDS.ropsten,
-      url: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-    kovan: {
-      chainId: CHAIN_IDS.kovan,
-      url: `https://kovan.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-    rinkeby: {
-      chainId: CHAIN_IDS.rinkeby,
-      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-    goerli: {
-      chainId: CHAIN_IDS.goerli,
-      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-    polygon: {
-      chainId: CHAIN_IDS.polygon,
-      url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-    polygonMumbai: {
-      chainId: CHAIN_IDS.polygonMumbai,
-      url: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`, `0x${CONTROLLER_PRIVATE_KEY}`], // Using private key instead of mnemonic for vanity deploy
-      saveDeployments: true,
-    },
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-      [CHAIN_IDS.mainnet]: 0,
-      [CHAIN_IDS.kovan]: 0,
-      [CHAIN_IDS.ropsten]: 0,
-      [CHAIN_IDS.goerli]: 0,
-      [CHAIN_IDS.rinkeby]: 0,
-      [CHAIN_IDS.polygon]: 0,
-      [CHAIN_IDS.polygonMumbai]: 0,
-      [CHAIN_IDS.dockerParity]: 0,
-    },
-    admin: {
-      default: 1, // here this will by default take the first account as deployer
-      // We use explicit chain IDs so that export-all works correctly: https://github.com/wighawag/hardhat-deploy#options-2
-      [CHAIN_IDS.mainnet]: '0x10A19e7eE7d7F8a52822f6817de8ea18204F2e4f',
-      [CHAIN_IDS.kovan]: 1,
-      [CHAIN_IDS.ropsten]: 1,
-      [CHAIN_IDS.goerli]: 1,
-      [CHAIN_IDS.rinkeby]: '0x44DDF1D6292F36B25230a72aBdc7159D37d317Cf',
-      [CHAIN_IDS.polygon]: 1,
-      [CHAIN_IDS.polygonMumbai]: 1,
-      [CHAIN_IDS.dockerParity]: 1,
-    },
-  },
-  solidity: {
-    compilers: hardhatBaseConfig.compilers,
-    overrides: { ...hardhatBaseConfig.overrides(name) },
-  },
-  tenderly: {
-    username: 'balancer',
-    project: 'v2',
-  },
-
-  // Hardhat deploy config
-  paths: {
-    deploy: 'migrations',
-    deployments: 'output',
-  },
-  external: {
-    contracts: [
-      {
-        artifacts: 'artifacts',
-      },
-    ],
-  },
-};
+export default {};
