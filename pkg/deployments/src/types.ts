3b8fec9ab52c3a54ac2b3a4c966a99ccc48b10e3
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2021-06-24 13:04:21 -0300

    Deployments: Implement tasks model (#649)
    * deployments: implement tasks model
    
    * task: small fix

---
+++ pkg/deployments/src/types.ts
@@ -0,0 +1,38 @@
+import { Contract, BigNumber } from 'ethers';
+
+import Task from './task';
+
+export const NETWORKS = ['goerli', 'kovan', 'mainnet', 'rinkeby', 'ropsten'];
+
+export type Network = typeof NETWORKS[number];
+
+export type NAry<T> = T | Array<T>;
+
+export type Param = boolean | string | number | BigNumber | any;
+
+export type Input = {
+  [key: string]: NAry<Param>;
+};
+
+export type RawInputByNetwork = {
+  [key in Network]: RawInputKeyValue;
+};
+
+export type RawInputKeyValue = {
+  [key: string]: NAry<Param> | Output | Task;
+};
+
+export type RawInput = RawInputKeyValue | RawInputByNetwork;
+
+export type Output = {
+  [key: string]: string;
+};
+
+export type RawOutput = {
+  [key: string]: string | Contract;
+};
+
+export type Artifact = {
+  abi: { [key: string]: string };
+  bytecode: string;
+};
