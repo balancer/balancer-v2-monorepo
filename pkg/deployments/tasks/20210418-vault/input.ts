a7b89323ae63cbedb0cea4e349f0361c791198bb
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-06-24 13:12:24 -0300

    deployments: fix vault input

--- pkg/deployments/tasks/20210418-vault/input.ts
+++ pkg/deployments/tasks/20210418-vault/input.ts
@@ -1,4 +1,5 @@
 import Task from '../../src/task';
+import { MONTH } from '@balancer-labs/v2-helpers/src/time';
 
 export type VaultDeployment = {
   authorizer: string;
@@ -10,8 +11,34 @@ export type VaultDeployment = {
 const authorizer = new Task('20210418-authorizer');
 
 export default {
-  authorizer,
-  weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-  pauseWindowDuration: 7776000,
-  bufferPeriodDuration: 2592000,
+  goerli: {
+    authorizer,
+    weth: '0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1',
+    pauseWindowDuration: 3 * MONTH,
+    bufferPeriodDuration: MONTH,
+  },
+  kovan: {
+    authorizer,
+    weth: '0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1',
+    pauseWindowDuration: 3 * MONTH,
+    bufferPeriodDuration: MONTH,
+  },
+  mainnet: {
+    authorizer,
+    weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    pauseWindowDuration: 3 * MONTH,
+    bufferPeriodDuration: MONTH,
+  },
+  rinkeby: {
+    authorizer,
+    weth: '0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1',
+    pauseWindowDuration: 3 * MONTH,
+    bufferPeriodDuration: MONTH,
+  },
+  ropsten: {
+    authorizer,
+    weth: '0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1',
+    pauseWindowDuration: 3 * MONTH,
+    bufferPeriodDuration: MONTH,
+  },
 };
