48d9064ce7d90a349d79b37947160ef024982f4d
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2021-06-28 11:27:48 -0300

    fix linter

--- pkg/asset-manager-utils/test/RebalancingRelayer.test.ts
+++ pkg/asset-manager-utils/test/RebalancingRelayer.test.ts
@@ -17,7 +17,7 @@ import {
   encodeJoinWeightedPool,
 } from '@balancer-labs/v2-helpers/src/models/pools/weighted/encoding';
 
-describe.only('RebalancingRelayer', function () {
+describe('RebalancingRelayer', function () {
   let poolId: string, tokens: TokenList;
   let sender: SignerWithAddress, recipient: SignerWithAddress, admin: SignerWithAddress;
   let vault: Contract, authorizer: Contract, relayer: Contract, pool: Contract, assetManagers: Contract[];
@@ -305,7 +305,7 @@ describe.only('RebalancingRelayer', function () {
       });
     });
 
-    context.only('when going through the vault', () => {
+    context('when going through the vault', () => {
       sharedBeforeEach('join and mock asset manager', async () => {
         await vault.connect(sender).joinPool(poolId, sender.address, sender.address, joinRequest);
         await assetManagers[0].mockShouldRebalance(true);
