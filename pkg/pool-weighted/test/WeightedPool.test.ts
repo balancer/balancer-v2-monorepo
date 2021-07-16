13f81cc7a77173febcc0b1f37e40949b1ebcac08
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-07-16 17:35:21 -0300

    Add LBP tests

--- pkg/pool-weighted/test/WeightedPool.test.ts
+++ pkg/pool-weighted/test/WeightedPool.test.ts
@@ -18,33 +18,35 @@ describe('WeightedPool', function () {
     allTokens = await TokenList.create(MAX_TOKENS, { sorted: true, varyDecimals: true });
   });
 
-  for (const numTokens of range(2, MAX_TOKENS + 1)) {
-    context(`with ${numTokens} tokens`, () => {
-      let pool: WeightedPool;
-      let tokens: TokenList;
+  describe('weights and scaling factors', () => {
+    for (const numTokens of range(2, MAX_TOKENS + 1)) {
+      context(`with ${numTokens} tokens`, () => {
+        let pool: WeightedPool;
+        let tokens: TokenList;
 
-      sharedBeforeEach('deploy pool', async () => {
-        tokens = allTokens.subset(numTokens);
+        sharedBeforeEach('deploy pool', async () => {
+          tokens = allTokens.subset(numTokens);
 
-        pool = await WeightedPool.create({
-          tokens,
-          weights: WEIGHTS.slice(0, numTokens),
-          swapFeePercentage: POOL_SWAP_FEE_PERCENTAGE,
+          pool = await WeightedPool.create({
+            tokens,
+            weights: WEIGHTS.slice(0, numTokens),
+            swapFeePercentage: POOL_SWAP_FEE_PERCENTAGE,
+          });
+        });
+
+        it('sets token weights', async () => {
+          const normalizedWeights = await pool.getNormalizedWeights();
+
+          expect(normalizedWeights).to.deep.equal(pool.normalizedWeights);
+        });
+
+        it('sets scaling factors', async () => {
+          const poolScalingFactors = await pool.getScalingFactors();
+          const tokenScalingFactors = tokens.map((token) => fp(10 ** (18 - token.decimals)));
+
+          expect(poolScalingFactors).to.deep.equal(tokenScalingFactors);
         });
       });
-
-      it('sets token weights', async () => {
-        const normalizedWeights = await pool.getNormalizedWeights();
-
-        expect(normalizedWeights).to.deep.equal(pool.normalizedWeights);
-      });
-
-      it('sets scaling factors', async () => {
-        const poolScalingFactors = await pool.getScalingFactors();
-        const tokenScalingFactors = tokens.map((token) => fp(10 ** (18 - token.decimals)));
-
-        expect(poolScalingFactors).to.deep.equal(tokenScalingFactors);
-      });
-    });
-  }
+    }
+  });
 });
