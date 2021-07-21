73b73c5f71b3a4dd1aaa69c941255629d4b64ee8
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-07-21 12:54:44 -0300

    Add LDO Merkle deployment (#744)
    * Approve typo fix
    
    * Prevent reentrancy
    
    * Create LDO merkle deploy task
    
    * Add basic readme
    
    * added deploy artifacts for LDO merkle redeem contract
    
    * Remove extra file
    
    * Restore compile task
    
    Co-authored-by: Greg Taschuk <gregorytaschuk@gmail.com>

--- pkg/distributors/contracts/MerkleRedeem.sol
+++ pkg/distributors/contracts/MerkleRedeem.sol
@@ -43,7 +43,7 @@ contract MerkleRedeem is IDistributor, Ownable {
     constructor(IVault _vault, IERC20 _rewardToken) {
         vault = _vault;
         rewardToken = _rewardToken;
-        IERC20(_rewardToken).approve(address(_vault), type(uint256).max);
+        _rewardToken.approve(address(_vault), type(uint256).max);
     }
 
     function _disburse(address recipient, uint256 balance) private {
@@ -196,8 +196,8 @@ contract MerkleRedeem is IDistributor, Ownable {
         uint256 amount
     ) external onlyOwner {
         require(weekMerkleRoots[week] == bytes32(0), "cannot rewrite merkle root");
-        rewardToken.safeTransferFrom(msg.sender, address(this), amount);
         weekMerkleRoots[week] = _merkleRoot;
+        rewardToken.safeTransferFrom(msg.sender, address(this), amount);
         emit RewardAdded(address(rewardToken), amount);
     }
 }
