8d2e0b306d9340b0e717c0e0f8d6dde006b40b21
Tom French <tom@tomfren.ch>
2021-08-11 15:10:40 +0100

    style: prettier fix

--- pkg/standalone-utils/contracts/LidoRelayer.sol
+++ pkg/standalone-utils/contracts/LidoRelayer.sol
@@ -30,7 +30,7 @@ import "./interfaces/IwstETH.sol";
  * @dev This relayer allows users to use stETH on Balancer without needing to wrap separately.
  *      Users may atomically wrap stETH into wstETH (and vice versa) while performing
  *      swaps, joins and exits on the Vault.
- *  
+ *
  *      The functions of this relayer are designed to match the interface of the underlying Vault equivalent.
  *      For more documentation, reference the Balancer Vault interface:
  *      https://github.com/balancer-labs/balancer-v2-monorepo/blob/4233f67035223fe5e7cf079624b9044dafe6e98f/pkg/vault/contracts/interfaces/IVault.sol
