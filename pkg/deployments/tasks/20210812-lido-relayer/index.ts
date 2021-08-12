bc408acf767b7db2c2f8ceecdc0a05733b62a885
Tom French <tom@tomfren.ch>
2021-08-12 19:03:03 +0100

    refactor: update lido relayer task to use deployAndVerify helper

--- pkg/deployments/tasks/20210812-lido-relayer/index.ts
+++ pkg/deployments/tasks/20210812-lido-relayer/index.ts
@@ -1,5 +1,4 @@
 import Task from '../../src/task';
-import logger from '../../src/logger';
 import { TaskRunOptions } from '../../src/types';
 import { LidoRelayerDeployment } from './input';
 
@@ -10,26 +9,11 @@ const wstETHMap: Record<string, string> = {
 };
 
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
-  const output = task.output({ ensure: false });
   const input = task.input() as LidoRelayerDeployment;
 
   const rateProviderArgs = [wstETHMap[task.network]];
-  if (force || !output.wstETHRateProvider) {
-    const wstETHRateProvider = await task.deploy('WstETHRateProvider', rateProviderArgs, from);
-    task.save({ wstETHRateProvider });
-    await task.verify('WstETHRateProvider', wstETHRateProvider.address, rateProviderArgs);
-  } else {
-    logger.info(`WstETHRateProvider already deployed at ${output.wstETHRateProvider}`);
-    await task.verify('WstETHRateProvider', output.wstETHRateProvider, rateProviderArgs);
-  }
+  task.deployAndVerify('WstETHRateProvider', rateProviderArgs, from, force);
 
   const relayerArgs = [input.vault, wstETHMap[task.network]];
-  if (force || !output.lidoRelayer) {
-    const lidoRelayer = await task.deploy('LidoRelayer', relayerArgs, from);
-    task.save({ lidoRelayer });
-    await task.verify('LidoRelayer', lidoRelayer.address, relayerArgs);
-  } else {
-    logger.info(`LidoRelayer already deployed at ${output.lidoRelayer}`);
-    await task.verify('LidoRelayer', output.lidoRelayer, relayerArgs);
-  }
+  task.deployAndVerify('LidoRelayer', relayerArgs, from, force);
 };
