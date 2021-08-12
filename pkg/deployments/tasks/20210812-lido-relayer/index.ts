a03f1fb405ac1192195ed996349f3ade78355c46
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-08-12 15:49:10 -0300

    refactor and remove wst from lido deployment

--- pkg/deployments/tasks/20210812-lido-relayer/index.ts
+++ pkg/deployments/tasks/20210812-lido-relayer/index.ts
@@ -2,18 +2,9 @@ import Task from '../../src/task';
 import { TaskRunOptions } from '../../src/types';
 import { LidoRelayerDeployment } from './input';
 
-const wstETHMap: Record<string, string> = {
-  mainnet: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
-  goerli: '0x6320cd32aa674d2898a68ec82e869385fc5f7e2f',
-  kovan: '0xA387B91e393cFB9356A460370842BC8dBB2F29aF',
-};
-
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as LidoRelayerDeployment;
 
-  const rateProviderArgs = [wstETHMap[task.network]];
-  task.deployAndVerify('WstETHRateProvider', rateProviderArgs, from, force);
-
-  const relayerArgs = [input.vault, wstETHMap[task.network]];
+  const relayerArgs = [input.vault, input.wstETH];
   task.deployAndVerify('LidoRelayer', relayerArgs, from, force);
 };
--- pkg/deployments/tasks/20210812-lido-relayer/input.ts
+++ pkg/deployments/tasks/20210812-lido-relayer/input.ts
@@ -2,10 +2,22 @@ import Task from '../../src/task';
 
 export type LidoRelayerDeployment = {
   vault: string;
+  wstETH: string;
 };
 
 const vault = new Task('20210418-vault');
 
 export default {
-  vault,
+  mainnet: {
+    vault,
+    wstETH: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
+  },
+  goerli: {
+    vault,
+    wstETH: '0x6320cd32aa674d2898a68ec82e869385fc5f7e2f',
+  },
+  kovan: {
+    vault,
+    wstETH: '0xA387B91e393cFB9356A460370842BC8dBB2F29aF',
+  },
 };
