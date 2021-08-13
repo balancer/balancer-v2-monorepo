afb5b2c1fb021f61915d37467aebe64f2db6f6b1
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-08-13 01:18:23 -0300

    Deploy Lido Relayer and WstETH RateProvider

--- pkg/deployments/tasks/20210812-lido-relayer/input.ts
+++ pkg/deployments/tasks/20210812-lido-relayer/input.ts
@@ -1,23 +1,19 @@
 import Task from '../../src/task';
 
 export type LidoRelayerDeployment = {
-  vault: string;
+  Vault: string;
   wstETH: string;
 };
 
-const vault = new Task('20210418-vault');
+const Vault = new Task('20210418-vault');
 
 export default {
   mainnet: {
-    vault,
+    Vault,
     wstETH: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
   },
-  goerli: {
-    vault,
-    wstETH: '0x6320cd32aa674d2898a68ec82e869385fc5f7e2f',
-  },
   kovan: {
-    vault,
+    Vault,
     wstETH: '0xA387B91e393cFB9356A460370842BC8dBB2F29aF',
   },
 };
