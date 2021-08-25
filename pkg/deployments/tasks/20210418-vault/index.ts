12c7c7363af616e7f7a778710ba391da46eed34c
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-08-25 15:09:25 -0300

    Add BalancerHelpers to Arbitrum deployment (#799)
    * Update vault task
    
    * Deploy BalancerHelpers to Arbitrum
    
    * Deployments package hotfix
    
    Co-authored-by: rabmarut <rabmarut@protonmail.com>

--- pkg/deployments/tasks/20210418-vault/index.ts
+++ pkg/deployments/tasks/20210418-vault/index.ts
@@ -4,6 +4,10 @@ import { TaskRunOptions } from '../../src/types';
 
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as VaultDeployment;
-  const args = [input.Authorizer, input.weth, input.pauseWindowDuration, input.bufferPeriodDuration];
-  await task.deployAndVerify('Vault', args, from, force);
+  const vaultArgs = [input.Authorizer, input.weth, input.pauseWindowDuration, input.bufferPeriodDuration];
+  await task.deployAndVerify('Vault', vaultArgs, from, force);
+
+  const vault = await task.deployedInstance('Vault');
+  const helpersArgs = [vault.address];
+  await task.deployAndVerify('BalancerHelpers', helpersArgs, from, force);
 };
