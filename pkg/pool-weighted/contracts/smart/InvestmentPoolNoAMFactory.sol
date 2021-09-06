5f2288e23e2621c52a728c7e6ec1222212f26343
Jeffrey Bennett <endymionjkb@gmail.com>
2021-09-06 19:57:04 -0400

    Add a factory for Investment Pools that doesn't allow asset managers

---
+++ pkg/pool-weighted/contracts/smart/InvestmentPoolNoAMFactory.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
+
+import "@balancer-labs/v2-pool-utils/contracts/factories/BasePoolSplitCodeFactory.sol";
+import "@balancer-labs/v2-pool-utils/contracts/factories/FactoryWidePauseWindow.sol";
+
+import "./InvestmentPool.sol";
+
+contract InvestmentPoolNoAMFactory is BasePoolSplitCodeFactory, FactoryWidePauseWindow {
+    constructor(IVault vault) BasePoolSplitCodeFactory(vault, type(InvestmentPool).creationCode) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /**
+     * @dev Deploys a new `InvestmentPool` with no asset managers.
+     */
+    function create(
+        string memory name,
+        string memory symbol,
+        IERC20[] memory tokens,
+        uint256[] memory weights,
+        uint256 swapFeePercentage,
+        address owner,
+        bool swapEnabledOnStart,
+        uint256 managementSwapFeePercentage
+    ) external returns (address) {
+        (uint256 pauseWindowDuration, uint256 bufferPeriodDuration) = getPauseConfiguration();
+
+        return
+            _create(
+                abi.encode(
+                    InvestmentPool.NewPoolParams({
+                        vault: getVault(),
+                        name: name,
+                        symbol: symbol,
+                        tokens: tokens,
+                        normalizedWeights: weights,
+                        assetManagers: new address[](tokens.length), // Don't allow asset managers,
+                        swapFeePercentage: swapFeePercentage,
+                        pauseWindowDuration: pauseWindowDuration,
+                        bufferPeriodDuration: bufferPeriodDuration,
+                        owner: owner,
+                        swapEnabledOnStart: swapEnabledOnStart,
+                        managementSwapFeePercentage: managementSwapFeePercentage
+                    })
+                )
+            );
+    }
+}
