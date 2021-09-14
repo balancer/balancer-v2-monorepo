702fa99025c89bbac6be1546cd7a3860c85ac99d
Tom French <tom@tomfren.ch>
2021-09-15 00:20:58 +0100

    feat: update benchmark to use bitmap gas savings

--- pvt/benchmarks/merkleClaim.ts
+++ pvt/benchmarks/merkleClaim.ts
@@ -3,24 +3,29 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-wit
 
 import { TokenList } from '@balancer-labs/v2-helpers/src/tokens';
 import { setupEnvironment } from './misc';
-import { printGas } from '@balancer-labs/v2-helpers/src/numbers';
+import { BigNumberish, printGas } from '@balancer-labs/v2-helpers/src/numbers';
 import { BytesLike, solidityKeccak256 } from 'ethers/lib/utils';
+import { MerkleTree } from '@balancer-labs/v2-distributors/lib/merkleTree';
 import { deploy } from '@balancer-labs/v2-helpers/src/contract';
+import { ethers } from 'hardhat';
 
 interface Claim {
-  distribution: BigNumber;
-  balance: BigNumber;
-  rewarder: string;
   rewardToken: string;
+  rewarder: string;
+  distribution: BigNumberish;
+  index: BigNumberish;
+  balance: BigNumberish;
   merkleProof: BytesLike[];
 }
 
 let vault: Contract;
 let tokens: TokenList;
-let trader: SignerWithAddress;
+let rewarder: SignerWithAddress;
+let claimer: SignerWithAddress;
 
 async function main() {
-  ({ vault, tokens, trader } = await setupEnvironment());
+  ({ vault, tokens, trader: rewarder } = await setupEnvironment());
+  [, claimer] = await ethers.getSigners();
 
   for (let i = 1; i <= 5; i++) {
     console.log(`\n# Claiming ${i} distributions`);
@@ -37,30 +42,30 @@ async function claimDistributions(numberOfDistributions: number, useInternalBala
 
   const rewardToken = Object.values(tokens)[0];
   const rewardAmount = BigNumber.from(100);
-  const merkleLeaf = solidityKeccak256(['address', 'uint256'], [trader.address, rewardAmount]);
+  const merkleLeaves = [
+    solidityKeccak256(['uint256', 'address', 'uint256'], [0, rewarder.address, rewardAmount]),
+    solidityKeccak256(['uint256', 'address', 'uint256'], [1, claimer.address, rewardAmount]),
+  ];
 
+  await prepareMerkleOrchard(merkleOrchard, rewardToken, numberOfDistributions, merkleLeaves, rewardAmount);
+
+  // The claimer now benefits from being able to reuse the same storage slots
   const claims: Claim[] = Array.from({ length: numberOfDistributions }, (_, distribution) => ({
-    distribution: BigNumber.from(distribution),
-    balance: rewardAmount,
-    rewarder: trader.address,
     rewardToken: rewardToken.address,
-    merkleProof: [],
+    rewarder: rewarder.address,
+    distribution,
+    index: 1,
+    balance: rewardAmount,
+    merkleProof: [merkleLeaves[0]],
   }));
-
-  await rewardToken.connect(trader).approve(merkleOrchard.address, rewardAmount.mul(numberOfDistributions));
-  for (let distribution = 0; distribution < numberOfDistributions; ++distribution) {
-    await (
-      await merkleOrchard.connect(trader).seedAllocations(rewardToken.address, distribution, merkleLeaf, rewardAmount)
-    ).wait();
-  }
 
   let receipt;
   if (useInternalBalance) {
     receipt = await (
-      await merkleOrchard.connect(trader).claimDistributionsToInternalBalance(trader.address, claims)
+      await merkleOrchard.connect(claimer).claimDistributionsToInternalBalance(claimer.address, claims)
     ).wait();
   } else {
-    receipt = await (await merkleOrchard.connect(trader).claimDistributions(trader.address, claims)).wait();
+    receipt = await (await merkleOrchard.connect(claimer).claimDistributions(claimer.address, claims)).wait();
   }
 
   console.log(
@@ -68,6 +73,41 @@ async function claimDistributions(numberOfDistributions: number, useInternalBala
       receipt.gasUsed / numberOfDistributions
     )} per claim)`
   );
+}
+
+async function prepareMerkleOrchard(
+  merkleOrchard: Contract,
+  rewardToken: Contract,
+  numberOfDistributions: number,
+  merkleLeaves: string[],
+  rewardAmount: BigNumber
+) {
+  const merkleTree = new MerkleTree(merkleLeaves);
+  const merkleRoot = merkleTree.getHexRoot();
+
+  await rewardToken.connect(rewarder).approve(merkleOrchard.address, rewardAmount.mul(numberOfDistributions).mul(2));
+  for (let distribution = 0; distribution < numberOfDistributions; ++distribution) {
+    await (
+      await merkleOrchard
+        .connect(rewarder)
+        .seedAllocations(rewardToken.address, distribution, merkleRoot, rewardAmount.mul(2))
+    ).wait();
+  }
+
+  // We need to initialize the bitmaps to a nonzero value so that we can see
+  // their effect on the claimer's gas costs.
+  // This is done by performing a claim on each distribution.
+  const rewarderClaims: Claim[] = Array.from({ length: numberOfDistributions }, (_, distribution) => ({
+    rewardToken: rewardToken.address,
+    rewarder: rewarder.address,
+    distribution,
+    index: 0,
+    balance: rewardAmount,
+    merkleProof: [merkleLeaves[1]],
+  }));
+
+  // After this, future claimers benefit from being able to reuse the same storage slots
+  await (await merkleOrchard.connect(rewarder).claimDistributions(rewarder.address, rewarderClaims)).wait();
 }
 
 main()
