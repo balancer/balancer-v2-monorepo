ac81813a2e691bc4e72aec7f5b39e631df158afb
Greg Taschuk <gregorytaschuk@gmail.com>
2021-09-28 10:25:53 -0700

    Distributors: added deploy task for MCB merkle distributor (#869)
    * added deploy task for mcb merkledistributor
    
    * Lint
    
    Co-authored-by: rabmarut <rabmarut@protonmail.com>

---
+++ pkg/deployments/tasks/20210928-mcb-merkle/index.ts
@@ -0,0 +1,10 @@
+import Task from '../../src/task';
+import { TaskRunOptions } from '../../src/types';
+import { MerkleRedeemDeployment } from './input';
+
+export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
+  const input = task.input() as MerkleRedeemDeployment;
+
+  const args = [input.Vault, input.rewardToken];
+  await task.deployAndVerify('MerkleRedeem', args, from, force);
+};
