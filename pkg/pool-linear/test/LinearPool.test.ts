701f4ed0fd959b28218f906f007f6eedadf59808
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-10-12 17:02:33 -0300

    Linear event renamings & fixes (#931)
    * Emit TargetsSet event in construction
    
    * Rename events to match MetaStable naming

--- pkg/pool-linear/test/LinearPool.test.ts
+++ pkg/pool-linear/test/LinearPool.test.ts
@@ -473,7 +473,10 @@ describe('LinearPool', function () {
           await wrappedTokenRateProvider.mockRate(newRate);
           const receipt = await action();
 
-          expectEvent.inReceipt(await receipt.wait(), 'WrappedTokenRateUpdated', { rate: newRate });
+          expectEvent.inReceipt(await receipt.wait(), 'PriceRateCacheUpdated', {
+            token: wrappedToken.address,
+            rate: newRate,
+          });
         });
       };
 
@@ -550,7 +553,8 @@ describe('LinearPool', function () {
         it('emits an event', async () => {
           const receipt = await pool.setWrappedTokenRateCacheDuration(newDuration, { from: owner });
 
-          expectEvent.inReceipt(await receipt.wait(), 'WrappedTokenRateProviderSet', {
+          expectEvent.inReceipt(await receipt.wait(), 'PriceRateProviderSet', {
+            token: wrappedToken.address,
             provider: wrappedTokenRateProvider.address,
             cacheDuration: newDuration,
           });
