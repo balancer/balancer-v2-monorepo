3ce1618e270440928d145efe5bece6636b8aef30
Tom French <tom@tomfren.ch>
2021-10-28 00:30:23 +0100

    feat: add functions to allow staking of ETH

--- pkg/standalone-utils/contracts/relayer/LidoWrapping.sol
+++ pkg/standalone-utils/contracts/relayer/LidoWrapping.sol
@@ -15,9 +15,12 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
+import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Address.sol";
+
 import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
 
 import "../interfaces/IBaseRelayerLibrary.sol";
+import "../interfaces/IstETH.sol";
 import "../interfaces/IwstETH.sol";
 
 /**
@@ -26,6 +29,8 @@ import "../interfaces/IwstETH.sol";
  * @dev All functions must be payable so that it can be called as part of a multicall involving ETH
  */
 abstract contract LidoWrapping is IBaseRelayerLibrary {
+    using Address for address payable;
+    
     IERC20 private immutable _stETH;
     IERC20 private immutable _wstETH;
 
@@ -85,6 +90,56 @@ abstract contract LidoWrapping is IBaseRelayerLibrary {
 
         if (recipient != address(this)){
             _stETH.transfer(msg.sender, result);
+        }
+
+        if (_isChainedReference(outputReference)) {
+            _setChainedReferenceValue(outputReference, result);
+        }
+    }
+
+    function stakeETH(
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 outputReference
+    ) external payable {
+        if (_isChainedReference(amount)) {
+            amount = _getChainedReferenceValue(amount);
+        }
+        
+        uint256 result = IstETH(address(_stETH)).submit{value: amount}(address(this));
+        
+        if (recipient != address(this)){
+            _wstETH.transfer(recipient, result);
+        }
+
+        if (_isChainedReference(outputReference)) {
+            _setChainedReferenceValue(outputReference, result);
+        }
+    }
+
+    function stakeETHAndWrap(
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 outputReference
+    ) external payable {
+        if (_isChainedReference(amount)) {
+            amount = _getChainedReferenceValue(amount);
+        }
+        
+        // The fallback function on the wstETH contract automatically stakes and wraps any ETH which is sent to it.
+        // We can then safely just send the ETH and just have to ensure that the call doesn't revert.
+        payable(address(_wstETH)).sendValue(amount);
+
+        // Note that the previous step would be dangerous should _wstETH be set to the zero address,
+        // however in this scenario the next line will revert, preventing loss of funds.
+        
+        // As the wstETH contract doesn't return how much wstETH was minted we must query this separately.
+        uint256 result = IwstETH(address(_wstETH)).getWstETHByStETH(amount);
+        
+        if (recipient != address(this)){
+            _wstETH.transfer(recipient, result);
         }
 
         if (_isChainedReference(outputReference)) {
