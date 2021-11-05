b32b7488ffd811b85a4f32c55da1ae78780bded0
Tom French <tom@tomfren.ch>
2021-11-05 15:42:52 +0000

    style: linting

--- pkg/standalone-utils/contracts/relayer/BaseRelayerLibrary.sol
+++ pkg/standalone-utils/contracts/relayer/BaseRelayerLibrary.sol
@@ -56,7 +56,11 @@ contract BaseRelayerLibrary is IBaseRelayerLibrary {
     /**
      * @notice Sets whether a particular relayer is authorised to act on behalf of the user
      */
-    function setRelayerApproval(address relayer, bool approved, bytes calldata authorisation) external payable {
+    function setRelayerApproval(
+        address relayer,
+        bool approved,
+        bytes calldata authorisation
+    ) external payable {
         require(relayer == address(this) || !approved, "Relayer can only approve itself");
         bytes memory data = abi.encodePacked(
             abi.encodeWithSelector(_vault.setRelayerApproval.selector, msg.sender, relayer, approved),
