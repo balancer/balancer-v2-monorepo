3ce4d92e2ad70b04a97dd5c149ae1fe5dcc98fdf
Tom French <15848336+TomAFrench@users.noreply.github.com>
2021-11-05 20:03:33 +0000

    Chained exits (#975)
    * feat: add chained reference replacement logic for weighted pool exits
    
    * feat: add logic for storing chained references from exit outputs
    
    * feat: add AssetHelpers to IBaseRelayerLibrary
    
    * feat: add support for tracking internal balances to exits
    
    * style: linting
    
    * style: linting
    
    * refactor: switch to passing an array of structs to avoid unnecessary reads of internal balances
    
    * refactor: remove redundant leftover if statement
    
    * fix: strengthen guarantees on OutputReference correctness
    
    * test: add tests for standard exit types (i.e. not to internal balance)
    
    * style: silence linter warnings
    
    * fix: remove `.only` from exitPool tests
    
    * refactor: remove unnecessary translation as ETH can't be sent to internal balance
    
    * refactor: address review comments on simplifying chained reference logic
    
    * test: add support for encoding exits to internal balance
    
    * test: add support for testing exits with toInternalBalance = true
    
    * test: test both exits to internal and external balances
    
    * refactor: add review comments on token vs asset
    
    * refactor: rename toInternalBalance to useInternalBalance for clarity
    
    * Update VaultActions.sol
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- pkg/standalone-utils/contracts/interfaces/IBaseRelayerLibrary.sol
+++ pkg/standalone-utils/contracts/interfaces/IBaseRelayerLibrary.sol
@@ -15,12 +15,17 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
+import "@balancer-labs/v2-vault/contracts/AssetHelpers.sol";
 import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
 
 /**
  * @title IBaseRelayerLibrary
  */
-abstract contract IBaseRelayerLibrary {
+abstract contract IBaseRelayerLibrary is AssetHelpers {
+    constructor(IWETH weth) AssetHelpers(weth) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
     function getVault() public view virtual returns (IVault);
 
     function _isChainedReference(uint256 amount) internal pure virtual returns (bool);
