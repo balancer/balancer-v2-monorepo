816ae6a6414f824ff6bca303048174b8f31ff83e
Tom French <tom@tomfren.ch>
2021-11-18 19:11:01 +0000

    refactor: simplify loading userStaking in _claim

--- pkg/distributors/contracts/MultiDistributor.sol
+++ pkg/distributors/contracts/MultiDistributor.sol
@@ -573,9 +573,7 @@ contract MultiDistributor is IMultiDistributor, ReentrancyGuard, MultiDistributo
         for (uint256 i; i < distributionIds.length; i++) {
             bytes32 distributionId = distributionIds[i];
             Distribution storage distribution = _getDistribution(distributionId);
-
-            IERC20 stakingToken = distribution.stakingToken;
-            UserStaking storage userStaking = _userStakings[stakingToken][sender];
+            UserStaking storage userStaking = _userStakings[distribution.stakingToken][sender];
 
             // Note that the user may have unsubscribed from the distribution but still be due tokens. We therefore only
             // update the distribution if the user is subscribed to it (otherwise, it is already up to date).
