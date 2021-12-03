16a224e96f2b6dd65c5a44dbd5fe6ad71161494d
Nicolás Venturo <nicolas.venturo@gmail.com>
2021-12-03 09:19:40 -0300

    Do deployments

--- pkg/deployments/tasks/20211203-batch-relayer/input.ts
+++ pkg/deployments/tasks/20211203-batch-relayer/input.ts
@@ -8,6 +8,7 @@ export type BatchRelayerDeployment = {
 const Vault = new Task('20210418-vault');
 
 export default {
+  // wstETH is only deployed on mainnet and kovan
   mainnet: {
     Vault,
     wstETH: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
@@ -15,5 +16,17 @@ export default {
   kovan: {
     Vault,
     wstETH: '0xa387b91e393cfb9356a460370842bc8dbb2f29af',
+  },
+  rinkeby: {
+    Vault,
+    wstETH: '0x0000000000000000000000000000000000000000',
+  },
+  polygon: {
+    Vault,
+    wstETH: '0x0000000000000000000000000000000000000000',
+  },
+  arbitrum: {
+    Vault,
+    wstETH: '0x0000000000000000000000000000000000000000',
   },
 };
