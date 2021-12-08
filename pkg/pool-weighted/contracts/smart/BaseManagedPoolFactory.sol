52fed33b0be28807a7a619f636fe243415cdc163
Tom French <tom@tomfren.ch>
2021-12-08 11:57:34 +0000

    refactor: remove unused imports

--- pkg/pool-weighted/contracts/smart/BaseManagedPoolFactory.sol
+++ pkg/pool-weighted/contracts/smart/BaseManagedPoolFactory.sol
@@ -15,8 +15,6 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
-
 import "@balancer-labs/v2-pool-utils/contracts/factories/BasePoolSplitCodeFactory.sol";
 import "@balancer-labs/v2-pool-utils/contracts/factories/FactoryWidePauseWindow.sol";
 
