61f999ffc69179453a0e56766ffffc56fe2a5e46
Tom French <tom@tomfren.ch>
2022-01-18 18:08:18 +0000

    refactor: add comment explaining snake casing in interfaces

--- pkg/gauges/contracts/interfaces/IGaugeController.sol
+++ pkg/gauges/contracts/interfaces/IGaugeController.sol
@@ -14,7 +14,10 @@
 
 pragma solidity ^0.7.0;
 
+// For compatibility, we're keeping the same function names as in the original Curve code, including the mixed-case
+// naming convention.
+// solhint-disable func-name-mixedcase
+
 interface IGaugeController {
-    // solhint-disable-next-line func-name-mixedcase
     function gauge_types(address gauge) external view returns (uint256);
 }
--- pkg/gauges/contracts/interfaces/ILiquidityGauge.sol
+++ pkg/gauges/contracts/interfaces/ILiquidityGauge.sol
@@ -14,10 +14,12 @@
 
 pragma solidity ^0.7.0;
 
+// For compatibility, we're keeping the same function names as in the original Curve code, including the mixed-case
+// naming convention.
+// solhint-disable func-name-mixedcase
+
 interface ILiquidityGauge {
-    // solhint-disable-next-line func-name-mixedcase
     function integrate_fraction(address user) external view returns (uint256);
 
-    // solhint-disable-next-line func-name-mixedcase
     function user_checkpoint(address user) external returns (bool);
 }
