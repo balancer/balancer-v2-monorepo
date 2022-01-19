9850e5adbb1613b706571ae1301c327bfbae903b
Tom French <tom@tomfren.ch>
2022-01-19 21:32:50 +0000

    refactor: better syntax for dealing with optional contractInterface in actionId

--- pvt/helpers/src/models/misc/actions.ts
+++ pvt/helpers/src/models/misc/actions.ts
@@ -2,6 +2,6 @@ import { Contract } from 'ethers';
 import { Interface } from 'ethers/lib/utils';
 
 export const actionId = (instance: Contract, method: string, contractInterface?: Interface): Promise<string> => {
-  const selector = contractInterface ? contractInterface.getSighash(method) : instance.interface.getSighash(method);
+  const selector = (contractInterface ?? instance.interface).getSighash(method);
   return instance.getActionId(selector);
 };
