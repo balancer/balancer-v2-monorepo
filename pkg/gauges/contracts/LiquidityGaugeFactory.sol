67ffc704d7b61c6e2f895d7286117bb81a44f5b4
Tom French <tom@tomfren.ch>
2022-01-25 22:50:10 +0000

    refactor: pull in tracking of AuthorizerAdaptor from Ownable

--- pkg/gauges/contracts/LiquidityGaugeFactory.sol
+++ pkg/gauges/contracts/LiquidityGaugeFactory.sol
@@ -17,20 +17,21 @@ pragma experimental ABIEncoderV2;
 
 import "@balancer-labs/v2-solidity-utils/contracts/helpers/Authentication.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Clones.sol";
-import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Ownable.sol";
 import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
 
 import "./interfaces/ILiquidityGauge.sol";
 
-contract LiquidityGaugeFactory is Authentication, Ownable {
+contract LiquidityGaugeFactory is Authentication {
     IVault private immutable _vault;
     ILiquidityGauge private _gaugeImplementation;
+    address private _authorizerAdaptor;
 
     mapping(address => bool) private _isGaugeFromFactory;
     mapping(address => address) private _poolGauge;
 
     event GaugeCreated(address indexed gauge, address indexed pool);
     event GaugeImplementationUpdated(address oldGaugeImplementation, address newGaugeImplementation);
+    event AuthorizerAdaptorUpdated(address oldAuthorizerAdaptor, address newAuthorizerAdaptor);
 
     constructor(
         IVault vault,
@@ -39,7 +40,7 @@ contract LiquidityGaugeFactory is Authentication, Ownable {
     ) Authentication(bytes32(uint256(address(this)))) {
         _vault = vault;
         _gaugeImplementation = gauge;
-        _transferOwnership(authorizerAdaptor);
+        _authorizerAdaptor = authorizerAdaptor;
     }
 
     /**
@@ -61,7 +62,7 @@ contract LiquidityGaugeFactory is Authentication, Ownable {
      * @dev This address has full admin powers over all gauges deployed by this factory.
      */
     function getAuthorizerAdaptor() external view returns (address) {
-        return owner();
+        return _authorizerAdaptor;
     }
 
     /**
@@ -99,7 +100,10 @@ contract LiquidityGaugeFactory is Authentication, Ownable {
      * @notice Sets the address of the adaptor used for administration of gauges.
      */
     function setAuthorizerAdaptor(address newAuthorizerAdaptor) external authenticate {
-        _transferOwnership(newAuthorizerAdaptor);
+        address currentAuthorizerAdaptor = address(_authorizerAdaptor);
+
+        _authorizerAdaptor = newAuthorizerAdaptor;
+        emit AuthorizerAdaptorUpdated(currentAuthorizerAdaptor, newAuthorizerAdaptor);
     }
 
     /**
