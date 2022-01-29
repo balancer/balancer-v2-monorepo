b0953239c9ea22f7f545a9249e6a1e2f3c78d6ac
Мосин Николай Григорьевич <Mosin.N.G@sberbank.ru>
2022-01-29 19:17:11 +0300

    add USD+ linear pool

---
+++ pkg/pool-linear/contracts/usdplus/UsdPlusLinearPool.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "../interfaces/IStaticUsdPlusToken.sol";
+
+import "../LinearPool.sol";
+
+contract UsdPlusLinearPool is LinearPool {
+    IStaticUsdPlusToken private immutable _wrappedToken;
+
+    constructor(
+        IVault vault,
+        string memory name,
+        string memory symbol,
+        IERC20 mainToken,
+        IERC20 wrappedToken,
+        uint256 upperTarget,
+        uint256 swapFeePercentage,
+        uint256 pauseWindowDuration,
+        uint256 bufferPeriodDuration,
+        address owner
+    )
+        LinearPool(
+            vault,
+            name,
+            symbol,
+            mainToken,
+            wrappedToken,
+            upperTarget,
+            swapFeePercentage,
+            pauseWindowDuration,
+            bufferPeriodDuration,
+            owner
+        )
+    {
+        _require(address(mainToken) == IStaticUsdPlusToken(address(wrappedToken)).mainToken(), Errors.TOKENS_MISMATCH);
+        _wrappedToken = IStaticUsdPlusToken(address(wrappedToken));
+    }
+
+    function _getWrappedTokenRate() internal view override returns (uint256) {
+        uint256 rate = _wrappedToken.rate();
+
+        // This function returns a 18 decimal fixed point number, but `rate` has 27 decimals (i.e. a 'ray' value)
+        // so we need to convert it.
+        return rate / 10**9;
+    }
+}
