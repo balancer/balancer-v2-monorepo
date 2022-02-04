6f34e7073330566b3f04242adeef582253b0ba96
Greg Taschuk <gregorytaschuk@gmail.com>
2022-02-04 09:29:21 -0800

    import fix

--- pkg/solidity-utils/contracts/misc/IERC4626.sol
+++ pkg/solidity-utils/contracts/misc/IERC4626.sol
@@ -14,7 +14,7 @@
 
 pragma solidity ^0.7.0;
 
-import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/IERC20.sol";
+import "../openzeppelin/IERC20.sol";
 
 interface IERC4626 is IERC20 {
     /**
