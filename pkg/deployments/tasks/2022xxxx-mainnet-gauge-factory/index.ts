a2ffbf96092f165c90d212e30144f205e1f6cfa2
Tom French <tom@tomfren.ch>
2022-02-07 13:39:41 +0000

    feat: update gauge implementation deployment to pass constructor args

--- pkg/deployments/tasks/2022xxxx-mainnet-gauge-factory/index.ts
+++ pkg/deployments/tasks/2022xxxx-mainnet-gauge-factory/index.ts
@@ -5,7 +5,11 @@ import { GaugeSystemDeployment } from './input';
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as GaugeSystemDeployment;
 
-  const gaugeImplementation = await task.deployAndVerify('LiquidityGaugeV5', [], from, force);
+  // veBoost proxy deployment does not exist so use zero address for now
+  // TODO: add deployment task for this
+  const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+  const gaugeImplementationArgs = [input.BalancerMinter, ZERO_ADDRESS];
+  const gaugeImplementation = await task.deployAndVerify('LiquidityGaugeV5', gaugeImplementationArgs, from, force);
 
   const args = [input.Vault, gaugeImplementation.address, input.AuthorizerAdaptor];
   await task.deployAndVerify('LiquidityGaugeFactory', args, from, force);
--- pkg/deployments/tasks/2022xxxx-mainnet-gauge-factory/input.ts
+++ pkg/deployments/tasks/2022xxxx-mainnet-gauge-factory/input.ts
@@ -2,13 +2,18 @@ import Task from '../../src/task';
 
 export type GaugeSystemDeployment = {
   AuthorizerAdaptor: string;
+  BalancerMinter: string;
   Vault: string;
 };
 
 const AuthorizerAdaptor = new Task('2022xxxx-authorizer-adaptor');
+const BalancerMinter = new Task('2022xxxx-gauge-controller');
 const Vault = new Task('20210418-vault');
 
 export default {
-  AuthorizerAdaptor,
-  Vault,
+  mainnet: {
+    AuthorizerAdaptor,
+    BalancerMinter,
+    Vault,
+  },
 };
