27d388e17a1eda850bda8b288fd33b516e0de408
Tom French <tom@tomfren.ch>
2022-03-08 00:09:48 +0000

    test: fill out simple test cases

--- pkg/gauges/test/GaugeAdder.test.ts
+++ pkg/gauges/test/GaugeAdder.test.ts
@@ -2,11 +2,20 @@ import { ethers } from 'hardhat';
 import { Contract } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
+import * as expectEvent from '@balancer-labs/v2-helpers/src/test/expectEvent';
 import { deploy } from '@balancer-labs/v2-helpers/src/contract';
 import Vault from '@balancer-labs/v2-helpers/src/models/vault/Vault';
 import { expect } from 'chai';
 import { actionId } from '@balancer-labs/v2-helpers/src/models/misc/actions';
-import Token from '@balancer-labs/v2-helpers/src/models/tokens/Token';
+import { ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
+
+enum GaugeType {
+  LiquidityMiningCommittee = 0,
+  veBAL,
+  Ethereum,
+  Polygon,
+  Arbitrum,
+}
 
 describe('GaugeAdder', () => {
   let vault: Vault;
@@ -26,12 +35,18 @@ describe('GaugeAdder', () => {
     if (!vault.authorizer) throw Error('Vault has no Authorizer');
     authorizer = vault.authorizer;
 
-    const token = await Token.create('BPT');
-    const veToken = await deploy('VotingEscrow', { args: [token.address, 'Voting Escrow BPT', 'veBAL'] });
-
-    gaugeController = await deploy('GaugeController', { args: [veToken.address] });
     adaptor = await deploy('AuthorizerAdaptor', { args: [vault.address] });
+    gaugeController = await deploy('MockGaugeController', {});
     gaugeAdder = await deploy('GaugeAdder', { args: [gaugeController.address, adaptor.address] });
+
+    await gaugeController.add_type('LiquidityMiningCommittee');
+    await gaugeController.add_type('veBAL');
+    await gaugeController.add_type('Ethereum');
+  });
+
+  sharedBeforeEach('set up permissions', async () => {
+    const action = await actionId(adaptor, 'add_gauge', gaugeController.interface);
+    await vault.grantPermissionsGlobally([action], gaugeAdder);
   });
 
   describe('constructor', () => {
@@ -59,22 +74,50 @@ describe('GaugeAdder', () => {
 
   describe('addGaugeFactory', () => {
     context('when caller is not authorized', () => {
-      it('reverts');
+      it('reverts', async () => {
+        await expect(gaugeAdder.connect(other).addGaugeFactory(ZERO_ADDRESS, GaugeType.Ethereum)).to.be.revertedWith(
+          'SENDER_NOT_ALLOWED'
+        );
+      });
     });
 
     context('when caller is authorized', () => {
+      sharedBeforeEach('authorize caller', async () => {
+        const action = await actionId(gaugeAdder, 'addGaugeFactory');
+        await vault.grantPermissionsGlobally([action], admin);
+      });
+
       context('when gauge type does not exist on GaugeController', () => {
-        it('reverts');
+        it('reverts', async () => {
+          await expect(gaugeAdder.connect(admin).addGaugeFactory(ZERO_ADDRESS, GaugeType.Polygon)).to.be.revertedWith(
+            'Invalid gauge type'
+          );
+        });
       });
 
       context('when gauge type exists on gauge controller', () => {
         context('when factory is a duplicate of an existing factory', () => {
-          it('reverts');
+          sharedBeforeEach('add gauge factory', async () => {
+            await gaugeAdder.connect(admin).addGaugeFactory(ZERO_ADDRESS, GaugeType.Ethereum);
+          });
+
+          it('reverts', async () => {
+            await expect(
+              gaugeAdder.connect(admin).addGaugeFactory(ZERO_ADDRESS, GaugeType.Ethereum)
+            ).to.be.revertedWith('Factory already added');
+          });
         });
 
         context('when factory is not a duplicate of an existing factory', () => {
           it('stores the new factory address');
-          it('emits a GaugeFactoryAdded event');
+          it('emits a GaugeFactoryAdded event', async () => {
+            const tx = await gaugeAdder.connect(admin).addGaugeFactory(ZERO_ADDRESS, GaugeType.Ethereum);
+            const receipt = await tx.wait();
+            expectEvent.inReceipt(receipt, 'GaugeFactoryAdded', {
+              gaugeType: GaugeType.Ethereum,
+              gaugeFactory: ZERO_ADDRESS,
+            });
+          });
         });
       });
     });
@@ -92,12 +135,21 @@ describe('GaugeAdder', () => {
 
   describe('addEthereumGauge', () => {
     context('when caller is not authorized', () => {
-      it('reverts');
+      it('reverts', async () => {
+        await expect(gaugeAdder.connect(other).addEthereumGauge(ZERO_ADDRESS)).to.be.revertedWith('SENDER_NOT_ALLOWED');
+      });
     });
 
     context('when caller is authorized', () => {
+      sharedBeforeEach('authorize caller', async () => {
+        const action = await actionId(gaugeAdder, 'addEthereumGauge');
+        await vault.grantPermissionsGlobally([action], admin);
+      });
+
       context('when gauge has not been deployed from a valid factory', () => {
-        it('reverts');
+        it('reverts', async () => {
+          await expect(gaugeAdder.connect(admin).addEthereumGauge(ZERO_ADDRESS)).to.be.revertedWith('Invalid gauge');
+        });
       });
 
       context('when gauge has been deployed from a valid factory', () => {
