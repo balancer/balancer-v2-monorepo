66b103a6a18623a602138837767dfae6f63f2f05
Tom French <tom@tomfren.ch>
2022-03-16 17:17:19 +0000

    refactor: remove unused input from gauge controller task

--- pkg/deployments/tasks/2022xxxx-gauge-controller/input.ts
+++ pkg/deployments/tasks/2022xxxx-gauge-controller/input.ts
@@ -1,7 +1,6 @@
 import Task from '../../src/task';
 
 export type GaugeSystemDeployment = {
-  BAL: string;
   BPT: string;
   BalancerTokenAdmin: string;
 };
@@ -13,12 +12,10 @@ const BalancerTokenAdmin = new Task('2022xxxx-balancer-token-admin');
 export default {
   mainnet: {
     BalancerTokenAdmin,
-    BAL: '0xba100000625a3754423978a60c9317c58a424e3D',
     BPT: '0x5c6Ee304399DBdB9C8Ef030aB642B10820DB8F56',
   },
   kovan: {
     BalancerTokenAdmin,
-    BAL: '0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7',
     BPT: '0x61d5dc44849c9C87b0856a2a311536205C96c7FD',
   },
 };
