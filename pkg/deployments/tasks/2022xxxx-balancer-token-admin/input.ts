c80df05900511ace6b7d37b05e7c92245a28845f
Tom French <tom@tomfren.ch>
2022-03-16 17:18:03 +0000

    feat: add kovan config for BalancerTokenAdmin task

--- pkg/deployments/tasks/2022xxxx-balancer-token-admin/input.ts
+++ pkg/deployments/tasks/2022xxxx-balancer-token-admin/input.ts
@@ -5,11 +5,16 @@ export type BalancerTokenAdminDeployment = {
   Vault: string;
 };
 
+const TestBAL = new Task('2021xxxx-test-balancer-token');
 const Vault = new Task('20210418-vault');
 
 export default {
   mainnet: {
     BAL: '0xba100000625a3754423978a60c9317c58a424e3D',
+    Vault,
+  },
+  kovan: {
+    BAL: TestBAL,
     Vault,
   },
 };
