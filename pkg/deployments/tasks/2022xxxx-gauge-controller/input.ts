c6ef15274d9a84c2f610e5f1e47ddf5be724507c
Tom French <tom@tomfren.ch>
2022-03-16 17:17:44 +0000

    feat: update gauge controller task to use new constructor

--- pkg/deployments/tasks/2022xxxx-gauge-controller/input.ts
+++ pkg/deployments/tasks/2022xxxx-gauge-controller/input.ts
@@ -3,18 +3,22 @@ import Task from '../../src/task';
 export type GaugeSystemDeployment = {
   BPT: string;
   BalancerTokenAdmin: string;
+  AuthorizerAdaptor: string;
 };
 
+const AuthorizerAdaptor = new Task('2022xxxx-authorizer-adaptor');
 const BalancerTokenAdmin = new Task('2022xxxx-balancer-token-admin');
 
 // Vote locking systems is only to be deployed to mainnet and kovan
 // BPT is the 80-20 BAL-WETH BPT token address
 export default {
   mainnet: {
+    AuthorizerAdaptor,
     BalancerTokenAdmin,
     BPT: '0x5c6Ee304399DBdB9C8Ef030aB642B10820DB8F56',
   },
   kovan: {
+    AuthorizerAdaptor,
     BalancerTokenAdmin,
     BPT: '0x61d5dc44849c9C87b0856a2a311536205C96c7FD',
   },
