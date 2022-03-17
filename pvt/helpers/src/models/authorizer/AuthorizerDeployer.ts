1ed42aaeb6f4a54337219ef6d63738a35c0536a2
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2022-03-17 17:46:06 -0300

    Authorizer: Ensure delays correctness (#1153)
    * authorizer: ensure delays correctness
    
    * authorizer: fix tests
    
    * authorizer: use `actionId` helper for tests
    
    * authorizer: extract set delay logic to helper

--- pvt/helpers/src/models/authorizer/AuthorizerDeployer.ts
+++ pvt/helpers/src/models/authorizer/AuthorizerDeployer.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'hardhat';
+
 import { deploy } from '../../contract';
 import { AuthorizerDeployment } from './types';
 
@@ -8,7 +9,8 @@ import TypesConverter from '../types/TypesConverter';
 export default {
   async deploy(deployment: AuthorizerDeployment): Promise<Authorizer> {
     const admin = deployment.admin || deployment.from || (await ethers.getSigners())[0];
-    const instance = await deploy('Authorizer', { args: [TypesConverter.toAddress(admin)] });
+    const vault = TypesConverter.toAddress(deployment.vault);
+    const instance = await deploy('Authorizer', { args: [TypesConverter.toAddress(admin), vault] });
     return new Authorizer(instance, admin);
   },
 };
