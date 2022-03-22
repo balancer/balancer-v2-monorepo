be5d2dd1e36060b22138249283bb11b5ed07604e
Tom French <tom@tomfren.ch>
2022-03-22 19:36:23 +0000

    feat: add function to checkpoint a token to be distributed

--- pkg/liquidity-mining/contracts/fee-distribution/FeeDistributor.sol
+++ pkg/liquidity-mining/contracts/fee-distribution/FeeDistributor.sol
@@ -27,6 +27,9 @@ contract FeeDistributor is ReentrancyGuard {
     uint256 private _timeCursor;
     mapping(uint256 => uint256) private _veSupplyCache;
     uint256 private _lastTokenTime;
+    uint256 private _tokenLastBalance;
+    mapping(uint256 => uint256) private _tokensPerWeek;
+
 
     // User State
     mapping(address => uint256) private _userTimeCursor;
@@ -39,6 +42,49 @@ contract FeeDistributor is ReentrancyGuard {
         startTime = _roundDownTimestamp(startTime);
         _startTime = startTime;
         _timeCursor = startTime;
+    }
+
+    function _checkpointToken(IERC20 token) internal {
+        uint256 tokenBalance = token.balanceOf(address(this));
+        uint256 tokensToDistribute = tokenBalance - _tokenLastBalance;
+        _tokenLastBalance = tokenBalance;
+
+        uint256 lastTokenTime = _lastTokenTime;
+        uint256 timeSinceLastCheckpoint = block.timestamp - lastTokenTime;
+        _lastTokenTime = block.timestamp;
+
+        uint256 thisWeek = _roundDownTimestamp(lastTokenTime);
+        uint256 nextWeek = 0;
+
+        bool checkpointedThisBlock = timeSinceLastCheckpoint == 0;
+
+        // Distribute `tokensToDistribute` evenly across the time period from `lastTokenTime` to now.
+        // These tokens are assigned to weeks proportionally to how much of this period falls into each week.
+        for (uint256 i = 0; i < 20; ++i){
+            nextWeek = thisWeek + 1 weeks;
+            if (block.timestamp < nextWeek){
+                // `thisWeek` is now the beginning of the current week, i.e. this is the final iteration.
+                if (checkpointedThisBlock && block.timestamp == lastTokenTime){
+                    _tokensPerWeek[thisWeek] += tokensToDistribute;
+                } else {
+                    _tokensPerWeek[thisWeek] += tokensToDistribute * (block.timestamp - lastTokenTime) / timeSinceLastCheckpoint;
+                }
+                // As we've caught up to the present then we should now break
+                break;
+            } else {
+                // We've gone a full week or more without checkpointing so need to distribute tokens to previous weeks
+                if (checkpointedThisBlock && nextWeek == lastTokenTime){
+                    // It shouldn't be possible to enter this block
+                    _tokensPerWeek[thisWeek] += tokensToDistribute;
+                } else {
+                    _tokensPerWeek[thisWeek] += tokensToDistribute * (nextWeek - lastTokenTime) / timeSinceLastCheckpoint;
+                }
+            }
+
+            // We've now "checkpointed" up to the beginning of next week so must update timestamps appropriately.
+            lastTokenTime = nextWeek;
+            thisWeek = nextWeek;
+        }
     }
 
     function _checkpointUserBalance(address user) internal {
