1c51a9fb52a66d1f9e775788f11b75ce90dfc318
rabmarut <rabmarut@protonmail.com>
2022-03-24 12:13:07 -0400

    Remove non-polygon inputs

--- pkg/deployments/tasks/20220318-batch-relayer-v2/input.ts
+++ pkg/deployments/tasks/20220318-batch-relayer-v2/input.ts
@@ -9,20 +9,8 @@ const Vault = new Task('20210418-vault');
 
 export default {
   Vault,
-  // wstETH is only deployed on mainnet and kovan
-  mainnet: {
-    wstETH: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
-  },
-  kovan: {
-    wstETH: '0xa387b91e393cfb9356a460370842bc8dbb2f29af',
-  },
-  rinkeby: {
-    wstETH: '0x0000000000000000000000000000000000000000',
-  },
+  // wstETH is not deployed on polygon
   polygon: {
-    wstETH: '0x0000000000000000000000000000000000000000',
-  },
-  arbitrum: {
     wstETH: '0x0000000000000000000000000000000000000000',
   },
 };
