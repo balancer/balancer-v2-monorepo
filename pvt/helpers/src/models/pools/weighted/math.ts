f73775b14eb70cf309a4e1be7b197856cf4f8196
dmf7z <fernandez.daniel@gmail.com>
2022-03-29 16:41:09 -0300

    Weighted pool: use correct compare operators

--- pvt/helpers/src/models/pools/weighted/math.ts
+++ pvt/helpers/src/models/pools/weighted/math.ts
@@ -61,7 +61,7 @@ export function calcBptOutGivenExactTokensIn(
   for (let i = 0; i < balances.length; i++) {
     let amountInWithoutFee;
 
-    if (balanceRatiosWithFee[i] > invariantRatioWithFees) {
+    if (balanceRatiosWithFee[i].gt(invariantRatioWithFees)) {
       const nonTaxableAmount = balances[i].mul(invariantRatioWithFees.sub(1));
       const taxableAmount = amountsIn[i].sub(nonTaxableAmount);
       amountInWithoutFee = nonTaxableAmount.add(taxableAmount.mul(decimal(1).sub(fromFp(fpSwapFeePercentage))));
@@ -125,7 +125,7 @@ export function calcBptInGivenExactTokensOut(
   let invariantRatio = decimal(1);
   for (let i = 0; i < balances.length; i++) {
     const tokenBalancePercentageExcess =
-      weightedBalanceRatio <= balanceRatiosWithoutFee[i]
+      weightedBalanceRatio.lte(balanceRatiosWithoutFee[i])
         ? 0
         : weightedBalanceRatio.sub(balanceRatiosWithoutFee[i]).div(decimal(1).sub(balanceRatiosWithoutFee[i]));
 
@@ -194,7 +194,7 @@ export function calculateBPTSwapFeeFeeAmount(
   currentInvariant: BigNumberish,
   fpProtocolSwapFeePercentage: BigNumberish
 ): Decimal {
-  if (currentInvariant <= lastInvariant) {
+  if (bn(currentInvariant).lte(lastInvariant)) {
     return decimal(1);
   }
 
