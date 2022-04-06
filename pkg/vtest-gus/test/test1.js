64736712233a90da80de5e52e092060ac3084347
Gustavo Belbruno <gustavobelbruno@gmail.com>
2022-04-06 14:16:34 -0300

    funded pool in test

--- pkg/vtest-gus/test/test1.js
+++ pkg/vtest-gus/test/test1.js
@@ -5,11 +5,18 @@ const wethAddress = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
 const bufferPeriodDuration = 0;
 const pauseWindowDuration = 0;
 
+let initial_mint = 100e6;
+const initialBalances = [50e6, 50e6];
+let swapFeePercentage = 3000000000000000;
+
+
+
 describe('TEST', () => {
 	let owner, addr1, addr2, addr3, addr4, addr5;
 	let wbtc, weth;
 	let vault;
 	let weightedPoolFactory;
+	let poolId;
 
 	describe('1) Deploy contracts', () => {
 		let Wbtc, Weth;
@@ -91,20 +98,20 @@ describe('TEST', () => {
 	
 		describe('Mint and approve ERC-20s', () => {
 				it('Mint WBTC 100 Token (from address1) to address2', async () => {
-					await wbtc.mint(addr2.address, 100);
-					expect(await wbtc.totalSupply()).to.equal(100);
+					await wbtc.mint(addr2.address, initial_mint);
+					expect(await wbtc.totalSupply()).to.equal(initial_mint);
 				});
 				it('Mint WETH 100 Token (from address1) to address2', async () => {
-					await weth.mint(addr2.address, 100);
-					expect(await weth.totalSupply()).to.equal(100);
+					await weth.mint(addr2.address, initial_mint);
+					expect(await weth.totalSupply()).to.equal(initial_mint);
 				});
 				it('Approbe WETH for Vault Contract in address2', async () => {
-					await weth.connect(addr2).approve(owner.address, 99999);
-					expect(await weth.allowance(addr2.address,owner.address)).to.equal(99999);
+					await weth.connect(addr2).approve(vault.address, initial_mint);
+					expect(await weth.allowance(addr2.address,vault.address)).to.equal(initial_mint);
 				});
 				it('Approbe WBTC for Vault Contract in address2', async () => {
-					await wbtc.connect(addr2).approve(owner.address, 99999);
-					expect(await wbtc.allowance(addr2.address,owner.address)).to.equal(99999);
+					await wbtc.connect(addr2).approve(vault.address, initial_mint);
+					expect(await wbtc.allowance(addr2.address,vault.address)).to.equal(initial_mint);
 				});
 		});
 			
@@ -113,6 +120,7 @@ describe('TEST', () => {
 	describe('3) Create pool and funded (address2)', () => {
 		//const erc20_address = ;
 		const weight_pool = [BigInt(400000000000000000), BigInt(600000000000000000)];
+		const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'; 
 
 		// it('Get creation Code Before', async () => {
 		// 	let a = await weightedPoolFactory.getCreationCode();
@@ -120,23 +128,31 @@ describe('TEST', () => {
 		// });
 
 		it('Address2 invoke CREATE function in WeightedPoolFactory', async () => {
-			let ver = await weightedPoolFactory.create(
+			let tx = await weightedPoolFactory.create(
 				"prueba",
 				"prue",
 				[weth.address, wbtc.address],
 				weight_pool,
 				[addr3.address, addr4.address],
-				3000000000000000,
-				addr5.address,
+				swapFeePercentage,
+				ZERO_ADDRESS,
 				);
+
+				const receipt = await tx.wait();
+
+				// We need to get the new pool address out of the PoolCreated event
+			const events = receipt.events.filter((e) => e.event === 'PoolCreated');
+			const poolAddress = events[0].args.pool;
+			console.log(`\t\tNew Pool Address: ${poolAddress}`);
+
+			const pool = await ethers.getContractAt('WeightedPool', poolAddress);
+			poolId  = await pool.getPoolId();
+			console.log(`\t\tNew Pool ID: ${poolId}`);
 
 			//console.log (`\t\tVerification: ${ver}`);
 			// for(var property in ver) {
 			// 	console.log("key: " + property + "|| value: " + ver[property]);
 			// }
-
-			let pool_id = ver['hash'];
-			console.log(pool_id);
 
 			//let b = await vault.getPoolTokens(pool_id);
 
@@ -144,7 +160,41 @@ describe('TEST', () => {
 		});
 
 		describe('Address2 Fund the new pool', () => {
-			// CODE
+
+			it('Address2 Fund the new pool', async () => {
+				// Tokens must be in the same order
+				// Values must be decimal-normalized! (USDT has 6 decimals)
+
+				// Construct userData
+				const JOIN_KIND_INIT = 0;
+				const initUserData =
+					ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256[]'],
+						[JOIN_KIND_INIT, initialBalances]);
+
+				// Pool Request		
+				const joinPoolRequest = {
+					assets: [weth.address, wbtc.address],
+					maxAmountsIn: initialBalances,
+					userData: initUserData,
+					fromInternalBalance: false
+				}
+
+				// define caller as the address you're calling from
+				//caller = addr2.address;
+
+				// joins are done on the Vault
+				const tx = await vault.connect(addr2).joinPool(
+														poolId, 
+														addr2.address, 
+														addr2.address, 
+														joinPoolRequest
+														);
+
+				// You can wait for it like this, or just print the tx hash and monitor
+				//const receipt = await tx.wait();
+
+			});
+
 		});
 	});
 
