7c120128055326d734114f7f54d0a05795f54395
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-04-05 22:12:53 -0300

    authorizer: test partial migrations

--- pkg/vault/test/authorizer/TimelockAuthorizerMigrator.test.ts
+++ pkg/vault/test/authorizer/TimelockAuthorizerMigrator.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { Contract } from 'ethers';
+import { Contract, ContractTransaction } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
 import { actionId } from '@balancer-labs/v2-helpers/src/models/misc/actions';
@@ -51,81 +51,93 @@ describe('TimelockAuthorizerMigrator', () => {
     REVOKE_ACTION_ID = await newAuthorizer.REVOKE_ACTION_ID();
   });
 
-  it('runs the migration properly', async () => {
-    expect(await migrator.migratedRoles()).to.be.equal(0);
+  const itMigratesPermissionsProperly = (migrate: () => Promise<unknown>) => {
+    it('runs the migration properly', async () => {
+      expect(await migrator.migratedRoles()).to.be.equal(0);
 
-    await migrator.migrate(0);
+      await migrate();
 
-    expect(await migrator.migratedRoles()).to.be.equal(rolesData.length);
-    expect(await migrator.isComplete()).to.be.true;
-  });
+      expect(await migrator.migratedRoles()).to.be.equal(rolesData.length);
+      expect(await migrator.isComplete()).to.be.true;
+    });
 
-  it('migrates all roles properly', async () => {
-    await migrator.migrate(0);
+    it('migrates all roles properly', async () => {
+      await migrate();
 
-    for (const roleData of rolesData) {
-      const membersCount = await oldAuthorizer.getRoleMemberCount(roleData.role);
-      for (let i = 0; i < membersCount; i++) {
-        const member = await oldAuthorizer.getRoleMember(roleData.role, i);
-        expect(await newAuthorizer.hasPermission(roleData.role, member, roleData.target)).to.be.true;
+      for (const roleData of rolesData) {
+        const membersCount = await oldAuthorizer.getRoleMemberCount(roleData.role);
+        for (let i = 0; i < membersCount; i++) {
+          const member = await oldAuthorizer.getRoleMember(roleData.role, i);
+          expect(await newAuthorizer.hasPermission(roleData.role, member, roleData.target)).to.be.true;
+        }
       }
-    }
-  });
+    });
 
-  it('migrates all admin roles properly', async () => {
-    await migrator.migrate(0);
+    it('migrates all admin roles properly', async () => {
+      await migrate();
 
-    for (const roleData of rolesData) {
-      const adminRole = await oldAuthorizer.getRoleAdmin(roleData.role);
-      const adminsCount = await oldAuthorizer.getRoleMemberCount(adminRole);
+      for (const roleData of rolesData) {
+        const adminRole = await oldAuthorizer.getRoleAdmin(roleData.role);
+        const adminsCount = await oldAuthorizer.getRoleMemberCount(adminRole);
+        for (let i = 0; i < adminsCount; i++) {
+          const admin = await oldAuthorizer.getRoleMember(adminRole, i);
+          expect(await newAuthorizer.hasPermissionOrWhatever(GRANT_ACTION_ID, admin, roleData.target, ONES_BYTES32)).to
+            .be.true;
+          expect(await newAuthorizer.hasPermissionOrWhatever(REVOKE_ACTION_ID, admin, roleData.target, ONES_BYTES32)).to
+            .be.true;
+        }
+      }
+    });
+
+    it('migrates all the default admins properly', async () => {
+      await migrate();
+
+      const adminsCount = await oldAuthorizer.getRoleMemberCount(ZERO_BYTES32);
       for (let i = 0; i < adminsCount; i++) {
-        const admin = await oldAuthorizer.getRoleMember(adminRole, i);
-        expect(await newAuthorizer.hasPermissionOrWhatever(GRANT_ACTION_ID, admin, roleData.target, ONES_BYTES32)).to.be
+        const admin = await oldAuthorizer.getRoleMember(ZERO_BYTES32, i);
+        expect(await newAuthorizer.hasPermissionOrWhatever(GRANT_ACTION_ID, admin, EVERYWHERE, ONES_BYTES32)).to.be
           .true;
-        expect(await newAuthorizer.hasPermissionOrWhatever(REVOKE_ACTION_ID, admin, roleData.target, ONES_BYTES32)).to
-          .be.true;
+        expect(await newAuthorizer.hasPermissionOrWhatever(REVOKE_ACTION_ID, admin, EVERYWHERE, ONES_BYTES32)).to.be
+          .true;
       }
-    }
+    });
+
+    it('does not set the new authorizer immediately', async () => {
+      await migrate();
+
+      expect(await newAuthorizer.isRoot(migrator.address)).to.be.true;
+      expect(await vault.getAuthorizer()).to.be.equal(oldAuthorizer.address);
+    });
+
+    it('revokes the admin roles from the migrator', async () => {
+      await migrate();
+
+      expect(await newAuthorizer.hasPermissionOrWhatever(GRANT_ACTION_ID, migrator.address, EVERYWHERE, ONES_BYTES32))
+        .to.be.false;
+      expect(await newAuthorizer.hasPermissionOrWhatever(REVOKE_ACTION_ID, migrator.address, EVERYWHERE, ONES_BYTES32))
+        .to.be.false;
+    });
+
+    it('finalizes the migration after the set root delay', async () => {
+      await migrate();
+
+      await expect(migrator.finalizeMigration()).to.be.revertedWith('CANNOT_TRIGGER_ROOT_CHANGE_YET');
+
+      const CHANGE_ROOT_DELAY = await newAuthorizer.getRootTransferDelay();
+      await advanceTime(CHANGE_ROOT_DELAY);
+
+      await migrator.finalizeMigration();
+      expect(await vault.getAuthorizer()).to.be.equal(newAuthorizer.address);
+      expect(await newAuthorizer.isRoot(root.address)).to.be.true;
+      expect(await newAuthorizer.isRoot(migrator.address)).to.be.false;
+    });
+  };
+
+  context('with a partial migration', () => {
+    itMigratesPermissionsProperly(() => migrator.migrate(0));
   });
 
-  it('migrates all the default admins properly', async () => {
-    await migrator.migrate(0);
-
-    const adminsCount = await oldAuthorizer.getRoleMemberCount(ZERO_BYTES32);
-    for (let i = 0; i < adminsCount; i++) {
-      const admin = await oldAuthorizer.getRoleMember(ZERO_BYTES32, i);
-      expect(await newAuthorizer.hasPermissionOrWhatever(GRANT_ACTION_ID, admin, EVERYWHERE, ONES_BYTES32)).to.be.true;
-      expect(await newAuthorizer.hasPermissionOrWhatever(REVOKE_ACTION_ID, admin, EVERYWHERE, ONES_BYTES32)).to.be.true;
-    }
-  });
-
-  it('does not set the new authorizer immediately', async () => {
-    await migrator.migrate(0);
-
-    expect(await newAuthorizer.isRoot(migrator.address)).to.be.true;
-    expect(await vault.getAuthorizer()).to.be.equal(oldAuthorizer.address);
-  });
-
-  it('revokes the admin roles from the migrator', async () => {
-    await migrator.migrate(0);
-
-    expect(await newAuthorizer.hasPermissionOrWhatever(GRANT_ACTION_ID, migrator.address, EVERYWHERE, ONES_BYTES32)).to
-      .be.false;
-    expect(await newAuthorizer.hasPermissionOrWhatever(REVOKE_ACTION_ID, migrator.address, EVERYWHERE, ONES_BYTES32)).to
-      .be.false;
-  });
-
-  it('finalizes the migration after the set root delay', async () => {
-    await migrator.migrate(0);
-
-    await expect(migrator.finalizeMigration()).to.be.revertedWith('CANNOT_TRIGGER_ROOT_CHANGE_YET');
-
-    const CHANGE_ROOT_DELAY = await newAuthorizer.getRootTransferDelay();
-    await advanceTime(CHANGE_ROOT_DELAY);
-
-    await migrator.finalizeMigration();
-    expect(await vault.getAuthorizer()).to.be.equal(newAuthorizer.address);
-    expect(await newAuthorizer.isRoot(root.address)).to.be.true;
-    expect(await newAuthorizer.isRoot(migrator.address)).to.be.false;
+  context('with a full migration', () => {
+    itMigratesPermissionsProperly(() => Promise.all(rolesData.map(async () => await migrator.migrate(1))));
   });
 });
