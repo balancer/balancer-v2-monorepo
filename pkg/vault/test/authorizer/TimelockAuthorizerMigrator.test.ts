74334d68312ba2a12b8113998a40fae814e94e3c
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-04-05 22:15:15 -0300

    authorizer: fix linter

--- pkg/vault/test/authorizer/TimelockAuthorizerMigrator.test.ts
+++ pkg/vault/test/authorizer/TimelockAuthorizerMigrator.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { Contract, ContractTransaction } from 'ethers';
+import { Contract } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
 import { actionId } from '@balancer-labs/v2-helpers/src/models/misc/actions';
