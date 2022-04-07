c602f0f12704298b8e4d09a7baa36559db1345b9
Jeffrey Bennett <endymionjkb@gmail.com>
2022-04-07 14:04:24 -0400

    lint

--- pkg/pool-weighted/test/ManagedPool.test.ts
+++ pkg/pool-weighted/test/ManagedPool.test.ts
@@ -894,7 +894,10 @@ describe('ManagedPool', function () {
       protocolFeesCollector = await vault.getFeesCollector();
 
       twoTokens = poolTokens.subset(2);
-      localBalances = [bn(tokenAmount * 10 ** twoTokens.first.decimals), bn(tokenAmount * 10 ** twoTokens.second.decimals)];
+      localBalances = [
+        bn(tokenAmount * 10 ** twoTokens.first.decimals),
+        bn(tokenAmount * 10 ** twoTokens.second.decimals),
+      ];
 
       // 10% of the initial balance
       swapAmount = localBalances[0].div(10);
