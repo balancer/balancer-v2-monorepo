323f2cba9e9cde2e796cee2da0bf4b9a823f63b7
Gustavo Belbruno <gustavobelbruno@gmail.com>
2022-04-08 10:24:39 -0300

    clean code

--- pkg/vtest-gus/test/test1.js
+++ pkg/vtest-gus/test/test1.js
@@ -5,8 +5,8 @@ const wethAddress = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
 const bufferPeriodDuration = 0;
 const pauseWindowDuration = 0;
 
-let initial_mint = 1000e6;
-let initial_mint2 = 20e6;
+let initial_mint = 500e6;
+let initial_mint2 = 10e6;
 const initialBalances = [500e6, 500e6];
 let swapFeePercentage = 3000000000000000;
 const weight_pool = [BigInt(50e16), BigInt(50e16)];
@@ -73,12 +73,9 @@ describe('TEST', () => {
 				it('Check the right authorizer address', async () => {
 				    expect(await vault.getAuthorizer()).to.equal(authorizer);
 				});
-
 				it('Check the right WETH address', async () => {
 				    expect(await vault.WETH()).to.equal(wethAddress);
 				});
-
-
 			});
 		});
 		
@@ -131,13 +128,6 @@ describe('TEST', () => {
 	});
 	
 	describe('3) Create pool and funded (address1)', () => {
-		//const erc20_address = ;
-
-
-		// it('Get creation Code Before', async () => {
-		// 	let a = await weightedPoolFactory.getCreationCode();
-		// 	console.log(a);
-		// });
 
 		it('Address1 invoke CREATE function in WeightedPoolFactory - Simple pool 50%-50%', async () => {
 			let tx = await weightedPoolFactory.create(
@@ -149,7 +139,6 @@ describe('TEST', () => {
 				swapFeePercentage,
 				ZERO_ADDRESS,
 				);
-
 			const receipt = await tx.wait();
 
 				// We need to get the new pool address out of the PoolCreated event
@@ -161,25 +150,11 @@ describe('TEST', () => {
 			poolId  = await pool.getPoolId();
 			console.log(`\t\tNew Pool ID: ${poolId}`);
 
-
-
 			console.log(`\t\tNew Pool Details: (before funds)`);
 			tx = await vault.getPoolTokens(poolId);
 			console.log(`\t\t\tPool Tokens: ${tx[0]}`);
 			console.log(`\t\t\tPool Balance: ${tx[1]}`);
 			console.log(`\t\t\tPool lastChangeBlock: ${tx[2]}`);
-			//receipt = await tx.wait();
-
-			//console.log(`\t\t\tPool Tokens: ${tx.events[0].args.tokens}`);
-
-
-			//console.log (`\t\tVerification: ${ver}`);
-			// for(var property in ver) {
-			// 	console.log("key: " + property + "|| value: " + ver[property]);
-			// }
-
-			//let b = await vault.getPoolTokens(pool_id);
-
 
 			const lpToken = await ethers.getContractAt('erc20', poolAddress);
 			let lpBalance = await lpToken.totalSupply();
@@ -227,7 +202,6 @@ describe('TEST', () => {
 				console.log(`\t\t\tPool Balance: ${tx[1]}`);
 				console.log(`\t\t\tPool lastChangeBlock: ${tx[2]}`);
 
-
 				const lpToken = await ethers.getContractAt('erc20', poolAddress);
 				let lpBalance = await lpToken.totalSupply();
 				console.log(`\n\t\t\tERC20 LP Balance: ${lpBalance}`);
@@ -238,52 +212,6 @@ describe('TEST', () => {
 	});
 
 	describe('4) Swap - Address2', () => {
-
-		// it('Swap Attempt', async () => {
-
-		// //it('Swap Attempt -> 2 WETH - XX WBTC', async () => {
-
-		// 	const swap_amount = 3e6;
-		// 	const deadline = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
-
-		// 	// BatchSwapStep	
-		// 	const swaps = {
-		// 		poolId: poolId,
-		// 		assetInIndex: 1,
-		// 		assetOutIndex: 0,
-		// 		amount: swap_amount,
-		// 		userData: '0x'
-		// 	};
-
-		// 	const assets = [wbtc.address, weth.address];
-
-		// 	// Fund Management	
-		// 	const funds = {
-		// 		sender: addr2.address,
-		// 		fromInternalBalance: false,
-		// 		recipient: addr2.address,
-		// 		toInternalBalance: false
-		// 	};
-			
-		// 	// let tx = await vault.connect(addr2).batchSwap(
-		// 	// 										0, 
-		// 	// 										swaps, 
-		// 	// 										assets, 
-		// 	// 										funds,
-		// 	// 										swap_amount,
-		// 	// 										BigInt(deadline)
-		// 	// 										);
-
-			
-		// 	// console.log(`\t\tNew Pool Details: (after funds)`);
-		// 	// tx = await vault.getPoolTokens(poolId);
-		// 	// console.log(`\t\t\tPool Tokens: ${tx[0]}`);
-		// 	// console.log(`\t\t\tPool Balance: ${tx[1]}`);
-		// 	// console.log(`\t\t\tPool lastChangeBlock: ${tx[2]}`);
-
-
-
-		// });
 
 		it('Swap Simple Attempt ->  10 WETH - X??X WBTC', async () => {
 
@@ -327,12 +255,53 @@ describe('TEST', () => {
 				const lpToken = await ethers.getContractAt('erc20', poolAddress);
 				let lpBalance = await lpToken.totalSupply();
 				console.log(`\n\t\t\tERC20 LP Balance: ${lpBalance}`);
-	
-	
-	
 			});
+
+			// it('Batch Swap Attempt', async () => {
+			// //it('Swap Attempt -> 2 WETH - XX WBTC', async () => {
+
+			// 	const swap_amount = 3e6;
+			// 	const deadline = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+			// 	// BatchSwapStep	
+			// 	const swaps = {
+			// 		poolId: poolId,
+			// 		assetInIndex: 1,
+			// 		assetOutIndex: 0,
+			// 		amount: swap_amount,
+			// 		userData: '0x'
+			// 	};
+
+			// 	const assets = [wbtc.address, weth.address];
+
+			// 	// Fund Management	
+			// 	const funds = {
+			// 		sender: addr2.address,
+			// 		fromInternalBalance: false,
+			// 		recipient: addr2.address,
+			// 		toInternalBalance: false
+			// 	};
+				
+			// 	// let tx = await vault.connect(addr2).batchSwap(
+			// 	// 										0, 
+			// 	// 										swaps, 
+			// 	// 										assets, 
+			// 	// 										funds,
+			// 	// 										swap_amount,
+			// 	// 										BigInt(deadline)
+			// 	// 										);
+				
+			// 	// console.log(`\t\tNew Pool Details: (after funds)`);
+			// 	// tx = await vault.getPoolTokens(poolId);
+			// 	// console.log(`\t\t\tPool Tokens: ${tx[0]}`);
+			// 	// console.log(`\t\t\tPool Balance: ${tx[1]}`);
+			// 	// console.log(`\t\t\tPool lastChangeBlock: ${tx[2]}`);
+			// });
 	});
-		
-
-
 });
+
+
+//console.log (`\t\tVerification: ${ver}`);
+// for(var property in ver) {
+// 	console.log("key: " + property + "|| value: " + ver[property]);
+// }
