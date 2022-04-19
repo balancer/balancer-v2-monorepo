25ec0c33bda3d3f527d2c6564e6541b7bad4a4e6
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-04-18 23:35:47 -0300

    Run tests in separate forks

--- pkg/deployments/src/test.ts
+++ pkg/deployments/src/test.ts
@@ -1,4 +1,5 @@
 import { RunSuperFunction, HardhatRuntimeEnvironment, HttpNetworkConfig, HardhatNetworkConfig } from 'hardhat/types';
+import sum from 'lodash.sum';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
@@ -25,7 +26,7 @@ async function runDeployTests(args: any, hre: HardhatRuntimeEnvironment, run: Ru
 async function runForkTests(args: any, hre: HardhatRuntimeEnvironment, run: RunSuperFunction<any>): Promise<void> {
   console.log('Running fork tests...');
   if (args.fork === 'hardhat') throw Error('Cannot fork local networks');
-  args.testFiles = args.testFiles.filter((file: string) => file.endsWith('.fork.ts'));
+  const testFiles = args.testFiles.filter((file: string) => file.endsWith('.fork.ts'));
 
   const forkingNetworkName = Object.keys(hre.config.networks).find((networkName) => networkName === args.fork);
   if (!forkingNetworkName) throw Error(`Could not find a config for network ${args.fork} to be forked`);
@@ -33,15 +34,24 @@ async function runForkTests(args: any, hre: HardhatRuntimeEnvironment, run: RunS
   const forkingNetworkConfig = hre.config.networks[forkingNetworkName] as HttpNetworkConfig;
   if (!forkingNetworkConfig.url) throw Error(`Could not find a RPC url in network config for ${forkingNetworkName}`);
 
-  await hre.network.provider.request({
-    method: 'hardhat_reset',
-    params: [{ forking: { jsonRpcUrl: forkingNetworkConfig.url, blockNumber: args.blockNumber } }],
-  });
-
   const config = hre.network.config as HardhatNetworkConfig;
   config.forking = { enabled: true, blockNumber: args.blockNumber, url: forkingNetworkConfig.url };
 
-  await run(args);
+  // Run each test file in their own brand-new fork
+  const results: number[] = [];
+  for (const testFile of testFiles) {
+    await hre.network.provider.request({
+      method: 'hardhat_reset',
+      params: [{ forking: { jsonRpcUrl: forkingNetworkConfig.url, blockNumber: args.blockNumber } }],
+    });
+
+    args.testFiles = [testFile];
+    results.push(await run(args));
+  }
+
+  // Since we're now effectively running the test task multiple times, and each run sets the process' exit code, we need
+  // to overwrite that ourselves (by erroring if there were any errors).
+  process.exitCode = sum(results);
 }
 
 export function getForkedNetwork(hre: HardhatRuntimeEnvironment): string {
