d49579becefaaa3254ef64b8c360f9a0b0aafe90
Tom French <tom@tomfren.ch>
2022-04-25 12:11:00 +0100

    refactor: remove unused import

--- pkg/pool-weighted/contracts/smart/ManagedPool.sol
+++ pkg/pool-weighted/contracts/smart/ManagedPool.sol
@@ -15,7 +15,6 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/EnumerableMap.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/ReentrancyGuard.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/helpers/ERC20Helpers.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/helpers/WordCodec.sol";
@@ -61,7 +60,6 @@ contract ManagedPool is BaseWeightedPool, ProtocolFeeCache, ReentrancyGuard {
     using WordCodec for bytes32;
     using WeightCompression for uint256;
     using WeightedPoolUserData for bytes;
-    using EnumerableMap for EnumerableMap.IERC20ToUint256Map;
 
     // State variables
 
