232cff0f4dca5aab93d314b0c9b9e940c3b3ec3c
Tom French <tom@tomfren.ch>
2022-04-26 20:47:02 +0100

    fix: fix benchmark CI to allow it to deploy oversized ManagedPool

--- pvt/benchmarks/hardhat.config.ts
+++ pvt/benchmarks/hardhat.config.ts
@@ -4,6 +4,11 @@ import { hardhatBaseConfig } from '@balancer-labs/v2-common';
 import { name } from './package.json';
 
 export default {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
   solidity: {
     compilers: hardhatBaseConfig.compilers,
     overrides: { ...hardhatBaseConfig.overrides(name) },
