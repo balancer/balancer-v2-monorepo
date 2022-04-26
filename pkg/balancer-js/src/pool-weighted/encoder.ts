9991678a64be4986c01b79681c39c20d39ce82f3
Tom French <tom@tomfren.ch>
2022-04-27 00:28:49 +0100

    fix: correct the type of the `token` (`tokenIndex`) argument of joinForAddToken

--- pkg/balancer-js/src/pool-weighted/encoder.ts
+++ pkg/balancer-js/src/pool-weighted/encoder.ts
@@ -103,11 +103,11 @@ export class ManagedPoolEncoder {
 
   /**
    * Encodes the userData parameter for adding a new token to a WeightedPool
-   * @param token - the token being added
+   * @param tokenIndex - the index of the token being added to the pool
    * @param amountIn - the amount of the tokens to send to the pool as its initial balance
    */
-  static joinForAddToken = (token: string, amountIn: BigNumberish): string =>
-    defaultAbiCoder.encode(['uint256', 'uint256', 'uint256'], [WeightedPoolJoinKind.ADD_TOKEN, token, amountIn]);
+  static joinForAddToken = (tokenIndex: number, amountIn: BigNumberish): string =>
+    defaultAbiCoder.encode(['uint256', 'uint256', 'uint256'], [WeightedPoolJoinKind.ADD_TOKEN, tokenIndex, amountIn]);
 
   /**
    * Encodes the userData parameter for exiting a ManagedPool to remove a token.
