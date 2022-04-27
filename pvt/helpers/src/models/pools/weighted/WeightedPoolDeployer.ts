c965d49308b7222e3c1ee0f43281e3f7c41006ab
Tom French <tom@tomfren.ch>
2022-04-27 01:01:19 +0100

    refactor: remove MockManagedPool

--- pvt/helpers/src/models/pools/weighted/WeightedPoolDeployer.ts
+++ pvt/helpers/src/models/pools/weighted/WeightedPoolDeployer.ts
@@ -121,7 +121,7 @@ export default {
         break;
       }
       case WeightedPoolType.MANAGED_POOL: {
-        result = deploy('v2-pool-weighted/MockManagedPool', {
+        result = deploy('v2-pool-weighted/ManagedPool', {
           args: [
             {
               name: NAME,
