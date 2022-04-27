99d0999aa6a7053ae7612c6cacbadab069b92bf6
Tom French <tom@tomfren.ch>
2022-04-27 20:18:55 +0100

    refactor: reorder error codes to avoid reordering those in master

--- pkg/solidity-utils/contracts/helpers/BalancerErrors.sol
+++ pkg/solidity-utils/contracts/helpers/BalancerErrors.sol
@@ -161,13 +161,13 @@ library Errors {
     uint256 internal constant OUT_OF_NEW_TARGET_RANGE = 343;
     uint256 internal constant UNAUTHORIZED_OPERATION = 344;
     uint256 internal constant UNINITIALIZED_POOL_CONTROLLER = 345;
-    uint256 internal constant CHANGE_TOKENS_DURING_WEIGHT_CHANGE = 346;
-    uint256 internal constant CHANGE_TOKENS_PENDING_WEIGHT_CHANGE = 347;
-    uint256 internal constant MAX_WEIGHT = 348;
-    uint256 internal constant UNAUTHORIZED_JOIN = 349;
-    uint256 internal constant MAX_MANAGEMENT_AUM_FEE_PERCENTAGE = 350;
-    uint256 internal constant SET_SWAP_FEE_DURING_FEE_CHANGE = 351;
-    uint256 internal constant SET_SWAP_FEE_PENDING_FEE_CHANGE = 352;
+    uint256 internal constant SET_SWAP_FEE_DURING_FEE_CHANGE = 346;
+    uint256 internal constant SET_SWAP_FEE_PENDING_FEE_CHANGE = 347;
+    uint256 internal constant CHANGE_TOKENS_DURING_WEIGHT_CHANGE = 348;
+    uint256 internal constant CHANGE_TOKENS_PENDING_WEIGHT_CHANGE = 349;
+    uint256 internal constant MAX_WEIGHT = 350;
+    uint256 internal constant UNAUTHORIZED_JOIN = 351;
+    uint256 internal constant MAX_MANAGEMENT_AUM_FEE_PERCENTAGE = 352;
 
     // Lib
     uint256 internal constant REENTRANCY = 400;
