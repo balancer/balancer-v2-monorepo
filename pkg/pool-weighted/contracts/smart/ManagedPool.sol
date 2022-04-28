dd41681b596ee496d68a39eb33c732c3c9c1c804
Tom French <tom@tomfren.ch>
2022-04-28 21:57:28 +0100

    docs: add comment explaining why we pull tokens into the Pool contract

--- pkg/pool-weighted/contracts/smart/ManagedPool.sol
+++ pkg/pool-weighted/contracts/smart/ManagedPool.sol
@@ -476,7 +476,12 @@ contract ManagedPool is BaseWeightedPool, ProtocolFeeCache, ReentrancyGuard {
     ) external authenticate whenNotPaused {
         uint256 weightSumAfterAdd = _validateAddToken(normalizedWeight);
 
-        // Transfer tokens from the caller to this contract, since the sender for the join must be the Pool
+        // In order to add a token to a Pool we must perform a two step process:
+        // - First the new token must be registered on the Vault as belonging to this Pool.
+        // - Second a special join action must be performed to seed the Pool with it's initial balance of the new token.
+        
+        // We only allow the Pool to perform the special join mentioned above to ensure it only happens
+        // as part of adding a new token to the Pool, we then pull the necessary tokens from the caller.
         token.transferFrom(msg.sender, address(this), tokenAmountIn);
         token.approve(address(getVault()), tokenAmountIn);
 
