5e5b3812a052ddeb4e2e192b6d61b113704de6f0
Jeffrey Bennett <endymionjkb@gmail.com>
2022-05-02 19:13:00 -0400

    Split setPaused into pause/unpause

--- pvt/helpers/src/models/pools/linear/LinearPool.ts
+++ pvt/helpers/src/models/pools/linear/LinearPool.ts
@@ -273,8 +273,9 @@ export default class LinearPool {
   }
 
   async pause(): Promise<void> {
-    const action = await actionId(this.instance, 'setPaused');
-    await this.vault.grantPermissionsGlobally([action]);
-    await this.instance.setPaused(true);
+    const pauseAction = await actionId(this.instance, 'pause');
+    const unpauseAction = await actionId(this.instance, 'unpause');
+    await this.vault.grantPermissionsGlobally([pauseAction, unpauseAction]);
+    await this.instance.pause();
   }
 }
