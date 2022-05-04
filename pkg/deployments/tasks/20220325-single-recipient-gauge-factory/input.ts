cc805f1f365c912f350a024d97e41162b3416f1e
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-05-04 15:13:10 -0300

    Misc deployments improvements (#1283)
    * Remove deploy tests
    
    * Remove regular task tests
    
    * Simplify input processing
    
    * Also save on non-verify deployments
    
    * Remove from HRE
    
    * Improve task internals
    
    * Pull verification keys from config file
    
    * Introduce task modes
    
    * Add deployment alias

--- pkg/deployments/tasks/20220325-single-recipient-gauge-factory/input.ts
+++ pkg/deployments/tasks/20220325-single-recipient-gauge-factory/input.ts
@@ -1,10 +1,10 @@
-import Task from '../../src/task';
+import Task, { TaskMode } from '../../src/task';
 
 export type SingleRecipientFactoryDelegationDeployment = {
   BalancerMinter: string;
 };
 
-const BalancerMinter = new Task('20220325-gauge-controller');
+const BalancerMinter = new Task('20220325-gauge-controller', TaskMode.READ_ONLY);
 
 export default {
   BalancerMinter,
--- pkg/deployments/tasks/20220325-veBAL-deployment-coordinator/input.ts
+++ pkg/deployments/tasks/20220325-veBAL-deployment-coordinator/input.ts
@@ -1,4 +1,4 @@
-import Task from '../../src/task';
+import Task, { TaskMode } from '../../src/task';
 
 export type veBALDeploymentCoordinatorDeployment = {
   AuthorizerAdaptor: string;
@@ -11,12 +11,12 @@ export type veBALDeploymentCoordinatorDeployment = {
   thirdStageDelay: string;
 };
 
-const BalancerMinter = new Task('20220325-gauge-controller');
-const AuthorizerAdaptor = new Task('20220325-authorizer-adaptor');
-const GaugeAdder = new Task('20220325-gauge-adder');
-const LiquidityGaugeFactory = new Task('20220325-mainnet-gauge-factory');
-const SingleRecipientGaugeFactory = new Task('20220325-single-recipient-gauge-factory');
-const BALTokenHolderFactory = new Task('20220325-bal-token-holder-factory');
+const BalancerMinter = new Task('20220325-gauge-controller', TaskMode.READ_ONLY);
+const AuthorizerAdaptor = new Task('20220325-authorizer-adaptor', TaskMode.READ_ONLY);
+const GaugeAdder = new Task('20220325-gauge-adder', TaskMode.READ_ONLY);
+const LiquidityGaugeFactory = new Task('20220325-mainnet-gauge-factory', TaskMode.READ_ONLY);
+const SingleRecipientGaugeFactory = new Task('20220325-single-recipient-gauge-factory', TaskMode.READ_ONLY);
+const BALTokenHolderFactory = new Task('20220325-bal-token-holder-factory', TaskMode.READ_ONLY);
 
 export default {
   AuthorizerAdaptor,
