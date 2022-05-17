af11ec7ffab0a11b515728f22214f334b182e3bf
Tom French <15848336+TomAFrench@users.noreply.github.com>
2022-05-17 16:45:13 +0100

    fix: use force argument in no fee LBP task (#1303)

--- pkg/deployments/tasks/20211202-no-protocol-fee-lbp/index.ts
+++ pkg/deployments/tasks/20211202-no-protocol-fee-lbp/index.ts
@@ -5,5 +5,5 @@ import { TaskRunOptions } from '../../src/types';
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as NoProtocolFeeLiquidityBootstrappingPoolDeployment;
   const args = [input.Vault];
-  await task.deployAndVerify('NoProtocolFeeLiquidityBootstrappingPoolFactory', args, from);
+  await task.deployAndVerify('NoProtocolFeeLiquidityBootstrappingPoolFactory', args, from, force);
 };
