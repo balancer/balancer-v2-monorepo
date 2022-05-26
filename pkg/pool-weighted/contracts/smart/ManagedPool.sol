1784eb6c2e722b016852d433892f8731386df10b
Jeffrey Bennett <endymionjkb@gmail.com>
2022-05-26 10:37:28 -0400

    Expand on maximum swap fee documentation

--- pkg/pool-weighted/contracts/smart/ManagedPool.sol
+++ pkg/pool-weighted/contracts/smart/ManagedPool.sol
@@ -69,7 +69,9 @@ contract ManagedPool is BaseWeightedPool, AumProtocolFeeCache, ReentrancyGuard {
     // creation gas consumption.
     uint256 private constant _MAX_MANAGED_TOKENS = 38;
 
-    // Must be < ONE for correct behavior of join/exit swaps
+    // The swap fee cannot be 100%: calculations that divide by (1-fee) would revert with division by zero.
+    // Swap fees close to 100% can still cause reverts when performing join/exit swaps, if the calculated fee
+    // amounts exceed the pool's token balances in the Vault. 80% is a very high, but relatively safe maximum value.
     uint256 private constant _MAX_SWAP_FEE_PERCENTAGE = 80e16; // 80%
 
     uint256 private constant _MAX_MANAGEMENT_SWAP_FEE_PERCENTAGE = 1e18; // 100%
