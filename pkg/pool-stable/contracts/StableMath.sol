797149327c063b6a28ae22e2d3f804b904edd8f8
Jeffrey Bennett <endymionjkb@gmail.com>
2022-05-31 00:35:24 -0400

    Add link to Curve's calculation

--- pkg/pool-stable/contracts/StableMath.sol
+++ pkg/pool-stable/contracts/StableMath.sol
@@ -52,6 +52,8 @@ library StableMath {
 
     // Computes the invariant given the current balances, using the Newton-Raphson approximation.
     // The amplification parameter equals: A n^(n-1)
+    // See: https://github.com/curvefi/curve-contract/blob/b0bbf77f8f93c9c5f4e415bce9cd71f0cdee960e/contracts/pool-templates/base/SwapTemplateBase.vy#L206
+    // solhint-disable-previous-line max-line-length
     function _calculateInvariant(
         uint256 amplificationParameter,
         uint256[] memory balances,
