43b10800f10ad2acee2be0a5196692ab758dadbc
Jeffrey Bennett <endymionjkb@gmail.com>
2022-05-31 09:40:51 -0400

    Merge import

--- pvt/helpers/src/models/types/TypesConverter.ts
+++ pvt/helpers/src/models/types/TypesConverter.ts
@@ -2,10 +2,9 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-wit
 import { toNormalizedWeights } from '@balancer-labs/balancer-js';
 import { ethers } from 'ethers';
 
-import { bn, fp } from '../../numbers';
+import { BigNumberish, bn, fp } from '../../numbers';
 import { DAY, MONTH } from '../../time';
 import { MAX_UINT256, ZERO_ADDRESS } from '../../constants';
-import { BigNumberish } from '../../numbers';
 import TokenList from '../tokens/TokenList';
 import { Account } from './types';
 import { RawVaultDeployment, VaultDeployment } from '../vault/types';
