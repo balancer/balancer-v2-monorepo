8043fe714e1a66d8d7ec75fc5e0f15bf9f2fe54e
Tom French <tom@tomfren.ch>
2022-06-14 20:20:40 +0100

    feat: add ChildChainGaugeRewardHelper

---
+++ pkg/liquidity-mining/contracts/gauges/ChildChainGaugeRewardHelper.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/IRewardsOnlyGauge.sol";
+import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/IChildChainStreamer.sol";
+
+contract ChildChainGaugeRewardHelper {
+    uint256 public constant CLAIM_FREQUENCY = 3600;
+
+    function claimRewardsFromGauge(IRewardsOnlyGauge gauge, address user) external {
+        _claimRewardsFromGauge(gauge, user);
+    }
+
+    function claimRewardsFromGauges(IRewardsOnlyGauge[] calldata gauges, address user) external {
+        for(uint256 i = 0; i < gauges.length; i++){
+            _claimRewardsFromGauge(gauges[i], user);
+        }
+    }
+
+    function _claimRewardsFromGauge(IRewardsOnlyGauge gauge, address user) internal {
+        if (gauge.last_claim() + CLAIM_FREQUENCY < block.timestamp) {
+            gauge.claim_rewards(user);
+        } else {
+            // Force rewards from the streamer onto the gauge.
+            gauge.reward_contract().get_reward();
+            gauge.claim_rewards(user);
+        }
+    }
+
+    function pendingRewards(
+        IRewardsOnlyGauge gauge,
+        address user,
+        address token
+    ) external returns (uint256) {
+        gauge.reward_contract().get_reward();
+        return gauge.claimable_reward_write(user, token);
+    }
+}
---
+++ pkg/liquidity-mining/test/ChildChainGaugeRewardHelper.test.ts
@@ -0,0 +1,152 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { Contract } from 'ethers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+
+import { deploy, deployedAt } from '@balancer-labs/v2-helpers/src/contract';
+import { currentTimestamp, HOUR } from '@balancer-labs/v2-helpers/src/time';
+import Token from '@balancer-labs/v2-helpers/src/models/tokens/Token';
+import Vault from '@balancer-labs/v2-helpers/src/models/vault/Vault';
+import { actionId } from '@balancer-labs/v2-helpers/src/models/misc/actions';
+import { parseFixed } from '@ethersproject/bignumber';
+import TokenList from '@balancer-labs/v2-helpers/src/models/tokens/TokenList';
+import { expectTransferEvent } from '@balancer-labs/v2-helpers/src/test/expectTransfer';
+
+describe('ChildChainGaugeRewardHelper', () => {
+  const rewardAmount = parseFixed('1', 18);
+
+  let vault: Vault;
+  let adaptor: Contract;
+
+  let balToken: Token;
+
+  let tokens: TokenList;
+  let gaugeOne: Contract;
+  let streamerOne: Contract;
+
+  let gaugeTwo: Contract;
+  let streamerTwo: Contract;
+
+  let gaugeHelper: Contract;
+
+  let admin: SignerWithAddress, distributor: SignerWithAddress, user: SignerWithAddress, other: SignerWithAddress;
+
+  before('setup signers', async () => {
+    [, admin, distributor, user, other] = await ethers.getSigners();
+  });
+
+  sharedBeforeEach('deploy token', async () => {
+    vault = await Vault.create({ admin });
+    if (!vault.authorizer) throw Error('Vault has no Authorizer');
+
+    adaptor = await deploy('AuthorizerAdaptor', { args: [vault.address] });
+
+    tokens = await TokenList.create([{ symbol: 'BPT' }, { symbol: 'BPT2' }]);
+    balToken = await Token.create({ symbol: 'BAL' });
+
+    const gaugeImplementation = await deploy('RewardsOnlyGauge', {
+      args: [balToken.address, vault.address, adaptor.address],
+    });
+    const streamerImplementation = await deploy('ChildChainStreamer', { args: [balToken.address, adaptor.address] });
+
+    const factory = await deploy('ChildChainLiquidityGaugeFactory', {
+      args: [gaugeImplementation.address, streamerImplementation.address],
+    });
+
+    await factory.create(tokens.addresses[0]);
+
+    gaugeOne = await deployedAt('RewardsOnlyGauge', await factory.getPoolGauge(tokens.addresses[0]));
+    streamerOne = await deployedAt('ChildChainStreamer', await factory.getPoolStreamer(tokens.addresses[0]));
+
+    await factory.create(tokens.addresses[1]);
+    gaugeTwo = await deployedAt('RewardsOnlyGauge', await factory.getPoolGauge(tokens.addresses[1]));
+    streamerTwo = await deployedAt('ChildChainStreamer', await factory.getPoolStreamer(tokens.addresses[1]));
+
+    gaugeHelper = await deploy('ChildChainGaugeRewardHelper');
+  });
+
+  sharedBeforeEach('stake into gauge', async () => {
+    await tokens.mint({ to: user });
+    await tokens.approve([
+      { to: gaugeOne, from: user },
+      { to: gaugeTwo, from: user },
+    ]);
+
+    await gaugeOne.connect(user)['deposit(uint256)'](rewardAmount);
+    await gaugeOne.connect(user)['deposit(uint256,address)'](rewardAmount.mul(2), other.address);
+    await gaugeTwo.connect(user)['deposit(uint256)'](rewardAmount);
+    await gaugeTwo.connect(user)['deposit(uint256,address)'](rewardAmount.mul(2), other.address);
+  });
+
+  sharedBeforeEach('set up distributor on streamer', async () => {
+    const setDistributorActionId = await actionId(adaptor, 'set_reward_distributor', streamerOne.interface);
+    await vault.grantPermissionsGlobally([setDistributorActionId], admin);
+
+    const calldata = streamerOne.interface.encodeFunctionData('set_reward_distributor', [
+      balToken.address,
+      distributor.address,
+    ]);
+    await adaptor.connect(admin).performAction(streamerOne.address, calldata);
+    await adaptor.connect(admin).performAction(streamerTwo.address, calldata);
+  });
+
+  sharedBeforeEach('send tokens to streamer', async () => {
+    await balToken.mint(streamerOne.address, rewardAmount);
+    await streamerOne.connect(distributor).notify_reward_amount(balToken.address);
+
+    await balToken.mint(streamerTwo.address, rewardAmount);
+    await streamerTwo.connect(distributor).notify_reward_amount(balToken.address);
+  });
+
+  describe('claimRewardsFromGauge', () => {
+    context('when the gauge has not claimed tokens from streamer recently', () => {
+      it('claims tokens from the streamer', async () => {
+        const tx = await gaugeHelper.claimRewardsFromGauge(gaugeOne.address, user.address);
+        expectTransferEvent(await tx.wait(), { from: streamerOne.address, to: gaugeOne.address }, balToken);
+      });
+    });
+
+    context('when the gauge has claimed tokens from streamer recently', () => {
+      sharedBeforeEach('send tokens to streamer', async () => {
+        await gaugeOne['claim_rewards(address)'](other.address);
+
+        // Ensure that we're still in the rate limiting period.
+        const now = await currentTimestamp();
+        const lastClaimTime = await gaugeOne.last_claim();
+        expect(now.sub(lastClaimTime)).to.be.lt(HOUR);
+      });
+
+      it('claims more tokens from the streamer', async () => {
+        const tx = await gaugeHelper.claimRewardsFromGauge(gaugeOne.address, user.address);
+        expectTransferEvent(await tx.wait(), { from: streamerOne.address, to: gaugeOne.address }, balToken);
+      });
+    });
+  });
+
+  describe('claimRewardsFromGauges', () => {
+    context('when the gauge has not claimed tokens from streamer recently', () => {
+      it('claims tokens from the streamer', async () => {
+        const tx = await gaugeHelper.claimRewardsFromGauges([gaugeOne.address, gaugeTwo.address], user.address);
+        expectTransferEvent(await tx.wait(), { from: streamerOne.address, to: gaugeOne.address }, balToken);
+        expectTransferEvent(await tx.wait(), { from: streamerTwo.address, to: gaugeTwo.address }, balToken);
+      });
+    });
+
+    context('when the gauge has claimed tokens from streamer recently', () => {
+      sharedBeforeEach('send tokens to streamer', async () => {
+        await gaugeOne['claim_rewards(address)'](other.address);
+
+        // Ensure that we're still in the rate limiting period.
+        const now = await currentTimestamp();
+        const lastClaimTime = await gaugeOne.last_claim();
+        expect(now.sub(lastClaimTime)).to.be.lt(HOUR);
+      });
+
+      it('claims more tokens from the streamer', async () => {
+        const tx = await gaugeHelper.claimRewardsFromGauges([gaugeOne.address, gaugeTwo.address], user.address);
+        expectTransferEvent(await tx.wait(), { from: streamerOne.address, to: gaugeOne.address }, balToken);
+        expectTransferEvent(await tx.wait(), { from: streamerTwo.address, to: gaugeTwo.address }, balToken);
+      });
+    });
+  });
+});
