049ce95d2ceb87614558bd63788a3051390e1268
Tom French <tom@tomfren.ch>
2022-06-14 23:07:49 +0100

    docs: add natspec

--- pkg/liquidity-mining/contracts/gauges/ChildChainGaugeRewardHelper.sol
+++ pkg/liquidity-mining/contracts/gauges/ChildChainGaugeRewardHelper.sol
@@ -18,13 +18,29 @@ pragma experimental ABIEncoderV2;
 import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/IRewardsOnlyGauge.sol";
 import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/IChildChainStreamer.sol";
 
+/**
+ * @title ChildChainGaugeRewardHelper
+ * @author Balancer Labs
+ * @notice Helper contract which allows claiming rewards from many RewardsOnlyGauges in a single transaction.
+ * This contract manually triggers an update to the gauges' streamers as a workaround for the gauge .
+ */
 contract ChildChainGaugeRewardHelper {
     uint256 public constant CLAIM_FREQUENCY = 3600;
 
+    /**
+     * @notice Claims pending rewards on RewardsOnlyGauge `gauge` for account `user`.
+     * @param gauge - The address of the RewardsOnlyGauge from which to claim rewards.
+     * @param user - The address of the user for which to claim rewards.
+     */
     function claimRewardsFromGauge(IRewardsOnlyGauge gauge, address user) external {
         _claimRewardsFromGauge(gauge, user);
     }
 
+    /**
+     * @notice Claims pending rewards on a list of RewardsOnlyGauges `gauge` for account `user`.
+     * @param gauges - An array of address of RewardsOnlyGauges from which to claim rewards.
+     * @param user - The address of the user for which to claim rewards.
+     */
     function claimRewardsFromGauges(IRewardsOnlyGauge[] calldata gauges, address user) external {
         for (uint256 i = 0; i < gauges.length; i++) {
             _claimRewardsFromGauge(gauges[i], user);
