ded481f6d1c0100598d8799c6b42c284b42d3ecf
Jeffrey Bennett <endymionjkb@gmail.com>
2022-07-05 11:37:20 -0400

    Remove asset manager utils

--- pkg/pool-utils/contracts/LegacyBasePool.sol
+++ pkg/pool-utils/contracts/LegacyBasePool.sol
@@ -15,7 +15,7 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "@balancer-labs/v2-interfaces/contracts/asset-manager-utils/IAssetManager.sol";
+import "@balancer-labs/v2-interfaces/contracts/pool-utils/IAssetManager.sol";
 import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 import "@balancer-labs/v2-interfaces/contracts/vault/IBasePool.sol";
 
