7986dc9ccf52d80e8392da0a9839f93d69394f44
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-07-08 16:17:59 -0300

    Add LinearPoolRebalancer

--- pkg/pool-linear/contracts/test/MockStaticAToken.sol
+++ pkg/pool-linear/contracts/test/MockStaticAToken.sol
@@ -52,4 +52,21 @@ contract MockStaticAToken is TestToken, IStaticAToken, ILendingPool {
     function setReserveNormalizedIncome(uint256 newRate) external {
         _rate = newRate;
     }
+
+    function deposit(
+        address,
+        uint256,
+        uint16,
+        bool
+    ) external override returns (uint256) {
+        return 0;
+    }
+
+    function withdraw(
+        address,
+        uint256,
+        bool
+    ) external override returns (uint256, uint256) {
+        return (0, 0);
+    }
 }
