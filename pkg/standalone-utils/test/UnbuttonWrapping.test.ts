cc8cece24633a0d07c809c1f1054d1414bde80b1
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-07-12 12:27:28 -0300

    Add GaugeActions to BatchRelayerLibrary

--- pkg/standalone-utils/test/UnbuttonWrapping.test.ts
+++ pkg/standalone-utils/test/UnbuttonWrapping.test.ts
@@ -61,7 +61,7 @@ describe('UnbuttonWrapping', function () {
 
   sharedBeforeEach('set up relayer', async () => {
     // Deploy Relayer
-    relayerLibrary = await deploy('MockBatchRelayerLibrary', { args: [vault.address, ZERO_ADDRESS] });
+    relayerLibrary = await deploy('MockBatchRelayerLibrary', { args: [vault.address, ZERO_ADDRESS, ZERO_ADDRESS] });
     relayer = await deployedAt('BalancerRelayer', await relayerLibrary.getEntrypoint());
 
     // Authorize Relayer for all actions
