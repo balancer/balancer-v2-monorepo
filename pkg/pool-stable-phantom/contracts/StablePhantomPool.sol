827555838be9e694139e74eac46d4c52a97a9a41
Tom French <tom@tomfren.ch>
2022-07-25 19:50:14 +0100

    refactor: more readable boolean logic

--- pkg/pool-stable-phantom/contracts/StablePhantomPool.sol
+++ pkg/pool-stable-phantom/contracts/StablePhantomPool.sol
@@ -251,7 +251,7 @@ contract StablePhantomPool is IRateProvider, BaseGeneralPool, ProtocolFeeCache {
             // The exemptFromYieldFlag should never be set on a token without a rate provider.
             // This would cause division by zero errors downstream.
             _require(
-                !exemptFromYieldFlags[i] || rateProviders[i] != IRateProvider(0),
+                !(exemptFromYieldFlags[i] && rateProviders[i] == IRateProvider(0)),
                 Errors.TOKEN_DOES_NOT_HAVE_RATE_PROVIDER
             );
         }
