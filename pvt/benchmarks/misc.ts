55a14a9b61df77780588e9f1ab92da5f8b8ebccd
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-07-27 14:11:55 -0300

    Fix benchmarks

--- pvt/benchmarks/misc.ts
+++ pvt/benchmarks/misc.ts
@@ -79,10 +79,6 @@ export async function deployPool(vault: Vault, tokens: TokenList, poolName: Pool
     const assetManagers = Array(weights.length).fill(ZERO_ADDRESS);
     let params;
 
-    const aumProtocolFeesCollector = await deploy('v2-standalone-utils/AumProtocolFeesCollector', {
-      args: [vault.address],
-    });
-
     switch (poolName) {
       case 'ManagedPool': {
         const newPoolParams: ManagedPoolParams = {
@@ -97,7 +93,6 @@ export async function deployPool(vault: Vault, tokens: TokenList, poolName: Pool
           protocolSwapFeePercentage: MAX_UINT256,
           managementSwapFeePercentage: managementFee,
           managementAumFeePercentage: aumFee,
-          aumProtocolFeesCollector: aumProtocolFeesCollector.address,
         };
 
         const basePoolRights: BasePoolRights = {
