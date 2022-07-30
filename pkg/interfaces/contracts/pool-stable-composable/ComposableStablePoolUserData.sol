2c77eea6770b9a55d264f375ed1e406596e6363d
Jeffrey Bennett <endymionjkb@gmail.com>
2022-07-29 20:55:36 -0400

    Retain support for proportional exit for standalone-utils, etc.

--- pkg/interfaces/contracts/pool-stable-composable/ComposableStablePoolUserData.sol
+++ pkg/interfaces/contracts/pool-stable-composable/ComposableStablePoolUserData.sol
@@ -14,12 +14,9 @@
 
 pragma solidity ^0.7.0;
 
-import "../solidity-utils/openzeppelin/IERC20.sol";
-
-library StablePoolUserData {
-    // In order to preserve backwards compatibility, make sure new join and exit kinds are added at the end of the enum.
+library ComposableStablePoolUserData {
     enum JoinKind { INIT, EXACT_TOKENS_IN_FOR_BPT_OUT, TOKEN_IN_FOR_EXACT_BPT_OUT }
-    enum ExitKind { EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, EXACT_BPT_IN_FOR_TOKENS_OUT, BPT_IN_FOR_EXACT_TOKENS_OUT }
+    enum ExitKind { EXACT_BPT_IN_FOR_ONE_TOKEN_OUT, BPT_IN_FOR_EXACT_TOKENS_OUT }
 
     function joinKind(bytes memory self) internal pure returns (JoinKind) {
         return abi.decode(self, (JoinKind));
@@ -51,10 +48,6 @@ library StablePoolUserData {
 
     function exactBptInForTokenOut(bytes memory self) internal pure returns (uint256 bptAmountIn, uint256 tokenIndex) {
         (, bptAmountIn, tokenIndex) = abi.decode(self, (ExitKind, uint256, uint256));
-    }
-
-    function exactBptInForTokensOut(bytes memory self) internal pure returns (uint256 bptAmountIn) {
-        (, bptAmountIn) = abi.decode(self, (ExitKind, uint256));
     }
 
     function bptInForExactTokensOut(bytes memory self)
