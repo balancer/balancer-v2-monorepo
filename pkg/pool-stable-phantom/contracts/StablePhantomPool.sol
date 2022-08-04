fd6c2a04bcb3bb95baf7628869d5b6119861a19a
Tom French <tom@tomfren.ch>
2022-08-04 16:30:55 +0100

    style: fix Nico breaking the linter

--- pkg/pool-stable-phantom/contracts/StablePhantomPool.sol
+++ pkg/pool-stable-phantom/contracts/StablePhantomPool.sol
@@ -974,8 +974,8 @@ contract StablePhantomPool is
         view
         virtual
         override(
-            // Our inheritance pattern creates a small diamond that requires explicitly listing the parents here. Each parent
-            // calls the `super` version, so linearization takes care of making sure all implementations are called.
+            // Our inheritance pattern creates a small diamond that requires explicitly listing the parents here.
+            // Each parent calls the `super` version, so linearization ensures all implementations are called.
             BasePool,
             BasePoolAuthorization,
             StablePoolAmplification,
