94d58ad33ea15a37fa3ecea57c3cc70c16306a04
Tom French <tom@tomfren.ch>
2022-08-05 20:54:56 +0100

    refactor: add rate provider existence to bitmap in StablePoolStorage

--- pkg/pool-stable-phantom/contracts/StablePoolRates.sol
+++ pkg/pool-stable-phantom/contracts/StablePoolRates.sol
@@ -229,12 +229,12 @@ abstract contract StablePoolRates is StablePoolStorage {
     function _updateOldRates() internal {
         uint256 totalTokens = _getTotalTokens();
 
-        if (_hasCacheEntry(0)) _updateOldRate(0);
-        if (_hasCacheEntry(1)) _updateOldRate(1);
-        if (_hasCacheEntry(2)) _updateOldRate(2);
-        if (totalTokens > 3 && _hasCacheEntry(3)) _updateOldRate(3);
-        if (totalTokens > 4 && _hasCacheEntry(4)) _updateOldRate(4);
-        if (totalTokens > 5 && _hasCacheEntry(5)) _updateOldRate(5);
+        if (_hasRateProvider(0)) _updateOldRate(0);
+        if (_hasRateProvider(1)) _updateOldRate(1);
+        if (_hasRateProvider(2)) _updateOldRate(2);
+        if (totalTokens > 3 && _hasRateProvider(3)) _updateOldRate(3);
+        if (totalTokens > 4 && _hasRateProvider(4)) _updateOldRate(4);
+        if (totalTokens > 5 && _hasRateProvider(5)) _updateOldRate(5);
     }
 
     /**
@@ -250,7 +250,7 @@ abstract contract StablePoolRates is StablePoolStorage {
         uint256[] memory adjustedBalances = new uint256[](totalTokens);
 
         for (uint256 i = 0; i < totalTokens; ++i) {
-            adjustedBalances[i] = _isTokenExemptFromYieldProtocolFee(i) || (ignoreExemptFlags && _hasCacheEntry(i))
+            adjustedBalances[i] = _isTokenExemptFromYieldProtocolFee(i) || (ignoreExemptFlags && _hasRateProvider(i))
                 ? _adjustedBalance(balances[i], _tokenRateCaches[i])
                 : balances[i];
         }
@@ -261,17 +261,6 @@ abstract contract StablePoolRates is StablePoolStorage {
     // Compute balance * oldRate/currentRate, doing division last to minimize rounding error.
     function _adjustedBalance(uint256 balance, bytes32 cache) private pure returns (uint256) {
         return Math.divDown(Math.mul(balance, cache.getOldRate()), cache.getCurrentRate());
-    }
-
-    // Return true if the token at this index has a rate provider, so that it has
-    // an entry in the token rate cache.
-    function _hasCacheEntry(uint256 index) private view returns (bool) {
-        if (index == 0) return _getRateProvider0() != IRateProvider(0);
-        if (index == 1) return _getRateProvider1() != IRateProvider(0);
-        if (index == 2) return _getRateProvider2() != IRateProvider(0);
-        if (index == 3) return _getRateProvider3() != IRateProvider(0);
-        if (index == 4) return _getRateProvider4() != IRateProvider(0);
-        if (index == 5) return _getRateProvider5() != IRateProvider(0);
     }
 
     // Scaling Factors
