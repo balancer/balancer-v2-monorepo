e0756fae71e2ad7e7128df3c98b8c73a4be27845
Tom French <tom@tomfren.ch>
2022-09-07 19:29:26 +0100

    refactor: put _lastPostJoinExitInvariant in miscdata

--- pkg/pool-weighted/contracts/WeightedPoolProtocolFees.sol
+++ pkg/pool-weighted/contracts/WeightedPoolProtocolFees.sol
@@ -23,6 +23,7 @@ import "./BaseWeightedPool.sol";
 
 abstract contract WeightedPoolProtocolFees is BaseWeightedPool, ProtocolFeeCache {
     using FixedPoint for uint256;
+    using WordCodec for bytes32;
 
     // Rate providers are used only for computing yield fees; they do not inform swap/join/exit.
     IRateProvider internal immutable _rateProvider0;
@@ -45,7 +46,12 @@ abstract contract WeightedPoolProtocolFees is BaseWeightedPool, ProtocolFeeCache
     // immutable, the invariant only changes due to accumulated swap fees, which saves gas by freeing the Pool
     // from performing any computation or accounting associated with protocol fees during swaps.
     // This mechanism requires keeping track of the invariant after the last join or exit.
-    uint256 private _lastPostJoinExitInvariant;
+    //
+    // The maximum value of the invariant is the maximum allowable balance in the Vault (2**112) multiplied by the
+    // largest possible scaling factor (10**18 for a zero decimals token). The largest invariant is then
+    // 2**112 * 10**18 ~= 2**172, which means that to save gas we can place this in BasePool's `_miscData`.
+    uint256 private constant LAST_POST_JOINEXIT_INVARIANT_OFFSET = 0;
+    uint256 private constant LAST_POST_JOINEXIT_INVARIANT_BIT_LENGTH = 192;
 
     constructor(uint256 numTokens, IRateProvider[] memory rateProviders) {
         _require(numTokens <= 8, Errors.MAX_TOKENS);
@@ -87,7 +93,7 @@ abstract contract WeightedPoolProtocolFees is BaseWeightedPool, ProtocolFeeCache
      * @notice Returns the value of the invariant after the last join or exit operation.
      */
     function getLastPostJoinExitInvariant() public view returns (uint256) {
-        return _lastPostJoinExitInvariant;
+        return _getMiscData().decodeUint(LAST_POST_JOINEXIT_INVARIANT_OFFSET, LAST_POST_JOINEXIT_INVARIANT_BIT_LENGTH);
     }
 
     /**
@@ -140,7 +146,7 @@ abstract contract WeightedPoolProtocolFees is BaseWeightedPool, ProtocolFeeCache
 
         return
             InvariantGrowthProtocolSwapFees.getProtocolOwnershipPercentage(
-                preJoinExitInvariant.divDown(_lastPostJoinExitInvariant),
+                preJoinExitInvariant.divDown(getLastPostJoinExitInvariant()),
                 FixedPoint.ONE, // Supply has not changed so supplyGrowthRatio = 1
                 protocolSwapFeePercentage
             );
@@ -281,7 +287,13 @@ abstract contract WeightedPoolProtocolFees is BaseWeightedPool, ProtocolFeeCache
     function _updatePostJoinExit(uint256 postJoinExitInvariant) internal virtual override {
         // After all joins and exits we store the post join/exit invariant in order to compute growth due to swap fees
         // in the next one.
-        _lastPostJoinExitInvariant = postJoinExitInvariant;
+        _setMiscData(
+            _getMiscData().insertUint(
+                postJoinExitInvariant,
+                LAST_POST_JOINEXIT_INVARIANT_OFFSET,
+                LAST_POST_JOINEXIT_INVARIANT_BIT_LENGTH
+            )
+        );
     }
 
     // Helper functions
