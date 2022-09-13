7099fbd8af305096f99df8d721d9f26f65d6c859
Tom French <15848336+TomAFrench@users.noreply.github.com>
2022-09-13 15:47:53 +0100

    Move helper functions for scaling out BasePool to library (#1738)
    * refactor: move helper functions for scaling out to lib
    
    * style: disable solhint
    
    * test: move tests for scaling into separate test file
    
    * docs: add blurb on ScalingHelpers
    
    * style: re-add linting disable

--- pkg/pool-utils/contracts/test/MockBasePool.sol
+++ pkg/pool-utils/contracts/test/MockBasePool.sol
@@ -147,45 +147,6 @@ contract MockBasePool is BasePool {
         }
     }
 
-    function upscale(uint256 amount, uint256 scalingFactor) external pure returns (uint256) {
-        return _upscale(amount, scalingFactor);
-    }
-
-    function upscaleArray(uint256[] memory amounts, uint256[] memory scalingFactors)
-        external
-        pure
-        returns (uint256[] memory)
-    {
-        _upscaleArray(amounts, scalingFactors);
-        return amounts;
-    }
-
-    function downscaleDown(uint256 amount, uint256 scalingFactor) external pure returns (uint256) {
-        return _downscaleDown(amount, scalingFactor);
-    }
-
-    function downscaleDownArray(uint256[] memory amounts, uint256[] memory scalingFactors)
-        external
-        pure
-        returns (uint256[] memory)
-    {
-        _downscaleDownArray(amounts, scalingFactors);
-        return amounts;
-    }
-
-    function downscaleUp(uint256 amount, uint256 scalingFactor) external pure returns (uint256) {
-        return _downscaleUp(amount, scalingFactor);
-    }
-
-    function downscaleUpArray(uint256[] memory amounts, uint256[] memory scalingFactors)
-        external
-        pure
-        returns (uint256[] memory)
-    {
-        _downscaleUpArray(amounts, scalingFactors);
-        return amounts;
-    }
-
     function doNotCallInRecovery() external view whenNotInRecoveryMode {
         // solhint-disable-previous-line no-empty-blocks
     }
