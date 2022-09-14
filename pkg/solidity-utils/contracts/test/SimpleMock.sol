cf57ade27c1de74bb1bd412137e7ad0081263b03
Juan Ignacio Ubeira <juani@balancer.finance>
2022-09-14 17:08:15 -0300

    Adding basic Foundry support and test.

--- pkg/solidity-utils/contracts/test/SimpleMock.sol
+++ pkg/solidity-utils/contracts/test/SimpleMock.sol
@@ -14,20 +14,8 @@
 
 pragma solidity ^0.7.0;
 
-import "../helpers/OptionalOnlyCaller.sol";
-
-/**
- * @dev Mock with an external method that affects an address.
- *
- * The user can opt in to a verification, so that the method becomes callable
- * only by their address.
- */
-contract OptionalOnlyCallerMock is OptionalOnlyCaller {
-    constructor() EIP712("OptionalOnlyCallerMock", "1") {}
-
-    event CheckFunctionCalled();
-
-    function checkFunction(address user) external optionalOnlyCaller(user) {
-        emit CheckFunctionCalled();
+contract SimpleMock {
+    function pipe(uint256 a) public pure returns (uint256) {
+        return a;
     }
 }
