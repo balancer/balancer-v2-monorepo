db6e6e459af3590f203f32a21e172955dc40937d
Daniel <danielBeets@protonmail.com>
2022-09-16 11:02:01 +0200

    move rate scale factor to an immutable variable

--- pkg/pool-linear/contracts/reaper/ReaperLinearPool.sol
+++ pkg/pool-linear/contracts/reaper/ReaperLinearPool.sol
@@ -22,6 +22,8 @@ import "../LinearPool.sol";
 contract ReaperLinearPool is LinearPool {
     IReaperTokenVault private immutable _tokenVault;
 
+    uint256 private immutable _rateScaleFactor;
+
     struct ConstructorArgs {
         IVault vault;
         string name;
@@ -54,6 +56,18 @@ contract ReaperLinearPool is LinearPool {
         IReaperTokenVault tokenVault = IReaperTokenVault(address(args.wrappedToken));
         
         _tokenVault = tokenVault;
+
+        // Reaper vault tokens are always 18 decimal places, but are stored in the precision of the mainToken
+        // ie: If ppfs === 1e18, 1 USDC === 0.000_000_000_001_000_000 rfUSDC
+        // ie: If ppfs === 1e18, 1 DAI === 1 rfDAI
+        // ie: If ppfs === 1e18, 1 WBTC === 0.000_000_000_100_000_000 rfWBTC
+        // -----------
+        // Internally, the LinearPool scales all balances and rates up to 18 decimal places, meaning that 1 USDC is 
+        // represented as 1e18 by the LinearPool. Since the rfUSDC is already 18 decimals,
+        // but in a different representation, we need to account for that in our wrappedTokenRate.
+        // Since we only accept tokens with <= 18 decimals, we know the smallest this can be is 10^0 === 1
+        _rateScaleFactor = 10**(18 - ERC20(tokenVault.token()).decimals());
+
         _require(address(args.mainToken) == tokenVault.token(), Errors.TOKENS_MISMATCH);
     }
 
@@ -66,30 +80,7 @@ contract ReaperLinearPool is LinearPool {
         return assetManagers;
     }
 
-    /**
-     * Reaper vault tokens are always 18 decimal places, but are stored in the precision of the mainToken
-     * ie: If ppfs === 1e18, 1 USDC === 0.000_000_000_001_000_000 rfUSDC
-     * ie: If ppfs === 1e18, 1 DAI === 1 rfDAI
-     * ie: If ppfs === 1e18, 1 WBTC === 0.000_000_000_100_000_000 rfWBTC
-     * -----------
-     * Internally, the LinearPool scales all balances and rates up to 18 decimal places, meaning that 1 USDC is 
-     * represented as 1e18 by the LinearPool. Since the rfUSDC is already 18 decimals,
-     * but in a different representation,we need to account for that in our wrappedTokenRate.
-     */
     function _getWrappedTokenRate() internal view override returns (uint256) {
-        ERC20 underlying = ERC20(_tokenVault.token());
-        uint256 underlyingDecimals = underlying.decimals();
-
-        uint256 sharePrice = _tokenVault.getPricePerFullShare();
-        if (underlyingDecimals > 18) {
-            // scale down to 18
-            return sharePrice / 10**(underlyingDecimals - 18);
-        } else if (underlyingDecimals < 18) {
-            //we need to scale the ppfs up by 18 - underlyingDecimals
-            //ie: for USDC we would scaled up by 1e12
-            return sharePrice * 10**(18 - underlyingDecimals);
-        }
-
-        return sharePrice;
+        return _tokenVault.getPricePerFullShare() * _rateScaleFactor;
     }
 }
