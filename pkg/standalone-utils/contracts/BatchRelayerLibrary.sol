23534c4f6b74857bb49a15ea03789c572be20570
Daniel <91405705+danielmkm@users.noreply.github.com>
2022-09-16 19:48:53 +0200

    Reaper linear pool and supporting utilities (#1717)
    * ReaperLinearPool and ReaperWrapping implementation
    
    * port linear pool tests, not passing
    
    * add reaper wrapping to batch relayer, add reaper wrapping tests, fix linear pool tests
    
    * Add a comment about decimals being fixed to 18
    
    * add single-strat
    
    * remove the getter and make the token public
    
    * add support for a reaper linear pool rebalancer
    
    * fix comment
    
    * Refactor test setup & add wrappedTokenRate tests
    
    * refactor the divisor into an immutable variable.
    
    * Add source link for IReaperTokenVault
    
    * provide examples in the token vault comment
    
    * move rate scale factor to an immutable variable
    
    * add a comment for the divUp
    
    * Update pkg/pool-linear/test/ReaperLinearPool.test.ts
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>
    
    * Update comment formatting, send full relayer balance, remove the amountBefore concept
    
    * Use SafeMath for rateScaleFactor calculation
    
    * clean up lint errors
    
    Co-authored-by: 0xSkly <skly.beets@protonmail.com>
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- pkg/standalone-utils/contracts/BatchRelayerLibrary.sol
+++ pkg/standalone-utils/contracts/BatchRelayerLibrary.sol
@@ -22,6 +22,7 @@ import "./relayer/ERC4626Wrapping.sol";
 import "./relayer/GaugeActions.sol";
 import "./relayer/LidoWrapping.sol";
 import "./relayer/UnbuttonWrapping.sol";
+import "./relayer/ReaperWrapping.sol";
 import "./relayer/VaultActions.sol";
 import "./relayer/VaultPermit.sol";
 
@@ -37,6 +38,7 @@ contract BatchRelayerLibrary is
     GaugeActions,
     LidoWrapping,
     UnbuttonWrapping,
+    ReaperWrapping,
     VaultActions,
     VaultPermit
 {
