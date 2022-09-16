acdcb7d00fc9f8412730b60d4cc8f08aea76b980
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-09-16 10:23:24 -0300

    Improve input helpers test

--- pkg/solidity-utils/test/foundry/InputHelpers.t.sol
+++ pkg/solidity-utils/test/foundry/InputHelpers.t.sol
@@ -5,35 +5,18 @@ import "forge-std/Test.sol";
 
 import "../../contracts/helpers/InputHelpers.sol";
 
-contract MockInputHelpers {
-    function ensureArrayIsSorted(address[] memory addresses) external pure {
-        InputHelpers.ensureArrayIsSorted(addresses);
-    }
-}
-
 contract InputHelpersTest is Test {
-    MockInputHelpers mock;
-
-    function setUp() external {
-        mock = new MockInputHelpers();
-    }
-
     function testEnsureArrayIsSorted(address[] memory addresses) public {
-        (bool success, ) = address(mock).call(abi.encodeWithSelector(mock.ensureArrayIsSorted.selector, addresses));
-
-        if (success) {
-            for (uint256 i = 1; i < addresses.length; ++i) {
-                assertTrue(addresses[i - 1] < addresses[i]);
+        bool unsorted = false;
+        for (uint256 i = 1; i < addresses.length; ++i) {
+            if (addresses[i - 1] >= addresses[i]) {
+                unsorted = true;
             }
-        } else {
-            bool unsorted = false;
-            for (uint256 i = 1; i < addresses.length; ++i) {
-                if (addresses[i - 1] >= addresses[i]) {
-                    unsorted = true;
-                }
-            }
-
-            assertTrue(unsorted);
         }
+
+        if (unsorted) {
+            vm.expectRevert("BAL#101");
+        }
+        InputHelpers.ensureArrayIsSorted(addresses);
     }
 }
