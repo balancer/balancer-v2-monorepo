5d0c7e1dc3942a3674b024aed92bf3183a8b41c1
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-09-19 16:09:12 -0500

    Upgrade ignore-warnings (#1789)
    * Upgrade ignores
    
    * Add config comments

--- pvt/common/hardhat-base-config.ts
+++ pvt/common/hardhat-base-config.ts
@@ -64,7 +64,14 @@ export const overrides = (packageName: string): Record<string, SolcConfig> => {
 };
 
 export const warnings = {
-  ignore: {
-    'contracts/test/**/*': { 'code-size': true },
+  // Ignore code-size in test files: mocks may make contracts not deployable on real networks, but we don't care about
+  // that.
+  'contracts/test/**/*': {
+    'code-size': 'off',
+  },
+  // Make all warnings cause errors, except code-size (contracts may go over the limit during development).
+  '*': {
+    'code-size': 'warn',
+    default: 'error',
   },
 };
