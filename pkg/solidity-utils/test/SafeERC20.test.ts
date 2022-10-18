2bd62623a3a0ceebc559a120cefb082a7f2ca84f
Juan Ignacio Ubeira <juani@balancer.finance>
2022-10-18 18:58:13 -0300

    Add test in SafeERC20: token approve reverts.

--- pkg/solidity-utils/test/SafeERC20.test.ts
+++ pkg/solidity-utils/test/SafeERC20.test.ts
@@ -10,7 +10,7 @@ import { MAX_UINT256 } from '@balancer-labs/v2-helpers/src/constants';
 import { sharedBeforeEach } from '@balancer-labs/v2-common/sharedBeforeEach';
 
 describe('SafeERC20', () => {
-  let erc20: Contract, usdt: Contract, falseApproveERC20: Contract, safeERC20: Contract;
+  let erc20: Contract, usdt: Contract, falseApproveERC20: Contract, safeERC20: Contract, brokenERC20: Contract;
   let token: Contract;
   let spender: SignerWithAddress;
 
@@ -22,6 +22,7 @@ describe('SafeERC20', () => {
     erc20 = await deploy('ERC20Mock', { args: ['Token', 'TKN'] });
     usdt = await deploy('USDTMock');
     falseApproveERC20 = await deploy('ERC20FalseApprovalMock', { args: ['Token', 'TKN'] });
+    brokenERC20 = await deploy('BrokenERC20Mock', { args: ['Token', 'TKN'] });
 
     safeERC20 = await deploy('SafeERC20Mock');
   });
@@ -78,6 +79,16 @@ describe('SafeERC20', () => {
           await expect(safeERC20.safeApprove(token.address, spender.address, fp(1))).to.be.revertedWith(
             'SAFE_ERC20_CALL_FAILED'
           );
+        });
+      });
+
+      context('when approve reverts', () => {
+        sharedBeforeEach(() => {
+          token = brokenERC20;
+        });
+
+        it('reverts respecting the original reason', async () => {
+          await expect(safeERC20.safeApprove(token.address, spender.address, fp(1))).to.be.revertedWith('BROKEN_TOKEN');
         });
       });
     });
