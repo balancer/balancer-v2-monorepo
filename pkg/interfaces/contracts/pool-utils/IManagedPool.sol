2770f21ffaeedd63b88a1a533fa8c1d14d246170
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-10-21 14:36:21 -0300

    Update IManagedPool.sol

--- pkg/interfaces/contracts/pool-utils/IManagedPool.sol
+++ pkg/interfaces/contracts/pool-utils/IManagedPool.sol
@@ -296,8 +296,9 @@ interface IManagedPool is IBasePool {
 
     /**
      * @notice Removes a token from the Pool's list of tradeable tokens.
-     * @dev Tokens can only be removed if the Pool has more than 2 tokens, as it can never have fewer than 2. Token
-     * removal is also forbidden during a weight change, or if one is scheduled to happen in the future.
+     * @dev Tokens can only be removed if the Pool has more than 2 tokens, as it can never have fewer than 2 (not 
+     * including BPT). Token removal is also forbidden during a weight change, or if one is scheduled to happen in 
+     * the future.
      *
      * Emits the TokenRemoved event. This is a permissioned function.
      *
