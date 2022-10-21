d18a622e99519acbed7390a815409d1c1b46e434
Juan Ignacio Ubeira <juani@balancer.finance>
2022-10-21 19:06:33 -0300

    Add from / force to dependencies in deployment index.

--- pkg/deployments/tasks/20221021-managed-pool/index.ts
+++ pkg/deployments/tasks/20221021-managed-pool/index.ts
@@ -5,8 +5,8 @@ import { ManagedPoolDeployment } from './input';
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as ManagedPoolDeployment;
 
-  const addRemoveTokenLib = await task.deployAndVerify('ManagedPoolAddRemoveTokenLib');
-  const circuitBreakerLib = await task.deployAndVerify('CircuitBreakerLib');
+  const addRemoveTokenLib = await task.deployAndVerify('ManagedPoolAddRemoveTokenLib', [], from, force);
+  const circuitBreakerLib = await task.deployAndVerify('CircuitBreakerLib', [], from, force);
 
   const args = [input.Vault, input.ProtocolFeePercentagesProvider];
   const factory = await task.deployAndVerify('ManagedPoolFactory', args, from, force, {
