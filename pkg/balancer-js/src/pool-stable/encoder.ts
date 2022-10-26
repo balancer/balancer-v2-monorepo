804f55cce70abce9b67930b8e652fe138dbf5994
Jeffrey Bennett <endymionjkb@gmail.com>
2022-10-26 18:00:54 -0400

    lint

--- pkg/balancer-js/src/pool-stable/encoder.ts
+++ pkg/balancer-js/src/pool-stable/encoder.ts
@@ -11,7 +11,7 @@ export enum StablePoolJoinKind {
 export enum StablePoolExitKind {
   EXACT_BPT_IN_FOR_ONE_TOKEN_OUT = 0,
   EXACT_BPT_IN_FOR_TOKENS_OUT,
-  BPT_IN_FOR_EXACT_TOKENS_OUT
+  BPT_IN_FOR_EXACT_TOKENS_OUT,
 }
 
 export class StablePoolEncoder {
