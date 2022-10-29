631f70fd753d41f0906f438b7675afe43d0fa478
Benjamin <benjaminxh+github@gmail.com>
2022-10-29 15:49:51 +0200

    Update pkg/interfaces/contracts/standalone-utils/IProtocolFeeSplitter.sol
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- pkg/interfaces/contracts/standalone-utils/IProtocolFeeSplitter.sol
+++ pkg/interfaces/contracts/standalone-utils/IProtocolFeeSplitter.sol
@@ -14,8 +14,8 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
-import "@balancer-labs/v2-interfaces/contracts/standalone-utils/IProtocolFeesWithdrawer.sol";
+import "../vault/IVault.sol";
+import "./IProtocolFeesWithdrawer.sol";
 
 /**
  * @title ProtocolFeeSplitter
