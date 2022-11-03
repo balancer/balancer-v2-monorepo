b9097b15f17ab3b382c884a635f3dc95dfa08110
Juan Ignacio Ubeira <juani@balancer.finance>
2022-11-03 14:27:54 -0300

    Add test to check value transfers in adaptor entrypoint.

--- pkg/liquidity-mining/test/AuthorizerAdaptorEntrypoint.test.ts
+++ pkg/liquidity-mining/test/AuthorizerAdaptorEntrypoint.test.ts
@@ -9,12 +9,15 @@ import { ANY_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
 import { deploy } from '@balancer-labs/v2-helpers/src/contract';
 import { actionId } from '@balancer-labs/v2-helpers/src/models/misc/actions';
 import Vault from '@balancer-labs/v2-helpers/src/models/vault/Vault';
+import { fp } from '@balancer-labs/v2-helpers/src/numbers';
+import * as expectEvent from '@balancer-labs/v2-helpers/src/test/expectEvent';
 
 describe('AuthorizerAdaptorEntrypoint', () => {
   let vault: Contract;
   let authorizer: Contract;
   let adaptor: Contract;
   let entrypoint: Contract;
+  let paymentReceiver: Contract;
   let admin: SignerWithAddress, grantee: SignerWithAddress, other: SignerWithAddress;
 
   before('setup signers', async () => {
@@ -27,6 +30,10 @@ describe('AuthorizerAdaptorEntrypoint', () => {
     // TODO(@jubeira): initialize entrypoint and adaptor inside helpers.
     entrypoint = await deploy('AuthorizerAdaptorEntrypoint', { args: [adaptor.address] });
     await authorizer.setAdaptorEntrypoint(entrypoint.address);
+  });
+
+  sharedBeforeEach('deploy mock to receive payments', async () => {
+    paymentReceiver = await deploy('MockPaymentReceiver');
   });
 
   describe('constructor', () => {
@@ -49,10 +56,11 @@ describe('AuthorizerAdaptorEntrypoint', () => {
   });
 
   describe('performAction', () => {
-    let action: string;
-    let target: string;
-    let calldata: string;
-    let expectedResult: string;
+    let action: string, payableAction: string;
+    let target: string, payableTarget: string;
+    let calldata: string, payableCalldata: string;
+    let expectedResult: string, payableExpectedResult: string;
+    const payment = fp(0.3141516);
 
     sharedBeforeEach('prepare action', async () => {
       action = await actionId(adaptor, 'getProtocolFeesCollector', vault.interface);
@@ -63,26 +71,61 @@ describe('AuthorizerAdaptorEntrypoint', () => {
       expectedResult = defaultAbiCoder.encode(['address'], [await vault.getProtocolFeesCollector()]);
     });
 
+    sharedBeforeEach('prepare payable action', async () => {
+      payableAction = await actionId(adaptor, 'receivePayment', paymentReceiver.interface);
+
+      payableTarget = paymentReceiver.address;
+      payableCalldata = paymentReceiver.interface.encodeFunctionData('receivePayment');
+
+      const tx = await paymentReceiver.connect(admin).receivePayment({ value: payment });
+      const event = expectEvent.inReceipt(await tx.wait(), 'PaymentReceived');
+      payableExpectedResult = event.args.amount;
+    });
+
     context('when caller is authorized globally', () => {
       sharedBeforeEach('authorize caller globally', async () => {
         await authorizer.connect(admin).grantPermissions([action], grantee.address, [ANY_ADDRESS]);
+        await authorizer.connect(admin).grantPermissions([payableAction], grantee.address, [ANY_ADDRESS]);
       });
 
       it('performs the expected function call', async () => {
         const value = await entrypoint.connect(grantee).callStatic.performAction(target, calldata);
         expect(value).to.be.eq(expectedResult);
+      });
+
+      it('sends value to target contract correctly', async () => {
+        const value = await entrypoint
+          .connect(grantee)
+          .callStatic.performAction(payableTarget, payableCalldata, { value: payment });
+        expect(value).to.be.eq(payableExpectedResult);
+      });
+
+      it('rejects direct calls from the adaptor', async () => {
+        await expect(adaptor.connect(grantee).performAction(target, calldata)).to.be.revertedWith('SENDER_NOT_ALLOWED');
       });
     });
 
     context('when caller is authorized locally on target', () => {
       sharedBeforeEach('authorize caller on target locally', async () => {
         await authorizer.connect(admin).grantPermissions([action], grantee.address, [vault.address]);
+        await authorizer.connect(admin).grantPermissions([payableAction], grantee.address, [paymentReceiver.address]);
       });
 
       it('performs the expected function call', async () => {
         const value = await entrypoint.connect(grantee).callStatic.performAction(target, calldata);
 
         expect(value).to.be.eq(expectedResult);
+      });
+
+      it('sends value to target contract correctly', async () => {
+        const value = await entrypoint
+          .connect(grantee)
+          .callStatic.performAction(payableTarget, payableCalldata, { value: payment });
+        expect(value).to.be.eq(payableExpectedResult);
+      });
+
+      it('rejects direct calls from the adaptor', async () => {
+        await expect(adaptor.connect(grantee).performAction(target, calldata)).to.be.revertedWith('SENDER_NOT_ALLOWED');
       });
     });
 
