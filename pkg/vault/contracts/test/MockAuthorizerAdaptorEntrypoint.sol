3dcf82596b156a675757374adf7239c2d4110d9a
Jeffrey Bennett <endymionjkb@gmail.com>
2022-11-03 23:49:06 -0400

    refactor: move entrypoint Mock to Vault

--- pkg/vault/contracts/test/MockAuthorizerAdaptorEntrypoint.sol
+++ pkg/vault/contracts/test/MockAuthorizerAdaptorEntrypoint.sol
@@ -15,8 +15,13 @@
 pragma solidity ^0.7.0;
 
 import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/IAuthorizerAdaptor.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 
 contract MockAuthorizerAdaptorEntrypoint {
+    function getVault() external pure returns (IVault) {
+        return IVault(0);
+    }
+
     function getAuthorizerAdaptor() external pure returns (IAuthorizerAdaptor) {
         return IAuthorizerAdaptor(0);
     }
