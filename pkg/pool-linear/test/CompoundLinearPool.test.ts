be02e7b34be4c103d5fd671cc10f915062e47d7d
Bailey Spraggins <baileyspraggins@me.com>
2022-11-08 19:49:58 -0600

    Functionality to test all underlying decimal amounts

--- pkg/pool-linear/test/CompoundLinearPool.test.ts
+++ pkg/pool-linear/test/CompoundLinearPool.test.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
-import { Contract } from 'ethers';
+import { /*BigNumber,*/ Contract } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
 import { bn, fp } from '@balancer-labs/v2-helpers/src/numbers';
@@ -13,31 +13,39 @@ import LinearPool from '@balancer-labs/v2-helpers/src/models/pools/linear/Linear
 
 import { deploy } from '@balancer-labs/v2-helpers/src/contract';
 import Vault from '@balancer-labs/v2-helpers/src/models/vault/Vault';
-import { ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
+import { /*MAX_UINT256,*/ ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
+// import { FundManagement, SingleSwap } from '@balancer-labs/balancer-js';
+// import { values, wrap } from 'lodash';
+
+interface testingToken {
+  mainTokenName: string;
+  wrappedTokenName: string;
+  underlyingDecimals: number;
+}
 
 describe('CompoundLinearPool', function () {
   let vault: Vault;
-  let pool: LinearPool, tokens: TokenList, mainToken: Token, wrappedToken: Token;
   let poolFactory: Contract;
-  let mockLendingPool: Contract;
   let trader: SignerWithAddress, lp: SignerWithAddress, owner: SignerWithAddress;
+  //let funds: FundManagement;
+  const testingTokens: testingToken[] = [
+    { mainTokenName: 'SUSHI', wrappedTokenName: 'cSUSHI', underlyingDecimals: 18 },
+    { mainTokenName: 'WBTC', wrappedTokenName: 'cWBTC', underlyingDecimals: 8 },
+    { mainTokenName: 'USDT', wrappedTokenName: 'cUSDT', underlyingDecimals: 6 },
+    { mainTokenName: 'MADEUPTOKEN', wrappedTokenName: 'cMADEUPTOKEN', underlyingDecimals: 4 },
+  ];
 
   const POOL_SWAP_FEE_PERCENTAGE = fp(0.01);
 
   before('setup', async () => {
     [, lp, trader, owner] = await ethers.getSigners();
-  });
 
-  sharedBeforeEach('deploy tokens', async () => {
-    mainToken = await Token.create({ symbol: 'WBTC', name: 'WBTC', decimals: 8 });
-    const wrappedTokenInstance = await deploy('MockCToken', {
-      args: ['cWBTC', 'cWBTC', 8, mainToken.address],
-    });
-
-    wrappedToken = await Token.deployedAt(wrappedTokenInstance.address);
-    tokens = new TokenList([mainToken, wrappedToken]).sort();
-    mockLendingPool = wrappedTokenInstance;
-    await tokens.mint({ to: [lp, trader], amount: fp(100) });
+    // funds = {
+    //   sender: lp.address,
+    //   fromInternalBalance: false,
+    //   toInternalBalance: false,
+    //   recipient: lp.address,
+    // };
   });
 
   sharedBeforeEach('deploy pool factory', async () => {
@@ -48,74 +56,100 @@ describe('CompoundLinearPool', function () {
     });
   });
 
-  sharedBeforeEach('deploy and initialize pool', async () => {
+  async function deployPool(mainTokenAddress: string, wrappedTokenAddress: string) {
     const tx = await poolFactory.create(
       'Balancer Pool Token',
       'BPT',
-      mainToken.address,
-      wrappedToken.address,
-      bn(0),
+      mainTokenAddress,
+      wrappedTokenAddress,
+      fp(1_000_000),
       POOL_SWAP_FEE_PERCENTAGE,
       owner.address
     );
 
     const receipt = await tx.wait();
-
     const event = expectEvent.inReceipt(receipt, 'PoolCreated');
 
-    pool = await LinearPool.deployedAt(event.args.pool);
-  });
+    return LinearPool.deployedAt(event.args.pool);
+  }
 
-  describe('asset managers', () => {
-    it('sets the same asset manager for main and wrapped token', async () => {
-      const poolId = await pool.getPoolId();
+  for (let i = 0; i <= testingTokens.length - 1; i++) {
+    describe(`Pool with ${testingTokens[i].underlyingDecimals} decimals tests`, () => {
+      let mainToken: Token;
+      let wrappedToken: Token;
+      let tokens: TokenList;
+      let mockLendingPool: Contract;
+      let pool: LinearPool;
 
-      const { assetManager: firstAssetManager } = await vault.getPoolTokenInfo(poolId, tokens.first);
-      const { assetManager: secondAssetManager } = await vault.getPoolTokenInfo(poolId, tokens.second);
+      sharedBeforeEach('setup tokens, vault and linear pool', async () => {
+        mainToken = await Token.create({
+          symbol: testingTokens[i].mainTokenName,
+          name: testingTokens[i].mainTokenName,
+          decimals: testingTokens[i].underlyingDecimals,
+        });
+        const wrappedTokenInstance = await deploy('MockCToken', {
+          args: [testingTokens[i].wrappedTokenName, testingTokens[i].wrappedTokenName, 8, mainToken.address],
+        });
+        wrappedToken = await Token.deployedAt(wrappedTokenInstance.address);
 
-      expect(firstAssetManager).to.equal(secondAssetManager);
+        tokens = new TokenList([mainToken, wrappedToken]).sort();
+        mockLendingPool = wrappedTokenInstance;
+
+        await tokens.mint({ to: [lp, trader], amount: fp(100) });
+
+        pool = await deployPool(mainToken.address, wrappedToken.address);
+      });
+
+      describe('asset managers', () => {
+        it('sets the same asset manager for main and wrapped token', async () => {
+          const poolId = await pool.getPoolId();
+
+          const { assetManager: firstAssetManager } = await vault.getPoolTokenInfo(poolId, tokens.first);
+          const { assetManager: secondAssetManager } = await vault.getPoolTokenInfo(poolId, tokens.second);
+
+          expect(firstAssetManager).to.equal(secondAssetManager);
+        });
+
+        it('sets the no asset manager for the BPT', async () => {
+          const poolId = await pool.getPoolId();
+          const { assetManager } = await vault.instance.getPoolTokenInfo(poolId, pool.address);
+          expect(assetManager).to.equal(ZERO_ADDRESS);
+        });
+      });
+
+      describe('getWrappedTokenRate', () => {
+        it('returns the expected value', async () => {
+          // Exchange rates are returned as uint256 scaled by 10**(18-8 + underlying token decimal)
+          // First test will be a 1:1 exchange rate (1*10^(18-8+decimal))
+          const scaledValue = 18 - 8 + pool.mainToken.decimals;
+          const mockExchange = bn(10 ** scaledValue);
+          await mockLendingPool.setExchangeRateCurrent(mockExchange);
+          expect(await pool.getWrappedTokenRate()).to.be.eq(fp(1));
+
+          // We now double the reserve's normalised income to change the exchange rate to 2:1
+          const doubleMockExchange = bn(2 * 10 ** scaledValue);
+          await mockLendingPool.setExchangeRateCurrent(doubleMockExchange);
+          expect(await pool.getWrappedTokenRate()).to.be.eq(fp(2));
+        });
+      });
+
+      describe('constructor', () => {
+        it('reverts if the mainToken is not the ASSET of the wrappedToken', async () => {
+          const otherToken = await Token.create('DAI');
+
+          await expect(
+            poolFactory.create(
+              'Balancer Pool Token',
+              'BPT',
+              otherToken.address,
+              wrappedToken.address,
+              bn(0),
+              POOL_SWAP_FEE_PERCENTAGE,
+              owner.address
+            )
+          ).to.be.revertedWith('TOKENS_MISMATCH');
+        });
+      });
     });
-
-    it('sets the no asset manager for the BPT', async () => {
-      const poolId = await pool.getPoolId();
-      const { assetManager } = await vault.instance.getPoolTokenInfo(poolId, pool.address);
-      expect(assetManager).to.equal(ZERO_ADDRESS);
-    });
-  });
-
-  describe('getWrappedTokenRate', () => {
-    it('returns the expected value', async () => {
-      // Exchange rates are returned as uint256 scaled by 10**(18-8 + underlying token decimal)
-      // First test will be a 1:1 exchange rate (1*10^(18-8+decimal))
-      const scaledValue = 18 - 8 + pool.mainToken.decimals;
-      console.log(`underlying decimals: ${pool.mainToken.decimals}`);
-      const mockExchange = bn(10 ** scaledValue);
-      await mockLendingPool.setExchangeRateCurrent(mockExchange);
-      console.log(`Exchange rate: ${mockExchange}`);
-      expect(await pool.getWrappedTokenRate()).to.be.eq(fp(1));
-
-      // We now double the reserve's normalised income to change the exchange rate to 2:1
-      const doubleMockExchange = bn(2 * 10 ** scaledValue);
-      await mockLendingPool.setExchangeRateCurrent(doubleMockExchange);
-      expect(await pool.getWrappedTokenRate()).to.be.eq(fp(2));
-    });
-  });
-
-  describe('constructor', () => {
-    it('reverts if the mainToken is not the ASSET of the wrappedToken', async () => {
-      const otherToken = await Token.create('DAI');
-
-      await expect(
-        poolFactory.create(
-          'Balancer Pool Token',
-          'BPT',
-          otherToken.address,
-          wrappedToken.address,
-          bn(0),
-          POOL_SWAP_FEE_PERCENTAGE,
-          owner.address
-        )
-      ).to.be.revertedWith('TOKENS_MISMATCH');
-    });
-  });
+  }
 });
