935608bd8a8e243cbbcaa1aa9b134fd27b3cdd65
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-11-15 18:46:00 -0300

    Apply suggestions from code review
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/standalone-utils/contracts/PoolRecoveryEnabler.sol
+++ pkg/standalone-utils/contracts/PoolRecoveryEnabler.sol
@@ -23,9 +23,10 @@ import "@balancer-labs/v2-solidity-utils/contracts/helpers/SingletonAuthenticati
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/EnumerableSet.sol";
 
 /**
- * @dev This contract allows anyone to set Pools that feature rate providers into recovery mode, provided any of said
- * rate providers revert. This helps identify failing Pools to both let LPs promptly exit them, and also prevent further
- * swaps from being routed through them by off-chain mechanisms (since said swaps would fail).
+ * @dev This contract allows anyone to check a given Pool's rate providers and put the Pool into recovery mode 
+ * if any are reverting on `getRate`. This allows LPs to exit promptly, and also helps off-chain mechanisms
+ * identify failed pools and prevent further traffic from being routed to them (since in this state swap operations
+ * would fail).
  */
 contract PoolRecoveryEnabler is SingletonAuthentication {
     using EnumerableSet for EnumerableSet.AddressSet;
