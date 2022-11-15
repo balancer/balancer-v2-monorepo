4635c6871f42a8cdc71d87b71d0c299924d66796
Tom French <tom@tomfren.ch>
2022-11-15 14:43:35 +0000

    fix: add extra constructor arg to deploy script

--- pkg/deployments/tasks/20221115-aave-rebalanced-linear-pool/index.ts
+++ pkg/deployments/tasks/20221115-aave-rebalanced-linear-pool/index.ts
@@ -4,7 +4,7 @@ import { AaveLinearPoolDeployment } from './input';
 
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as AaveLinearPoolDeployment;
-  const args = [input.Vault, input.BalancerQueries];
+  const args = [input.Vault, input.ProtocolFeePercentagesProvider, input.BalancerQueries];
 
   await task.deployAndVerify('AaveLinearPoolFactory', args, from, force);
 };
