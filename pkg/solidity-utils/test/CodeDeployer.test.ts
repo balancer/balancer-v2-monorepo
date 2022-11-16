f02bae37209212733f7debcb8e66cc9245a61e2a
Juan Ignacio Ubeira <juani@balancer.finance>
2022-11-16 17:34:49 -0300

    Fix `CodeDeployer` test.

--- pkg/solidity-utils/test/CodeDeployer.test.ts
+++ pkg/solidity-utils/test/CodeDeployer.test.ts
@@ -35,7 +35,7 @@ describe('CodeDeployer', function () {
       const receipt = await (await factory.deploy(data)).wait();
       const event = expectEvent.inReceipt(receipt, 'CodeDeployed');
 
-      expect(await ethers.provider.getCode(event.args.at)).to.equal(data);
+      expect(await ethers.provider.getCode(event.args[0])).to.equal(data);
     });
   }
 });
