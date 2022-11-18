27f66bd8eea112b69c2f8da90477d05d40c0a657
Tom French <tom@tomfren.ch>
2022-11-18 00:43:28 +0000

    feat: begin adding tracking for state of permissions granted onchain

---
+++ pkg/permissions/scripts/getCallableFunctions.ts
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import path from 'path';
+import { ActionIdInfo, ContractActionIdData, getActionIdFunctions, safeReadJsonFile } from '../src/actionIds';
+import { getAccountLabel } from '../src/labelling';
+
+const funcsPath = path.join(__dirname, '../../deployments/action-ids/mainnet/action-ids.json');
+
+const actionIdFunctions = getActionIdFunctions(safeReadJsonFile<Record<string, ContractActionIdData>>(funcsPath));
+
+const specialCasedActionIds: Record<string, ActionIdInfo[]> = {
+  // Withdraw permission over BALTokenHolder for veBAL gauge.
+  '0x79922681fd17c90b4f3409d605f5b059ffcbcef7b5440321ae93b87f3b5c1c78': [
+    {
+      taskId: '20220325-bal-token-holder-factory',
+      contractName: 'BALTokenHolder',
+      signature: 'function withdrawFunds(address,uint256)',
+      useAdaptor: false,
+    },
+  ],
+};
+
+const ignoredActionIds: string[] = [
+  // Root permission. This is being removed as part of the migration to TimelockAuthorizer.
+  '0x0000000000000000000000000000000000000000000000000000000000000000',
+  // Permissions to managed an unused deployment of ComposableStablePool.
+  // Granted in https://forum.balancer.fi/t/bip-49-composablestable-and-aavelinearpools-permission-granting/3631
+  // Should be revoked/renounced before migration.
+  '0x11562115fbcf4955e097732f59969867f1cb458a8cbd648231b0ffae14c800de',
+  '0xf8ab8bdb4497d157053d2f796e50c33e6fff3d586b6db6880ab12eff1d907b2b',
+  '0x94611f33019f04ed070e076bbacb9ff5c5fe03d7184bef4026e1ee669d3b623e',
+  '0xd4f0c40da2129d4b1aba541e693e03b92a323a66f649257a258fe6e4ea331b52',
+  '0x367e95c6cc9f3041f3c6ee21b06ef8992a82318a6b2adbbfb6af3ee601769a30',
+  '0xfef90c64be79cb170a20e526196e7c8f2f37f441ae85c945c18a91a64777d309',
+  // Withdraw permission over unused BALTokenHolders
+  // LM committee BALTokenHolder
+  '0x590e300e371ba81baff1c912e578fdecbfa490f39994607a18ee692ab942f846',
+  // Polygon BALTokenHolder
+  '0x802db13f34b039826402f87748c166a94c8130bf894f8af7e1144c874b36b76e',
+  // Arbitrum BALTokenHolder
+  '0xbfa133e7b0ebe7bf8b3f11a17a38c0f4492b428e4fb7fc8b509da63189247b06',
+];
+
+const main = async () => {
+  const inputPath = path.join(__dirname, '../permissions/actionIds.json');
+  const userPermissions = safeReadJsonFile<string[]>(inputPath);
+
+  const callableFunctions = Object.fromEntries(
+    Object.entries(userPermissions).map(([user, actionIds]) => [
+      getAccountLabel(user),
+      actionIds
+        .filter((actionId) => !ignoredActionIds.includes(actionId))
+        .flatMap((actionId) => {
+          const actionIdInfo = actionIdFunctions[actionId] ?? specialCasedActionIds[actionId];
+          if (actionIdInfo === undefined) throw new Error(`Unknown action id: ${actionId}`);
+          return actionIdInfo;
+        }),
+    ])
+  );
+
+  const filePath = path.join(__dirname, '../permissions/functions.json');
+
+  fs.writeFileSync(filePath, JSON.stringify(callableFunctions, null, 2));
+};
+
+main();
