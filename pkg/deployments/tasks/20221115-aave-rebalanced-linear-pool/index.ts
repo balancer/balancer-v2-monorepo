e00ac4b414eccdc39bfc667cb43c853985df179b
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-11-22 19:45:21 -0300

    Update linear build info

--- pkg/deployments/tasks/20221115-aave-rebalanced-linear-pool/index.ts
+++ pkg/deployments/tasks/20221115-aave-rebalanced-linear-pool/index.ts
@@ -4,7 +4,13 @@ import { AaveLinearPoolDeployment } from './input';
 
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as AaveLinearPoolDeployment;
-  const args = [input.Vault, input.ProtocolFeePercentagesProvider, input.BalancerQueries];
+  const args = [
+    input.Vault,
+    input.ProtocolFeePercentagesProvider,
+    input.BalancerQueries,
+    input.FactoryVersion,
+    input.PoolVersion,
+  ];
 
   await task.deployAndVerify('AaveLinearPoolFactory', args, from, force);
 };
