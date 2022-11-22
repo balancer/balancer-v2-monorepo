741d77131dcf52ddbdff168a69847695a153dce5
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-11-22 14:22:18 -0300

    Apply review comments

--- pkg/deployments/tasks/20210418-vault/input.ts
+++ pkg/deployments/tasks/20210418-vault/input.ts
@@ -3,38 +3,17 @@ import { MONTH } from '@balancer-labs/v2-helpers/src/time';
 
 export type VaultDeployment = {
   Authorizer: string;
-  weth: string;
+  WETH: string;
   pauseWindowDuration: number;
   bufferPeriodDuration: number;
 };
 
 const Authorizer = new Task('20210418-authorizer', TaskMode.READ_ONLY);
-const Tokens = new Task('00000000-tokens', TaskMode.READ_ONLY);
+const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
 
 export default {
   Authorizer,
   pauseWindowDuration: 3 * MONTH,
   bufferPeriodDuration: MONTH,
-
-  mainnet: {
-    weth: Tokens.output({ network: 'mainnet' }).WETH,
-  },
-  polygon: {
-    weth: Tokens.output({ network: 'polygon' }).WETH, // WMATIC
-  },
-  arbitrum: {
-    weth: Tokens.output({ network: 'arbitrum' }).WETH,
-  },
-  optimism: {
-    weth: Tokens.output({ network: 'optimism' }).WETH,
-  },
-  gnosis: {
-    weth: Tokens.output({ network: 'gnosis' }).WETH, // wxDAI
-  },
-  bsc: {
-    weth: Tokens.output({ network: 'bsc' }).WETH, // WBNB
-  },
-  goerli: {
-    weth: Tokens.output({ network: 'goerli' }).WETH,
-  },
+  WETH,
 };
--- pkg/deployments/tasks/20220413-child-chain-gauge-factory/input.ts
+++ pkg/deployments/tasks/20220413-child-chain-gauge-factory/input.ts
@@ -8,21 +8,10 @@ export type ChildChainLiquidityGaugeFactoryDeployment = {
 
 const AuthorizerAdaptor = new Task('20220325-authorizer-adaptor', TaskMode.READ_ONLY);
 const Vault = new Task('20210418-vault', TaskMode.READ_ONLY);
-const Tokens = new Task('00000000-tokens', TaskMode.READ_ONLY);
+const BAL = new Task('00000000-tokens', TaskMode.READ_ONLY);
 
 export default {
   AuthorizerAdaptor,
   Vault,
-  polygon: {
-    BAL: Tokens.output({ network: 'polygon' }).BAL,
-  },
-  arbitrum: {
-    BAL: Tokens.output({ network: 'arbitrum' }).BAL,
-  },
-  optimism: {
-    BAL: Tokens.output({ network: 'optimism' }).BAL,
-  },
-  goerli: {
-    BAL: Tokens.output({ network: 'goerli' }).BAL,
-  },
+  BAL,
 };
