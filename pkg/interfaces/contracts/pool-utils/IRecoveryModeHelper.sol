317a4b483a7421fd3c836315617cec9cd6f83915
Juan Ignacio Ubeira <juani@balancer.finance>
2022-11-28 18:23:59 -0300

    Recovery mode helper suggestions (#2073)
    * Fix typo.
    
    * Add comment suggestion.
    
    * Add getVault to interface.

--- pkg/interfaces/contracts/pool-utils/IRecoveryModeHelper.sol
+++ pkg/interfaces/contracts/pool-utils/IRecoveryModeHelper.sol
@@ -14,6 +14,8 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "../vault/IVault.sol";
+
 /**
  * Interface for an auxiliary contract that computes Recovery Mode exits, removing logic from the core Pool contract
  * that would otherwise take up a lot of bytecode size at the cost of some slight gas overhead. Since Recovery Mode
@@ -21,8 +23,14 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 interface IRecoveryModeHelper {
     /**
+     * @notice Returns the Balancer Vault
+     */
+    function getVault() external view returns (IVault);
+
+    /**
      * @dev Computes a Recovery Mode Exit BPT and token amounts for a Pool. Only 'cash' balances are considered, to
-     * avoid scenarios where the last LPs to attempt to exit the Pool cannot because only 'managed' balance remains.
+     * avoid scenarios where the last LPs to attempt to exit the Pool cannot do it because only 'managed' balance
+     * remains.
      *
      * The Pool is assumed to be a Composable Pool that uses ComposablePoolLib, meaning BPT will be its first token. It
      * is also assumed that there is no 'managed' balance for BPT.
