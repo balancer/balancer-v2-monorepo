626160216f612e15042a1986e7f09d4ebf92dbc1
Juan Ignacio Ubeira <juani@balancer.finance>
2022-11-30 18:44:48 -0300

    Add version to managed pool.

--- pkg/pool-weighted/contracts/managed/ManagedPool.sol
+++ pkg/pool-weighted/contracts/managed/ManagedPool.sol
@@ -15,6 +15,7 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
+import "@balancer-labs/v2-interfaces/contracts/pool-utils/IVersion.sol";
 import "@balancer-labs/v2-interfaces/contracts/pool-weighted/IExternalWeightedMath.sol";
 import "@balancer-labs/v2-interfaces/contracts/pool-weighted/WeightedPoolUserData.sol";
 
@@ -43,7 +44,7 @@ import "./ManagedPoolSettings.sol";
  * rebalancing through token changes, gradual weight or fee updates, fine-grained control of protocol and
  * management fees, allowlisting of LPs, and more.
  */
-contract ManagedPool is ManagedPoolSettings {
+contract ManagedPool is IVersion, ManagedPoolSettings {
     // ManagedPool weights and swap fees can change over time: these periods are expected to be long enough (e.g. days)
     // that any timestamp manipulation would achieve very little.
     // solhint-disable not-rely-on-time
@@ -57,6 +58,7 @@ contract ManagedPool is ManagedPoolSettings {
     // conceivable real liquidity - to allow for minting new BPT as a result of regular joins.
     uint256 private constant _PREMINTED_TOKEN_BALANCE = 2**(111);
     IExternalWeightedMath private immutable _weightedMath;
+    string private _version;
 
     struct ManagedPoolParams {
         string name;
@@ -70,6 +72,7 @@ contract ManagedPool is ManagedPoolSettings {
         IExternalWeightedMath weightedMath;
         uint256 pauseWindowDuration;
         uint256 bufferPeriodDuration;
+        string version;
     }
 
     constructor(
@@ -95,6 +98,11 @@ contract ManagedPool is ManagedPoolSettings {
         ManagedPoolSettings(settingsParams, configParams.protocolFeeProvider)
     {
         _weightedMath = configParams.weightedMath;
+        _version = configParams.version;
+    }
+
+    function version() external view override returns (string memory) {
+        return _version;
     }
 
     function _getWeightedMath() internal view returns (IExternalWeightedMath) {
