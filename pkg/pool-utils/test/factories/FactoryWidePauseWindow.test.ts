a28fe4bfa548dd1f0e5db6883b3270f29a2aa4ae
Jeffrey Bennett <endymionjkb@gmail.com>
2022-12-02 17:46:27 -0500

    test: add FactoryWidePauseWindow tests

--- pkg/pool-utils/test/factories/FactoryWidePauseWindow.test.ts
+++ pkg/pool-utils/test/factories/FactoryWidePauseWindow.test.ts
@@ -11,65 +11,91 @@ describe('FactoryWidePauseWindow', function () {
   const PAUSE_WINDOW_DURATION = DAY * 90;
   const BUFFER_PERIOD_DURATION = DAY * 30;
 
-  sharedBeforeEach(async () => {
-    factory = await deploy('FactoryWidePauseWindow');
-    factoryDeployTime = await currentTimestamp();
-  });
-
-  context('before the pause window end time', () => {
-    context('at the beginning of the pause window', () => {
-      itReturnsANonZeroWindow();
+  context('with valid durations', () => {
+    sharedBeforeEach(async () => {
+      factory = await deploy('MockFactoryWidePauseWindow', { args: [PAUSE_WINDOW_DURATION, BUFFER_PERIOD_DURATION] });
+      factoryDeployTime = await currentTimestamp();
     });
 
-    context('after some time has passed', () => {
-      sharedBeforeEach('advance some time', async () => {
-        await advanceTime(PAUSE_WINDOW_DURATION / 3);
+    context('before the pause window end time', () => {
+      context('at the beginning of the pause window', () => {
+        itReturnsANonZeroWindow();
       });
 
-      itReturnsANonZeroWindow();
+      context('after some time has passed', () => {
+        sharedBeforeEach('advance some time', async () => {
+          await advanceTime(PAUSE_WINDOW_DURATION / 3);
+        });
+
+        itReturnsANonZeroWindow();
+      });
     });
+
+    context('at the pause window end time', () => {
+      sharedBeforeEach('move to pause window end time', async () => {
+        await advanceTime(PAUSE_WINDOW_DURATION);
+      });
+
+      itReturnsAZeroWindow();
+    });
+
+    context('after the pause window end time', () => {
+      sharedBeforeEach('advance time', async () => {
+        await advanceTime(PAUSE_WINDOW_DURATION * 2);
+      });
+
+      itReturnsAZeroWindow();
+    });
+
+    function itReturnsANonZeroWindow() {
+      it('returns the current pause window duration', async () => {
+        const now = await currentTimestamp();
+        const expectedDuration = bn(PAUSE_WINDOW_DURATION).sub(now.sub(factoryDeployTime));
+
+        const { pauseWindowDuration } = await factory.getPauseConfiguration();
+        expect(pauseWindowDuration).to.equal(expectedDuration);
+      });
+
+      it('returns the full buffer period duration', async () => {
+        const { bufferPeriodDuration } = await factory.getPauseConfiguration();
+        expect(bufferPeriodDuration).to.equal(BUFFER_PERIOD_DURATION);
+      });
+    }
+
+    function itReturnsAZeroWindow() {
+      it('returns a zero pause window duration', async () => {
+        const { pauseWindowDuration } = await factory.getPauseConfiguration();
+        expect(pauseWindowDuration).to.be.zero;
+      });
+
+      it('returns a zero buffer period duration', async () => {
+        const { bufferPeriodDuration } = await factory.getPauseConfiguration();
+        expect(bufferPeriodDuration).to.be.zero;
+      });
+    }
   });
 
-  context('at the pause window end time', () => {
-    sharedBeforeEach('move to pause window end time', async () => {
-      await advanceTime(PAUSE_WINDOW_DURATION);
+  context('with invalid durations', () => {
+    let maxPauseWindow: number;
+    let maxBuffer: number;
+
+    sharedBeforeEach(async () => {
+      factory = await deploy('MockFactoryWidePauseWindow', { args: [PAUSE_WINDOW_DURATION, BUFFER_PERIOD_DURATION] });
+
+      maxPauseWindow = await factory.getMaxPauseWindowDuration();
+      maxBuffer = await factory.getMaxBufferPeriodDuration();
     });
 
-    itReturnsAZeroWindow();
+    it('rejects a pause window duration above the max', async () => {
+      await expect(
+        deploy('MockFactoryWidePauseWindow', { args: [maxPauseWindow + 1, BUFFER_PERIOD_DURATION] })
+      ).to.be.revertedWith('MAX_PAUSE_WINDOW_DURATION');
+    });
+
+    it('rejects a buffer duration above the max', async () => {
+      await expect(
+        deploy('MockFactoryWidePauseWindow', { args: [PAUSE_WINDOW_DURATION, maxBuffer + 1] })
+      ).to.be.revertedWith('MAX_BUFFER_PERIOD_DURATION');
+    });
   });
-
-  context('after the pause window end time', () => {
-    sharedBeforeEach('advance time', async () => {
-      await advanceTime(PAUSE_WINDOW_DURATION * 2);
-    });
-
-    itReturnsAZeroWindow();
-  });
-
-  function itReturnsANonZeroWindow() {
-    it('returns the current pause window duration', async () => {
-      const now = await currentTimestamp();
-      const expectedDuration = bn(PAUSE_WINDOW_DURATION).sub(now.sub(factoryDeployTime));
-
-      const { pauseWindowDuration } = await factory.getPauseConfiguration();
-      expect(pauseWindowDuration).to.equal(expectedDuration);
-    });
-
-    it('returns the full buffer period duration', async () => {
-      const { bufferPeriodDuration } = await factory.getPauseConfiguration();
-      expect(bufferPeriodDuration).to.equal(BUFFER_PERIOD_DURATION);
-    });
-  }
-
-  function itReturnsAZeroWindow() {
-    it('returns a zero pause window duration', async () => {
-      const { pauseWindowDuration } = await factory.getPauseConfiguration();
-      expect(pauseWindowDuration).to.be.zero;
-    });
-
-    it('returns a zero buffer period duration', async () => {
-      const { bufferPeriodDuration } = await factory.getPauseConfiguration();
-      expect(bufferPeriodDuration).to.be.zero;
-    });
-  }
 });
