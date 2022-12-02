204e0f9176ee07e5a43b39bd2fd73b2b2abc7eef
gerg <gerrrrrrrg@gmail.com>
2022-12-02 13:41:22 -0500

    remove signers.ts dependency on the private v2-helpers numbers package

--- pkg/deployments/src/signers.ts
+++ pkg/deployments/src/signers.ts
@@ -2,7 +2,6 @@ import { BigNumber } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 
 import { impersonateAccount, setBalance as setAccountBalance } from '@nomicfoundation/hardhat-network-helpers';
-import { fp } from '@balancer-labs/v2-helpers/src/numbers';
 
 export async function getSigners(): Promise<SignerWithAddress[]> {
   const { ethers } = await import('hardhat');
@@ -13,7 +12,7 @@ export async function getSigner(index = 0): Promise<SignerWithAddress> {
   return (await getSigners())[index];
 }
 
-export async function impersonate(address: string, balance = fp(100)): Promise<SignerWithAddress> {
+export async function impersonate(address: string, balance = BigNumber.from(100e18)): Promise<SignerWithAddress> {
   await impersonateAccount(address);
   await setBalance(address, balance);
 
