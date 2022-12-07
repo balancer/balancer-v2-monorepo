21abc0c0153a43fa32eb9f1ee73cfcf817d69bd2
Nicolás Venturo <nicolas.venturo@gmail.com>
2022-12-07 16:02:56 -0300

    Make protocol id indexed (#2123)

--- pkg/pool-linear/contracts/aave/AaveLinearPoolFactory.sol
+++ pkg/pool-linear/contracts/aave/AaveLinearPoolFactory.sol
@@ -55,7 +55,7 @@ contract AaveLinearPoolFactory is
 
     // This event allows off-chain tools to differentiate between different protocols that use this factory
     // to deploy Aave Linear Pools.
-    event AaveLinearPoolCreated(address indexed pool, uint256 protocolId);
+    event AaveLinearPoolCreated(address indexed pool, uint256 indexed protocolId);
 
     // Record protocol ID registrations.
     event AaveLinearPoolProtocolIdRegistered(uint256 indexed protocolId, string name);
