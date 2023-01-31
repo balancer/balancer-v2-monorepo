b9eb5aa534d2098ec30afa6a9f312b38bde06b65
Juan Ignacio Ubeira <juani@balancer.finance>
2023-01-31 10:33:54 -0300

    Document `getActualSupply`.

--- pkg/pool-weighted/contracts/WeightedPool.sol
+++ pkg/pool-weighted/contracts/WeightedPool.sol
@@ -15,6 +15,8 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
+import "@balancer-labs/v2-pool-utils/contracts/lib/VaultReentrancyLib.sol";
+
 import "./BaseWeightedPool.sol";
 import "./WeightedPoolProtocolFees.sol";
 
@@ -367,6 +369,13 @@ contract WeightedPool is BaseWeightedPool, WeightedPoolProtocolFees {
      * even if they don't yet exist, they will effectively be included in any Pool operation that involves BPT.
      *
      * In the vast majority of cases, this function should be used instead of `totalSupply()`.
+     *
+     * **IMPORTANT NOTE**: calling this function within a Vault context (i.e. in the middle of a join or an exit) is
+     * potentially unsafe, since the returned value may be incorrect. It is up to the caller to protect itself.
+     *
+     * This is because this function calculates the invariant, which requires the state of the pool to be in sync
+     * with the state of the vault. That condition may not be true in the middle of a join or an exit, which is why
+     * the value returned by this function under that circumstance could be incorrect.
      */
     function getActualSupply() external view returns (uint256) {
         uint256 supply = totalSupply();
