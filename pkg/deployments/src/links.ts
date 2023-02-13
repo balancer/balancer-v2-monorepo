47738f6f8e2a69aaae0d42db1643c88bfd630b06
Jeffrey Bennett <endymionjkb@gmail.com>
2023-02-13 12:23:51 -0500

    lint

--- pkg/deployments/src/links.ts
+++ pkg/deployments/src/links.ts
@@ -38,7 +38,7 @@ export function checkLinks(task: Task): void {
       logger.success(`Verified ${linkCnt} links in task '${task.id}'`);
     }
   } else {
-    const isTopLevelFolder = ["deprecated", "scripts"].includes(path.basename(task.dir()));
+    const isTopLevelFolder = ['deprecated', 'scripts'].includes(path.basename(task.dir()));
     if (!isTopLevelFolder) {
       throw Error(`Missing readme.md for task '${task.id}'`);
     }
