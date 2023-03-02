644e655555d091079ac3e40949be56e13edf1083
João Bruno Abou Hatem de Liz <joaobrunoah@gmail.com>
2023-03-02 18:05:06 -0300

    Batch relayer Gearbox (#2301)
    * BatchRelayer Gearbox
    
    * Fixing Gearbox Batch Relayer Unit Tests
    
    * Fixing Gearbox Relayer tests
    
    * Removing only from Gearbox test
    
    * Removing "only" from Gearbox tests
    
    * Fixing StablePoolEncoder on tests
    
    * Auto stash before merge of "master" and "origin/master"
    
    Replacing mint for safeTransferFrom in the mocked gearbox vault
    
    * Commiting Batch Relayer fork-tests
    
    * Implementing Gearbox tests
    
    * Separating fork tests from each relayer
    
    * Finishing Gearbox Wrapping tests
    
    * Adding comments to explain Gearbox Wrapping, fixing Mock contract
    
    * Fixing Gearbox Tests
    
    * Extending DieselToken interface from IERC20, to use same class on fork tests
    
    * Removing deployment task from Gearbox Batch Relayer PR
    
    * Change burn function from test token to don`t collide with existing burn function from ERC20Burnable contract
    
    * Adding exitPool tests to gearbox wrapping
    
    * Fixing exitPool tests (Encoder needs the exit token index without BPT)
    
    * Update pkg/interfaces/contracts/standalone-utils/IGearboxDieselToken.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Describing removeLiquidity
    
    * Update pkg/interfaces/contracts/standalone-utils/IGearboxDieselToken.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    ---------
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/solidity-utils/contracts/test/TestToken.sol
+++ pkg/solidity-utils/contracts/test/TestToken.sol
@@ -30,4 +30,12 @@ contract TestToken is ERC20, ERC20Burnable, ERC20Permit {
     function mint(address recipient, uint256 amount) external {
         _mint(recipient, amount);
     }
+
+    // burnWithoutAllowance was created to allow burn of token without approval. Example of use:
+    //
+    // MockGearboxVault.sol can't use burnFrom function (from ERC20Burnable) in unit tests, since
+    // MockGearboxVault doesn't have permission to burn relayer wrapped tokens and relayer is not a Signer
+    function burnWithoutAllowance(address sender, uint256 amount) external {
+        _burn(sender, amount);
+    }
 }
