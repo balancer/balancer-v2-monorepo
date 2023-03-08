99fe4856e0245d39c2cd0e61c28af928145504d8
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2023-03-08 19:58:50 +0100

    Euler batch relayer (#2327)
    * add euler-wrapping capabilities to batch-relayer
    
    * preliminary progress on EulerWrapping
    
    * `EulerWrapping` to work with actual balances
    
    * add `almostEqual` assertion
    
    * lint & cleanup
    
    * handle withdraw flow on mockEulerProtocol
    
    * new build info & cleanup
    
    * `refactor`: IEulerToken inherits from IERC20
    
    * `test`: adjust tests to IERC20 inheritance
    
    * `test`: Add constructor args due to Euler addition
    
    * `test`: Add constructor args due to Euler addition
    
    * `style`: lint & remove unnecessary import
    
    * `refactor`: remove batch-relayer deployment task
    
    * `refactor`: remove batch-relayer deployment task
    
    * `refactor`: add FixedPointMath & cleanup
    
    * `style`: use TypesConverter in expectTransferEvent
    
    * `fix`: add token burn event
    
    * `refactor`: remove private var and compiler warning
    
    * `feat`: allow higher exchangeRates
    
    * `feat`: allow user to specify amount to withdraw
    
    * `docs`: improve documentation clarity
    
    * `test`: add exitPool
    
    * `refactor:` use safeTransfer
    
    * `refactor`: change max uint256 representation
    
    * `test`: args to account for relayer constructor
    
    * `lint`: lint
    
    * `lint`: lint
    
    * `feat`: allow passing of euler for relayer approval
    
    * `refactor`: change tests for relayer constructor
    
    * `lint`: move link outside NatSpec
    
    * `test`: remove not required arg

--- pkg/standalone-utils/contracts/BatchRelayerLibrary.sol
+++ pkg/standalone-utils/contracts/BatchRelayerLibrary.sol
@@ -19,6 +19,7 @@ import "./relayer/BaseRelayerLibrary.sol";
 
 import "./relayer/AaveWrapping.sol";
 import "./relayer/ERC4626Wrapping.sol";
+import "./relayer/EulerWrapping.sol";
 import "./relayer/GaugeActions.sol";
 import "./relayer/GearboxWrapping.sol";
 import "./relayer/LidoWrapping.sol";
@@ -39,6 +40,7 @@ contract BatchRelayerLibrary is
     AaveWrapping,
     BaseRelayerLibrary,
     ERC4626Wrapping,
+    EulerWrapping,
     GaugeActions,
     GearboxWrapping,
     LidoWrapping,
