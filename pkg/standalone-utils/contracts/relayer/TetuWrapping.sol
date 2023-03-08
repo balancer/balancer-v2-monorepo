0714602a2a42d933c4c5a6139a9e70547c2714b2
João Bruno Abou Hatem de Liz <joaobrunoah@gmail.com>
2023-03-07 23:36:36 -0300

    Batch Relayer: Tetu (#2318)
    * Adding Tetu batch relayer
    
    * Adapting withdraw and deposit functions to don`t need to calculate rate in multiple places
    
    * Fixing tetu batch relayer tests
    
    * Removing "only" from test
    
    * Fixing StablePoolEncoder on tests
    
    * Refactoring mock of unwrap token to use safeTransferFrom
    
    * Fork test setup
    
    * Tetu batch relayer fork tests
    
    * Fixing Tetu Relayer
    
    * Removing deployment task from Tetu Batch Relayer PR
    
    * Organizing Tetu Wrapping test
    
    * Change burn function from test token to don`t collide with existing burn function from ERC20Burnable contract
    
    * Implementing exit pool test for Tetu Wrapping
    
    * Fixing tetu wrapping logic to remove reentrancy issue
    
    * Removing TetuController
    
    * Update pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Fixing deposit function for Tetu Wrapping
    
    * Fixint Tetu Wrapping tests after deposit function was modified
    
    * Replacing safeTransferFrom to safeTransfer
    
    * Fixing setRate function in the mocked Tetu's smart vault
    
    * Removing ExternalCallLib from relayer library
    
    * Overriding mint and burn functions of Tetu's token vault, so that balances are always recalculated and rate is adjusted to new total supply
    
    * Update pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Fixing _fromTetu and _toTetu functions
    
    * Fixing Tetu Wrapping
    
    * Update pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/standalone-utils/contracts/test/MockTetuSmartVault.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/standalone-utils/contracts/test/MockTetuSmartVault.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Removing casts of ITetuSmartVault (using token instead of address)
    
    * Removing address from IERC20 casting, since ITetuSmartVault inherits IERC20
    
    * Removing not necessary cast to IERC20
    
    * Fix Lint
    
    * Update pkg/standalone-utils/contracts/relayer/special/TetuShareValueHelper.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Moving mock library to correct folder
    
    ---------
    
    Co-authored-by: baileyspraggins <baileyspraggins@me.com>
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

---
+++ pkg/standalone-utils/contracts/relayer/TetuWrapping.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@balancer-labs/v2-interfaces/contracts/standalone-utils/ITetuSmartVault.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
+
+import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Address.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/SafeERC20.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/math/FixedPoint.sol";
+import "./IBaseRelayerLibrary.sol";
+
+/**
+ * @title TetuWrapping
+ * @notice Allows users to wrap and unwrap Tetu tokens
+ * @dev All functions must be payable so they can be called from a multicall involving ETH
+ */
+abstract contract TetuWrapping is IBaseRelayerLibrary {
+    using Address for address payable;
+    using SafeERC20 for IERC20;
+    using FixedPoint for uint256;
+
+    function wrapTetu(
+        ITetuSmartVault wrappedToken,
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 outputReference
+    ) external payable {
+        if (_isChainedReference(amount)) {
+            amount = _getChainedReferenceValue(amount);
+        }
+
+        IERC20 underlying = IERC20(wrappedToken.underlying());
+
+        // The wrap caller is the implicit sender of tokens, so if the goal is for the tokens
+        // to be sourced from outside the relayer, we must first pull them here.
+        if (sender != address(this)) {
+            require(sender == msg.sender, "Incorrect sender");
+            _pullToken(sender, underlying, amount);
+        }
+
+        underlying.safeApprove(address(wrappedToken), amount);
+        wrappedToken.deposit(amount);
+        uint256 receivedWrappedAmount = wrappedToken.balanceOf(address(this));
+
+        if (recipient != address(this)) {
+            IERC20(wrappedToken).safeTransfer(recipient, receivedWrappedAmount);
+        }
+
+        if (_isChainedReference(outputReference)) {
+            _setChainedReferenceValue(outputReference, receivedWrappedAmount);
+        }
+    }
+
+    function unwrapTetu(
+        ITetuSmartVault wrappedToken,
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 outputReference
+    ) external payable {
+        if (_isChainedReference(amount)) {
+            amount = _getChainedReferenceValue(amount);
+        }
+
+        // The unwrap caller is the implicit sender of tokens, so if the goal is for the tokens
+        // to be sourced from outside the relayer, we must first pull them here.
+        if (sender != address(this)) {
+            require(sender == msg.sender, "Incorrect sender");
+            _pullToken(sender, wrappedToken, amount);
+        }
+
+        IERC20 mainToken = IERC20(wrappedToken.underlying());
+        wrappedToken.withdraw(amount);
+        uint256 withdrawnMainAmount = mainToken.balanceOf(address(this));
+
+        if (recipient != address(this)) {
+            mainToken.safeTransfer(recipient, withdrawnMainAmount);
+        }
+
+        if (_isChainedReference(outputReference)) {
+            _setChainedReferenceValue(outputReference, withdrawnMainAmount);
+        }
+    }
+}
