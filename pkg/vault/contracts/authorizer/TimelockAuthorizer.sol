674fa5849f9079872533f54fd8325076c834dc24
Igor Yalovoy <to@ylv.io>
2023-03-14 19:42:16 -0300

    Update pkg/vault/contracts/authorizer/TimelockAuthorizer.sol
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- pkg/vault/contracts/authorizer/TimelockAuthorizer.sol
+++ pkg/vault/contracts/authorizer/TimelockAuthorizer.sol
@@ -1184,7 +1184,7 @@ contract TimelockAuthorizer is IAuthorizer, ReentrancyGuard {
         );
 
         for (uint256 i = 0; i < executors.length; i++) {
-            require(!_isExecutor[scheduledExecutionId][executors[i]], "ALREADY_AN_EXECUTOR");
+            require(!_isExecutor[scheduledExecutionId][executors[i]], "DUPLICATE_EXECUTORS");
             _isExecutor[scheduledExecutionId][executors[i]] = true;
             emit ExecutorAdded(scheduledExecutionId, executors[i]);
         }
