c42ed0beb8bec46804b3dfc1ae3ea3e977116c41
Igor Yalovoy <to@ylv.io>
2023-03-15 16:16:49 -0600

    Split TimelockAuthorizer into TimelockAuthorizer and TimelockAuthorizerManagement (#2349)
    * Split TimelockAuthorizer into TimelockAuthorizer and TimelockAuthorizerManagement
    
    * Lint
    
    * Move events
    
    * Move consts
    
    * Lint
    
    * Add TimelockAuthorizerManagement interface
    
    * More comments
    
    * Split ITimelockAuthorizer inteface
    
    * Update pkg/vault/contracts/authorizer/TimelockAuthorizerManagement.sol
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>
    
    * Update pkg/vault/contracts/authorizer/TimelockAuthorizerManagement.sol
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>
    
    * Update pkg/vault/contracts/authorizer/TimelockAuthorizerManagement.sol
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>
    
    * Add events
    
    * Remove events
    
    * Remove events
    
    * Fix SENDER_IS_NOT_EXECUTION_HELPER issue
    
    * Fix tests
    
    * Use only ITimelockAuthorizer; Add consts to the interface
    
    * Use @inheritdoc for implementatino NatSpec
    
    * Fix inheritdoc
    
    * Fix const naming
    
    * Merge with master
    
    ---------
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- pkg/vault/test/authorizer/TimelockAuthorizer.test.ts
+++ pkg/vault/test/authorizer/TimelockAuthorizer.test.ts
@@ -1459,9 +1459,7 @@ describe('TimelockAuthorizer', () => {
                   const id = await schedule();
                   await advanceTime(delay);
 
-                  await expect(authorizer.execute(id, { from: granter })).to.be.revertedWith(
-                    'SENDER_IS_NOT_EXECUTION_HELPER'
-                  );
+                  await expect(authorizer.execute(id, { from: granter })).to.be.revertedWith('SENDER_IS_NOT_EXECUTOR');
 
                   const receipt = await authorizer.execute(id, { from: executors[0] });
                   expectEvent.inReceipt(await receipt.wait(), 'ExecutionExecuted', { scheduledExecutionId: id });
@@ -1667,9 +1665,7 @@ describe('TimelockAuthorizer', () => {
             id = await schedule();
             await advanceTime(delay);
 
-            await expect(authorizer.execute(id, { from: granter })).to.be.revertedWith(
-              'SENDER_IS_NOT_EXECUTION_HELPER'
-            );
+            await expect(authorizer.execute(id, { from: granter })).to.be.revertedWith('SENDER_IS_NOT_EXECUTOR');
           });
         });
       });
