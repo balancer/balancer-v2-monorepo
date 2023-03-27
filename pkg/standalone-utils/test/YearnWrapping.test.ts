29cf18e5c76f0fc84d15c1006d4856e1a1075050
Igor Yalovoy <to@ylv.io>
2023-03-27 13:22:14 -0600

    Fix tests

--- pkg/standalone-utils/test/YearnWrapping.test.ts
+++ pkg/standalone-utils/test/YearnWrapping.test.ts
@@ -65,11 +65,8 @@ describe('YearnWrapping', function () {
         actionId(vault.instance, action)
       )
     );
-    await Promise.all(
-      relayerActionIds.map((action) => {
-        vault.grantPermissionGlobally(action, relayer);
-      })
-    );
+    await Promise.all(relayerActionIds.map((action) => vault.grantPermissionGlobally(action, relayer)));
+
 
     // Approve relayer by sender
     await vault.setRelayerApproval(senderUser, relayer, true);
