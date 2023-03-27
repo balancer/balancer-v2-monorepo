853dad90430dfd7c588d0856ebc6947b873e87c6
Igor Yalovoy <to@ylv.io>
2023-03-27 14:01:51 -0600

    lint

--- pkg/standalone-utils/test/YearnWrapping.test.ts
+++ pkg/standalone-utils/test/YearnWrapping.test.ts
@@ -67,7 +67,6 @@ describe('YearnWrapping', function () {
     );
     await Promise.all(relayerActionIds.map((action) => vault.grantPermissionGlobally(action, relayer)));
 
-
     // Approve relayer by sender
     await vault.setRelayerApproval(senderUser, relayer, true);
   });
