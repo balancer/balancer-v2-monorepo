c905a7451bb1561200f036e59e13d37cb2337648
Jeffrey Bennett <endymionjkb@gmail.com>
2023-04-01 00:54:57 -0400

    docs: constrcutor typo and light edits

--- pkg/deployments/src/task.ts
+++ pkg/deployments/src/task.ts
@@ -152,19 +152,21 @@ export default class Task {
   }
 
   async check(name: string, args: Array<Param> = [], libs?: Libraries): Promise<Contract> {
-    // There's multiple approaches to checking that a deployed contract matches known source code. A naive approach is
-    // to check for a match in the runtime code, but that doesn't account for actions taken during  construction,
-    // including calls, storage writes and setting immutable state variables. Since immutable state variables modify the
-    // runtime code, it can be actually quite tricky to produce a matching runtime code.
-    // What we do instead is check for both runtime code and constrcutor execution (including constructor arguments) by
-    // looking at the transaction in which the contract was deployed. The data of said transaction will be the contract
+    // There are multiple approaches to checking that a deployed contract matches known source code. A naive approach
+    // is to check for a match in the runtime code, but that doesn't account for actions taken during construction,
+    // including calls, storage writes, and setting immutable state variables. Since immutable state variables modify
+    // the runtime code, it can actually be quite tricky to produce matching runtime code.
+    //
+    // What we do instead is check for both runtime code and constructor execution (including constructor arguments) by
+    // looking at the transaction in which the contract was deployed. The data of this transaction will be the contract
     // creation code followed by the abi-encoded constructor arguments, which we can compare against what the task would
     // attempt to deploy. In this way, we are testing the task's build info, inputs and deployment code.
+    //
     // The only thing we're not checking is what account deployed the contract, but our code does not have dependencies
     // on the deployer.
-
+    //
     // The only problem with the approach described above is that it is not easy to find the transaction in which a
-    // contract is deployed. Tenderly has a dedicated endpoint for this however.
+    // contract is deployed. However, Tenderly has a dedicated endpoint for this.
 
     const { ethers } = await import('hardhat');
 
