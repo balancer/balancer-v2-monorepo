16857fffca876f6fce9c1144c470970b83815cdf
Joao Bruno <joaobrunoah@gmail.com>
2023-04-10 11:40:41 -0300

    Fixing tests from deprecated pool factory

--- pkg/deployments/tasks/deprecated/20230206-erc4626-linear-pool-v3/input.ts
+++ pkg/deployments/tasks/deprecated/20230206-erc4626-linear-pool-v3/input.ts
@@ -1,4 +1,4 @@
-import Task, { TaskMode } from '../../src/task';
+import Task, { TaskMode } from '../../../src/task';
 import { MONTH } from '@balancer-labs/v2-helpers/src/time';
 
 export type ERC4626LinearPoolDeployment = {
