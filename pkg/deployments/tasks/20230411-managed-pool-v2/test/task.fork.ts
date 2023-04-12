8061ac2671a9aa6c03d424dcdb838d406f87e1f6
Jeffrey Bennett <endymionjkb@gmail.com>
2023-04-12 14:17:57 -0400

    test: extend fork test (still WIP)

--- pkg/deployments/tasks/20230411-managed-pool-v2/test/task.fork.ts
+++ pkg/deployments/tasks/20230411-managed-pool-v2/test/task.fork.ts
@@ -3,19 +3,20 @@ import { expect } from 'chai';
 import { Contract } from 'ethers';
 import { BasePoolEncoder, SwapKind, toNormalizedWeights, WeightedPoolEncoder } from '@balancer-labs/balancer-js';
 import * as expectEvent from '@balancer-labs/v2-helpers/src/test/expectEvent';
-import { fp } from '@balancer-labs/v2-helpers/src/numbers';
+import { bn, fp } from '@balancer-labs/v2-helpers/src/numbers';
 import { expectEqualWithError } from '@balancer-labs/v2-helpers/src/test/relativeError';
 import { actionId } from '@balancer-labs/v2-helpers/src/models/misc/actions';
-import { MAX_UINT256, ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
+import { MAX_UINT256, ONES_BYTES32, ZERO_ADDRESS, ZERO_BYTES32 } from '@balancer-labs/v2-helpers/src/constants';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import {
   ManagedPoolParams,
   ManagedPoolSettingsParams,
 } from '@balancer-labs/v2-helpers/src/models/pools/weighted/types';
 import { ProtocolFee } from '@balancer-labs/v2-helpers/src/models/vault/types';
-
+import { sharedBeforeEach } from '@balancer-labs/v2-common/sharedBeforeEach';
 import { getSigner, impersonate, getForkedNetwork, Task, TaskMode, describeForkTest } from '../../../src';
 import { randomBytes } from 'ethers/lib/utils';
+import { deploy } from '@balancer-labs/v2-helpers/src/contract';
 
 describeForkTest('ManagedPoolFactory', 'mainnet', 17033100, function () {
   let owner: SignerWithAddress, whale: SignerWithAddress, govMultisig: SignerWithAddress;
@@ -28,6 +29,14 @@ describeForkTest('ManagedPoolFactory', 'mainnet', 17033100, function () {
     math: Contract;
 
   let task: Task;
+
+  enum AttackType {
+    SET_MANAGEMENT_AUM_FEE,
+    COLLECT_AUM_MANAGEMENT_FEES,
+    ADD_TOKEN,
+    REMOVE_TOKEN,
+    UPDATE_PROTOCOL_FEE_CACHE,
+  }
 
   const COMP = '0xc00e94cb662c3520282e6f5717214004a7f26888';
   const UNI = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984';
@@ -73,10 +82,10 @@ describeForkTest('ManagedPoolFactory', 'mainnet', 17033100, function () {
     aave = await task.instanceAt('IERC20', AAVE);
   });
 
-  async function createPool(swapEnabled = true, mustAllowlistLPs = false): Promise<Contract> {
+  async function createPool(swapEnabled = true, mustAllowlistLPs = false, saltParam = ''): Promise<Contract> {
     const assetManagers: string[] = Array(tokens.length).fill(ZERO_ADDRESS);
     assetManagers[0] = owner.address;
-    const salt = randomBytes(32);
+    const salt = saltParam == '' ? randomBytes(32) : saltParam;
 
     const newPoolParams: ManagedPoolParams = {
       name: NAME,
@@ -102,7 +111,31 @@ describeForkTest('ManagedPoolFactory', 'mainnet', 17033100, function () {
     return task.instanceAt('ManagedPool', event.args.pool);
   }
 
-  describe('create and swap', () => {
+  describe('getters', () => {
+    it('check factory version', async () => {
+      const expectedFactoryVersion = {
+        name: 'ManagedPoolFactory',
+        version: 2,
+        deployment: '20230411-managed-pool-v2',
+      };
+
+      expect(await factory.version()).to.equal(JSON.stringify(expectedFactoryVersion));
+    });
+
+    it('check pool version', async () => {
+      const pool = await createPool();
+
+      const expectedPoolVersion = {
+        name: 'ManagedPool',
+        version: 2,
+        deployment: '20230411-managed-pool-v2',
+      };
+
+      expect(await pool.version()).to.equal(JSON.stringify(expectedPoolVersion));
+    });
+  });
+
+  describe('create and swap/join/exit', () => {
     let pool: Contract;
     let poolId: string;
 
@@ -161,6 +194,118 @@ describeForkTest('ManagedPoolFactory', 'mainnet', 17033100, function () {
       expectEqualWithError(await comp.balanceOf(owner.address), 0, 0.0001);
       expectEqualWithError(await uni.balanceOf(owner.address), expectedUNI, 0.1);
     });
+
+    it('joins proportionally', async () => {
+      const ownerBptBalance = await pool.balanceOf(owner.address);
+      const bptOut = ownerBptBalance.div(5);
+
+      const { tokens: registeredTokens } = await vault.getPoolTokens(poolId);
+
+      await vault.connect(whale).joinPool(poolId, whale.address, whale.address, {
+        assets: registeredTokens,
+        maxAmountsIn: Array(tokens.length + 1).fill(MAX_UINT256),
+        fromInternalBalance: false,
+        userData: WeightedPoolEncoder.joinAllTokensInForExactBPTOut(bptOut),
+      });
+
+      // Make sure received BPT is close to what we expect
+      const currentBptBalance = await pool.balanceOf(whale.address);
+      expect(currentBptBalance).to.be.equalWithError(bptOut, 0.001);
+    });
+
+    it('exits proportionally', async () => {
+      const previousBptBalance = await pool.balanceOf(whale.address);
+      const bptIn = previousBptBalance.div(4);
+
+      const { tokens: registeredTokens } = await vault.getPoolTokens(poolId);
+
+      await vault.connect(whale).exitPool(poolId, whale.address, whale.address, {
+        assets: registeredTokens,
+        minAmountsOut: Array(registeredTokens.length).fill(0),
+        fromInternalBalance: false,
+        userData: WeightedPoolEncoder.exitExactBPTInForTokensOut(bptIn),
+      });
+
+      // Make sure sent BPT is close to what we expect
+      const currentBptBalance = await pool.balanceOf(whale.address);
+      expect(currentBptBalance).to.be.equalWithError(bn(previousBptBalance).sub(bptIn), 0.001);
+    });
+  });
+
+  describe('create2 functionality', () => {
+    it('can be deployed with a different salt', async () => {
+      const pool = await createPool(true, false, ZERO_BYTES32);
+      const pool2 = await createPool(true, false, ONES_BYTES32);
+
+      expect(pool2.address).to.not.equal(pool.address);
+    });
+  });
+
+  describe('read-only reentrancy protection', () => {
+    let pool: Contract;
+    let poolId: string;
+    let attacker: Contract;
+    const attackerFunds = fp(1000);
+
+    sharedBeforeEach('deploy and fund attacker', async () => {
+      attacker = await deploy('ReadOnlyReentrancyAttackerMP', { args: [vault.address] });
+      await comp.connect(whale).transfer(attacker.address, attackerFunds);
+      await uni.connect(whale).transfer(attacker.address, attackerFunds);
+      await aave.connect(whale).transfer(attacker.address, attackerFunds);
+    });
+
+    sharedBeforeEach('deploy pool and attacker', async () => {
+      pool = await createPool();
+      poolId = await pool.getPoolId();
+
+      await comp.connect(whale).approve(vault.address, MAX_UINT256);
+      await uni.connect(whale).approve(vault.address, MAX_UINT256);
+      await aave.connect(whale).approve(vault.address, MAX_UINT256);
+
+      const userData = WeightedPoolEncoder.joinInit(initialBalances);
+      // This is a composable pool, so assets array has to contain BPT.
+      await vault.connect(whale).joinPool(poolId, whale.address, owner.address, {
+        assets: [pool.address, ...tokens],
+        maxAmountsIn: [MAX_UINT256, ...initialBalances],
+        fromInternalBalance: false,
+        userData,
+      });
+    });
+
+    describe.skip('survives attacks', async () => {
+      it(`rejects fee update attack`, async () => {
+        await performAttack(AttackType.SET_MANAGEMENT_AUM_FEE);
+      });
+
+      it(`rejects fee collection attack`, async () => {
+        await performAttack(AttackType.COLLECT_AUM_MANAGEMENT_FEES);
+      });
+
+      it(`rejects add token attack`, async () => {
+        await performAttack(AttackType.ADD_TOKEN);
+      });
+
+      it(`rejects remove token attack`, async () => {
+        await performAttack(AttackType.REMOVE_TOKEN);
+      });
+
+      it(`rejects protocol fee cache attack`, async () => {
+        await performAttack(AttackType.UPDATE_PROTOCOL_FEE_CACHE);
+      });
+    });
+
+    async function performAttack(attackType: AttackType) {
+      const bptOut = fp(1);
+
+      const joinRequest = {
+        assets: [pool.address, ...tokens],
+        maxAmountsIn: Array(tokens.length).fill(MAX_UINT256),
+        userData: WeightedPoolEncoder.joinAllTokensInForExactBPTOut(bptOut),
+        fromInternalBalance: false,
+      };
+
+      await expect(attacker.startAttack(poolId, joinRequest, attackType, { value: 10 })).to.be.revertedWith('BAL#420');
+    }
   });
 
   describe('recovery mode', () => {
