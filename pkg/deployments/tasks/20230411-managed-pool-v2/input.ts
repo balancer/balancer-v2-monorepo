3142a07d9d519a49fd9dc17db41c87f16fc2a721
Jeffrey Bennett <endymionjkb@gmail.com>
2023-04-13 14:11:55 -0400

    feat: add mock managed pool deployment

--- pkg/deployments/tasks/20230411-managed-pool-v2/input.ts
+++ pkg/deployments/tasks/20230411-managed-pool-v2/input.ts
@@ -8,11 +8,15 @@ export type ManagedPoolDeployment = {
   PoolVersion: string;
   InitialPauseWindowDuration: number;
   BufferPeriodDuration: number;
+  WETH: string;
+  BAL: string;
 };
 
 const Vault = new Task('20210418-vault', TaskMode.READ_ONLY);
 const ProtocolFeePercentagesProvider = new Task('20220725-protocol-fee-percentages-provider', TaskMode.READ_ONLY);
 const BaseVersion = { version: 2, deployment: '20230411-managed-pool-v2' };
+const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
+const BAL = new Task('00000000-tokens', TaskMode.READ_ONLY);
 
 // Since these pools have many experimental features, use a longer pause period.
 const extendedPauseWindowDuration = MONTH * 9;
@@ -24,4 +28,6 @@ export default {
   PoolVersion: JSON.stringify({ name: 'ManagedPool', ...BaseVersion }),
   InitialPauseWindowDuration: extendedPauseWindowDuration,
   BufferPeriodDuration: MONTH,
+  WETH,
+  BAL,
 };
