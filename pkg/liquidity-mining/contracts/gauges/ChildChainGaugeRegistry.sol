d20129639c78c7dfb31deb2f9ca025f69f22f58b
Igor Yalovoy <to@ylv.io>
2023-04-14 13:49:54 -0600

    Add import

--- pkg/liquidity-mining/contracts/gauges/ChildChainGaugeRegistry.sol
+++ pkg/liquidity-mining/contracts/gauges/ChildChainGaugeRegistry.sol
@@ -15,6 +15,8 @@
 pragma solidity ^0.7.0;
 
 import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/ILiquidityGaugeFactory.sol";
+import "@balancer-labs/v2-interfaces/contracts/liquidity-mining/IChildChainGauge.sol";
+
 import "@balancer-labs/v2-solidity-utils/contracts/helpers/SingletonAuthentication.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/EnumerableSet.sol";
 
