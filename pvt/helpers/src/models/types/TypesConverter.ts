4eb0bee0104eec21b1aef7aba799ed1dae6d85b6
Jeffrey Bennett <endymionjkb@gmail.com>
2023-04-24 20:44:16 -0400

    lint

--- pvt/helpers/src/models/types/TypesConverter.ts
+++ pvt/helpers/src/models/types/TypesConverter.ts
@@ -10,11 +10,7 @@ import { Account } from './types';
 import { ProtocolFee, RawVaultDeployment, VaultDeployment } from '../vault/types';
 import { RawLinearPoolDeployment, LinearPoolDeployment } from '../pools/linear/types';
 import { RawStablePoolDeployment, StablePoolDeployment } from '../pools/stable/types';
-import {
-  RawWeightedPoolDeployment,
-  WeightedPoolDeployment,
-  WeightedPoolType,
-} from '../pools/weighted/types';
+import { RawWeightedPoolDeployment, WeightedPoolDeployment, WeightedPoolType } from '../pools/weighted/types';
 import {
   RawTokenApproval,
   RawTokenMint,
