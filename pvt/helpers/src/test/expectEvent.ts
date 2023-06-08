d72a62ddd690a094f8490995446fc5b03cb74a4d
Juan Ignacio Ubeira <juani@balancer.finance>
2023-06-07 21:20:32 -0300

    Improve docs.

--- pvt/helpers/src/test/expectEvent.ts
+++ pvt/helpers/src/test/expectEvent.ts
@@ -42,6 +42,19 @@ export function inReceipt(receipt: ContractReceipt, eventName: string, eventArgs
   return event;
 }
 
+/**
+ * Throws error if the given receipt does not contain a set of events with specific arguments.
+ * Expecting a specific amount of events from a particular address is optional.
+ * @param receipt Receipt to analyze.
+ * @param emitter Interface of the contract emitting the event(s).
+ * @param eventName Name of the event(s).
+ * @param eventArgs Arguments of the event(s). This does not need to be a complete list; as long as the event contains
+ *  the specified ones, the function will not throw.
+ * @param address Contract address that emits the event(s). If undefined, the logs will not be filtered by address.
+ * @param amount Amount of expected events that match all the specified conditions. If not specified, at least one is
+ *  expected.
+ * @returns First matching event if the amount is not specified, all matching events otherwise.
+ */
 export function inIndirectReceipt(
   receipt: ContractReceipt,
   emitter: Interface,
