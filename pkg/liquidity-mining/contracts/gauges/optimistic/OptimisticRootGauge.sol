281e98f78dca2c3aabb990c5841e5036c12fa7f6
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2024-05-31 13:28:26 +0100

    Refactor: Optimism --> Optimistic gauges, with public network tag. (#2565)

--- pkg/liquidity-mining/contracts/gauges/optimistic/OptimisticRootGauge.sol
+++ pkg/liquidity-mining/contracts/gauges/optimistic/OptimisticRootGauge.sol
@@ -21,8 +21,11 @@ import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/SafeERC20.sol";
 
 import "../StakelessGauge.sol";
 
-contract OptimismRootGauge is StakelessGauge {
+contract OptimisticRootGauge is StakelessGauge {
     using SafeERC20 for IERC20;
+
+    // solhint-disable-next-line var-name-mixedcase
+    string public NETWORK;
 
     IL1StandardBridge private immutable _optimismL1StandardBridge;
     address private immutable _optimismBal;
@@ -41,11 +44,16 @@ contract OptimismRootGauge is StakelessGauge {
         _factory = IOptimismGasLimitProvider(msg.sender);
     }
 
-    function initialize(address recipient, uint256 relativeWeightCap) external {
+    function initialize(
+        address recipient,
+        uint256 relativeWeightCap,
+        string memory targetNetwork
+    ) external {
         // This will revert in all calls except the first one
         __StakelessGauge_init(relativeWeightCap);
 
         _recipient = recipient;
+        NETWORK = targetNetwork;
     }
 
     function getRecipient() external view override returns (address) {
